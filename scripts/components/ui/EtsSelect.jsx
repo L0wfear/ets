83d82be22d2fcaaad5b6aa28709bb910707f6094
Maxim Chepel <maxim.chepel@gost-group.com>
2016-02-01 14:54:46 +0300

    ETS-60, ETS-124, ETS-117.

--- scripts/components/ui/EtsSelect.jsx
+++ scripts/components/ui/EtsSelect.jsx
@@ -1,4 +1,4 @@
 import React, {Component} from 'react';
 import Select from 'react-select';
 
-export default (props) => <Select {...props} placeholder="Выберите..."/>;
+export default (props) => <Select {...props} placeholder="Выберите..." noResultsText="Ничего не найдено" />;

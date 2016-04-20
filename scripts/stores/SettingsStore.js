42d0f13888db49f88457b70b43507ec04bd99fd6
amorphousxd <amorphousxd@gmail.com>
2016-04-20 18:21:14 +0300

    Добавил контроли в легенду, поправил центрирование карты по ОДХ

--- scripts/stores/SettingsStore.js
+++ scripts/stores/SettingsStore.js
@@ -10,10 +10,14 @@ class SettingsStore extends Store {
 
     this.register(settingsActions.setShowPlates, this.handleSetShowPlates);
     this.register(settingsActions.setShowTrack, this.handleSetShowTrack);
+    this.register(settingsActions.setShowRoute, this.handleSetShowRoute);
+    this.register(settingsActions.setShowSelectedElement, this.handleSetShowSelectedElement);
 
     this.state = {
       showPlates: false,
-      showTrack: true
+      showTrack: true,
+      showRoute: true,
+      showSelectedElement: true
     };
 
   }
@@ -30,6 +34,24 @@ class SettingsStore extends Store {
     }
     this.setState({
       showTrack
+    });
+  }
+
+  handleSetShowRoute(showRoute) {
+    if (typeof showRoute === 'undefined') {
+      showRoute = !this.state.showRoute;
+    }
+    this.setState({
+      showRoute
+    });
+  }
+
+  handleSetShowSelectedElement(showSelectedElement) {
+    if (typeof showSelectedElement === 'undefined') {
+      showSelectedElement = !this.state.showSelectedElement;
+    }
+    this.setState({
+      showSelectedElement
     });
   }
 

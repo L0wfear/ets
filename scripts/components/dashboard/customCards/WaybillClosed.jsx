46fb4a6bb24c61cda501736e0fd8fc521d3187e9
Stanislav <totenraum@gmail.com>
2016-04-22 16:19:25 +0300

    ETS-787

--- scripts/components/dashboard/customCards/WaybillClosed.jsx
+++ scripts/components/dashboard/customCards/WaybillClosed.jsx
@@ -22,7 +22,7 @@ export default class WaybillClosed extends DashboardCardMedium {
     this.props.openSubitemsList(true);
     this.context.flux.getActions('waybills').getWaybill(item.data.waybill_id).then(r => {
       console.log(r.result[0])
-      this.setState({showWaybillForm: true, selectedWaybill: r.result[0]});
+      if (r.result[0]) this.setState({showWaybillForm: true, selectedWaybill: r.result[0]});
     });
   }
 

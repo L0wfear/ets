4f1b18170901c808e7846867a787c769c12466ba
Stanislav <totenraum@gmail.com>
2016-05-10 19:03:59 +0300

    ETS-921

--- scripts/components/waybill/WaybillJournal.jsx
+++ scripts/components/waybill/WaybillJournal.jsx
@@ -80,6 +80,22 @@ let getTableMeta = (props) => {
 				filter: false
 			},
 			{
+				name: 'car_model_name',
+				caption: 'Модель ТС/Марка шасси',
+				type: 'string',
+			},
+			{
+				name: 'car_special_model_name',
+				caption: 'Марка шасси',
+				type: 'string',
+				display: false
+			},
+			{
+				name: 'garage_number',
+				caption: 'Гаражный номер',
+				type: 'string',
+			},
+			{
 				name: 'fact_departure_date',
 				caption: 'Выезд факт',
 				type: 'date',
@@ -124,6 +140,7 @@ let WaybillsTable = (props) => {
 			date_create: ({data}) => <DateFormatter date={data} />,
 			fact_departure_date: ({data}) => <DateFormatter date={data} time={true} />,
 			fact_arrival_date: ({data}) => <DateFormatter date={data} time={true} />,
+			car_model_name: (meta) => <div>{meta.data+"/"+meta.rowData.car_special_model_name}</div>,
 			all_missions_completed_or_failed: ({data}) => <div>{waybillMissionsCompleteStatusLabelFunction(data)}</div>
 		};
 

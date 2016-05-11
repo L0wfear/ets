9ddd44a0a038897d5cbea28bd06c15b1e56d81df
Stanislav <totenraum@gmail.com>
2016-05-11 20:42:18 +0300

    ETS-918

--- scripts/components/dashboard/customCards/WaybillActive.jsx
+++ scripts/components/dashboard/customCards/WaybillActive.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Div from '../../ui/Div.jsx';
+import _ from 'lodash';
 import { Panel, Collapse, Glyphicon, Fade, Well, Button } from 'react-bootstrap';
 import DashboardCardMedium from '../DashboardCardMedium.jsx';
 import FaxogrammMissionsFormWrap from '../../directories/faxogramm/FaxogrammMissionsFormWrap.jsx';
@@ -13,25 +14,34 @@ export default class WaybillActive extends WaybillClosed {
 
   renderSubitems(subItems) {
 
-    let subItemsCompleted = subItems.filter((e) => e.data.mission_status === 'Выполнено');
-    let subItemsInProgress = subItems.filter((e) => e.data.mission_status === 'В работе');
+    subItems
+      .sort((a,b) => (new Date(b.data.create_date).getTime() - new Date(a.data.create_date).getTime()))
+      .map((e, i, a) => {
+        if (a[i+1]) {
+          if (e.data.create_date !== a[i+1].data.create_date) {
+            e.data.break = true;
+          }
+        }
+        return e;
+      });
+    subItems.sort((a,b) => {
+        let ast = a.data.mission_status === 'Выполнено' ? 1 : 0;
+        let bst = b.data.mission_status === 'Выполнено' ? 1 : 0;
+        let atime = new Date(a.data.create_date.split('T')[0]).getTime();
+        let btime = new Date(b.data.create_date.split('T')[0]).getTime();
+        return btime+bst - atime+ast
+      })
 
     return (
       <ul>
-        {subItemsCompleted.map((item, i) => (
-          <li key={i} className="pointer_completed" onClick={this.action.bind(this, item)}>
+        {subItems.map((item, i) => (
+          <li key={i} className={item.data.mission_status === "Выполнено" ? "pointer_completed" : "pointer_in_progress"} onClick={this.action.bind(this, item)}>
             {`№${item.data.waybill_number} (${item.data.mission_status}), ${item.data.car_gov_number}`}
             <br/>
             {`${item.data.driver_fio || ''}${item.data.driver_phone ? ', ' + item.data.driver_phone : ''}`}
+            {item.data.break ? <p/> : ''}
           </li>
-        ))}
-        {subItemsInProgress.map((item, i) => (
-          <li key={i} className="pointer_in_progress" onClick={this.action.bind(this, item)}>
-            {`№${item.data.waybill_number} (${item.data.mission_status}), ${item.data.car_gov_number}`}
-            <br/>
-            {`${item.data.driver_fio || ''}${item.data.driver_phone ? ', ' + item.data.driver_phone : ''}`}
-          </li>
-        ))}
+          ))}
       </ul>
     );
   }

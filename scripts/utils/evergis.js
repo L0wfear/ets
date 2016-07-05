0bf1b30abb8f6c0c26a3574fe4fd2147de84be8a
amorphousxd <amorphousxd@gmail.com>
2016-07-05 17:39:38 +0300

    ETS-1195 Добавил обработку истекшего evergis токена

---
+++ scripts/utils/evergis.js
@@ -0,0 +1,45 @@
+import { getEverGisToken } from '../adapter.js';
+
+const ATTEMPTS_LIMIT = 10;
+const FIFTY_SECONDS = 1000 * 50;
+
+let EVERGIS_TOKEN = null;
+let lastTokenUsedTime = null;
+let loading = false;
+let attempts = 0;
+let refreshAttemptsTimeoutId = null;
+
+function refreshAttempts() {
+  attempts = 0;
+}
+
+// Получаем токен из evergis
+export function fetchToken() {
+  attempts += 1;
+  if (refreshAttemptsTimeoutId !== null) {
+    clearInterval(refreshAttemptsTimeoutId);
+  }
+  // Добавляем интервал, через который попытки получения токена обнулятся, на случай долгой сессии
+  if (!attemptsLimitExceeded()) {
+    refreshAttemptsTimeoutId = setTimeout(refreshAttempts, FIFTY_SECONDS);
+  }
+  loading = true;
+  return getEverGisToken().then((token) => {
+    loading = false;
+    EVERGIS_TOKEN = token;
+  });
+}
+
+export function isFetchingToken() {
+  return loading;
+}
+
+export function getToken() {
+  return EVERGIS_TOKEN;
+}
+
+export function attemptsLimitExceeded() {
+  return attempts >= ATTEMPTS_LIMIT;
+}
+
+export default EVERGIS_TOKEN;

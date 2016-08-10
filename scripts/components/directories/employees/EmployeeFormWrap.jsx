96498a084e6283b295f49dd2eff5d63e94021693
Stanislav <totenraum@gmail.com>
2016-08-10 17:52:09 +0300

    ETS-1523

--- scripts/components/directories/employees/EmployeeFormWrap.jsx
+++ scripts/components/directories/employees/EmployeeFormWrap.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import EmployeeForm from './EmployeeForm.jsx';
 import FormWrap from '../../compositions/FormWrap.jsx';
+import { validateRow } from 'validate/validateRow.js';
 
 class EmployeeFormWrap extends FormWrap {
 
@@ -12,6 +13,57 @@ class EmployeeFormWrap extends FormWrap {
 		};
 		this.createAction = context.flux.getActions('employees').createEmployee;
 		this.updateAction = context.flux.getActions('employees').updateEmployee;
+		this.schema = {
+		  properties: [
+				{
+					key: 'last_name',
+		      title: 'Фамилия',
+					type: 'string',
+					required: true,
+				},
+				{
+					key: 'first_name',
+		      title: 'Имя',
+					type: 'string',
+					required: true,
+				},
+				{
+					key: 'position_id',
+		      title: 'Должность',
+					type: 'number',
+					required: true,
+				},
+				{
+					key: 'special_license',
+		      title: 'Специальное удостоверение',
+					type: 'string',
+					required: true,
+				},
+				{
+					key: 'drivers_license',
+		      title: 'Водительское удостоверение',
+					type: 'string',
+					required: true,
+				},
+			],
+		};
+	}
+
+	validate(formState, errors) {
+		if (typeof this.schema === 'undefined') return errors;
+		let formErrors = _.clone(errors);
+		let schema = _.cloneDeep(this.schema);
+		if (formState.position_id !== 15 && formState.position_id !== 24) {
+			schema.properties.splice(-2);
+			delete formErrors["special_license"]
+			delete formErrors["drivers_license"]
+		};
+		console.log(this.schema)
+		_.each(schema.properties, prop => {
+			formErrors[prop.key] = validateRow(prop, formState[prop.key]);
+		});
+
+		return formErrors;
 	}
 
 	render() {
@@ -19,11 +71,14 @@ class EmployeeFormWrap extends FormWrap {
 		let props = this.props;
 
 		return props.showForm ?
-	    <EmployeeForm formState = {this.state.formState}
-									onSubmit={this.handleFormSubmit.bind(this)}
-									handleFormChange={this.handleFormStateChange.bind(this)}
-									show={this.props.showForm}
-									onHide={this.props.onFormHide}/>
+	    <EmployeeForm
+					formState={this.state.formState}
+					formErrors={this.state.formErrors}
+					canSave={this.state.canSave}
+					onSubmit={this.handleFormSubmit.bind(this)}
+					handleFormChange={this.handleFormStateChange.bind(this)}
+					show={this.props.showForm}
+					onHide={this.props.onFormHide}/>
 								: null;
 
 	}

c987494726897b5491297cd49f33e12e8f888a01
amorphousxd <pavel.makeenko@gost-group.com>
2016-08-17 11:12:21 +0300

    Сделал наработки для ETS-1531

--- scripts/stores/DashboardStore.js
+++ scripts/stores/DashboardStore.js
@@ -1,10 +1,8 @@
 import { Store } from 'flummox';
 import _ from 'lodash';
 
-
-//`${key}=${c.filter[key].join(`&${key}=`)}`
-
-const commonComponents = [
+// TODO сделано для ETS-1531
+const dashboardComponents = [
   {
     id: 1,
     key: 'current_missions',
@@ -15,18 +13,17 @@ const commonComponents = [
     key: 'future_missions',
   },
   {
-    id: 3,
-    key: 'count_traveled_routes_by_current_operations',
-    itemsTitle: 'Расшифровка факсограммы'
+    id: 4,
+    key: 'odh_not_covered_by_missions_of_current_shift',
+    itemsTitle: 'Список одх'
   },
   {
-    id: 6,
-    key: 'count_offline_cars',
+    id: 5,
+    key: 'odh_not_covered_by_routes',
   },
   {
-    id: 7,
-    key: 'car_in_work_on_current_missions',
-    itemsTitle: 'Гос. номер ТС'
+    id: 8,
+    key: 'car_in_work_overall',
   },
   {
     id: 9,
@@ -34,13 +31,46 @@ const commonComponents = [
     itemsTitle: 'Расшифровка факсограммы'
   },
   {
-    id: 13,
-    key: 'estimated_time',
+    id: 12,
+    key: 'current_duty_missions',
+    itemsTitle: 'Карточка наряд-задания'
+  },
+  {
+    id: 19,
+    key: 'waybill_draft',
+    itemsTitle: 'Гос. номер ТС'
+  },
+  {
+    id: 20,
+    key: 'waybill_active',
+    itemsTitle: 'Информация о ПЛ'
+  },
+  {
+    id: 21,
+    key: 'waybill_closed',
+    itemsTitle: 'Информация о ПЛ'
   }
-]
+];
+
+const commonComponents = [
+  {
+    id: 1,
+    key: 'current_missions',
+    itemsTitle: 'Карточка задания'
+  },
+  {
+    id: 9,
+    key: 'faxogramms',
+    itemsTitle: 'Расшифровка факсограммы'
+  }
+];
 
 const componentsByRole = {
-  master: [...commonComponents.filter(c => [3, 6, 7, 13].indexOf(c.id) === -1),
+  master: [...commonComponents,
+    {
+      id: 2,
+      key: 'future_missions',
+    },
     {
       id: 4,
       key: 'odh_not_covered_by_missions_of_current_shift',
@@ -50,49 +80,17 @@ const componentsByRole = {
       id: 5,
       key: 'odh_not_covered_by_routes',
     },
-    { //временно не показываем
+    {
       id: 8,
       key: 'car_in_work_overall',
-      //side: true,
     },
     {
       id: 12,
       key: 'current_duty_missions',
       itemsTitle: 'Карточка наряд-задания'
     }
-    // {
-    //   id: 11,
-    //   key: 'daily_missions_percent',
-    //   side: true,
-    // },
-    // {
-    //   id: 10,
-    //   key: 'count_waybill_closed',
-    //   side: true,
-    //   filter: {
-    //     status: ['closed'],
-    //     date_create: [moment().format('YYYY-MM-DD')],
-    //   }
-    // },
-    // {
-    //   id: 13,
-    //   key: 'estimated_time'
-    // }
   ],
-  dispatcher: [//...commonComponents,
-    // {
-    //   id: 15,
-    //   key: 'count_assigned_routes'
-    // },
-    // {
-    //   id: 16,
-    //   key: 'released_waybill',
-    //   side: true,
-    // },
-    // {
-    //   id: 18,
-    //   key: 'count_closed_waybill_by_current_operations'
-    // }
+  dispatcher: [...commonComponents,
     {
       id: 19,
       key: 'waybill_draft',
@@ -107,28 +105,20 @@ const componentsByRole = {
       id: 21,
       key: 'waybill_closed',
       itemsTitle: 'Информация о ПЛ'
-    },
-    {
-      id: 22,
-      key: 'current_missions',
-      itemsTitle: 'Карточка задания'
-    },
-    {
-      id: 24,
-      key: 'faxogramms',
-      itemsTitle: 'Расшифровка факсограммы'
     }
   ]
 }
 
-const initialState = {
-  componentsIndex: {},
-  componentsSideIndex: {},
-  componentsList: [],
-  componentsSideList: [],
-};
+export default class DashboardStore extends Store {
 
-class DashboardStore extends Store {
+  static get initialState() {
+    return {
+      componentsIndex: {},
+      componentsSideIndex: {},
+      componentsList: [],
+      componentsSideList: []
+    };
+  }
 
   constructor(flux) {
     super();
@@ -138,7 +128,7 @@ class DashboardStore extends Store {
     this.register(dashboardActions.getDashboardComponent, this.handleGetDashboardComponent);
     this.register(dashboardActions.getDashboardSideComponent, this.handleGetDashboardSideComponent);
 
-    this.state = _.cloneDeep(initialState);
+    this.state = _.cloneDeep(DashboardStore.initialState);
   }
 
   handleGetDashboardComponent({key, component}) {
@@ -168,15 +158,24 @@ class DashboardStore extends Store {
     this.setState({componentsSideList, componentsSideIndex});
   }
 
+  // TODO сделано для ETS-1531
+  // получение какие компоненты отображать
+  getComponentsByPermissions() {
+    let { permissions = [] } = this.flux.getStore('session').getCurrentUser();
+    const dashboardPermissions = permissions
+      .map(p => p.toLowerCase())
+      .filter(p => p.indexOf('dashboard') + 1)
+      .map(p => p.replace('dashboard.', ''));
+    return dashboardComponents.filter(c => dashboardPermissions.indexOf(c.key) + 1);
+  }
+
   getComponentsByRole() {
     let { role } = this.flux.getStore('session').getCurrentUser();
     return componentsByRole[role];
   }
 
   resetState() {
-    this.setState(_.cloneDeep(initialState));
+    this.setState(_.cloneDeep(DashboardStore.initialState));
   }
 
 }
-
-export default DashboardStore;

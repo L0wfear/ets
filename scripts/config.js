6cce15c806ad9df8d9a5bfde9f645895d51434c6
amorphousxd <pavel.makeenko@gost-group.com>
2016-09-01 16:32:18 +0300

    изменил конфигурацию

--- scripts/config.js
+++ scripts/config.js
@@ -2,48 +2,24 @@ const PROTO = window.location.protocol;
 const WS_PROTO = 'wss:';
 
 let config = {
-  backend: PROTO + '//ods.mos.ru/ssd/ets/services',
-  backendOld: PROTO + '//ods.mos.ru/ssd/city-dashboard',
   ws: WS_PROTO + '//ods.mos.ru/ssd/city-dashboard/stream',
-  images: PROTO + '//ods.mos.ru/ssd/ets/data/images/',
-  REQUEST_PARAMS: {
-    credentials: 'include'
-  }
+  images: PROTO + '//ods.mos.ru/ssd/ets/data/images/'
 };
 
-console.log(process.env);
+const STANDS = {
+  'test': PROTO + '//213.79.88.5/ets-test/services',
+  'study': PROTO + '//213.79.88.5/ets-study/services'
+};
 
 try {
-  switch (process.env.STAND) {
-    case 'study':
-      config.backend = PROTO + '//ods.mos.ru/ssd/ets-study/services';
-      break;
-    case 'test':
-      config.backend = PROTO + '//ods.mos.ru/ssd/ets-test/services';
-      break;
-    case 'development':
-      config.backend = PROTO + '//ods.mos.ru/ssd/ets-test/services';
-      break;
-    case 'second':
-      config.backend = PROTO + '//213.79.88.5/ets-test/services';
-      break;
-    case 'secondstudy':
-      config.backend = PROTO + '//213.79.88.5/ets-study/services';
-      break;
+  if (process.env.APIHOST) {
+    config.backend =  `${PROTO}//${APIHOST}`;
+  } else {
+    if (process.env.STAND) {
+      config.backend = STANDS[process.env.STAND] || STANDS['test'];
+    }
   }
 } catch (e) {
-}
-
-// локальный сервер или специфичный стенд
-let localServerUrl;
-
-try {
-  localServerUrl = LOCAL_SERVER_URL;
-  if (localServerUrl) {
-    config.backend = localServerUrl;
-  }
-} catch (e) {
-  localServerUrl = null;
 }
 
 export default config;

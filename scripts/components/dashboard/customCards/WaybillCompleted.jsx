c709e1b805a59de913dacedd452e3973537aa9bc
Stanislav Skorobovenko <totenraum@gmail.com>
2016-09-07 17:50:50 +0300

    ETS-1733

---
+++ scripts/components/dashboard/customCards/WaybillCompleted.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Div from 'components/ui/Div.jsx';
+import WaybillActive from './WaybillActive.jsx';
+
+export default class WaybillCompleted extends WaybillActive {
+
+  constructor(props) {
+    super(props);
+  }
+
+}

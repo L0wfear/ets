f3fa76a272559e8211d645455fdd2a4f8f4a0248
amorphousxd <pavel.makeenko@gost-group.com>
2016-09-09 10:59:35 +0300

    ETS-1777

--- scripts/components/login/LoginPage.jsx
+++ scripts/components/login/LoginPage.jsx
@@ -19,23 +19,6 @@ export default class LoginPage extends Component {
     };
   }
 
-  render() {
-    const { login, password } = this.state;
-    const disabled = login.length === 0 || password.length === 0;
-
-    return (
-      <div style={{ paddingTop: 40 }}>
-        <div className="form-signin">
-          <label className="sr-only">Имя пользователя</label>
-          <input type="text" className="form-control" placeholder="Имя пользователя" value={login} onChange={this.handleChange.bind(this, 'login')}/>
-          <label className="sr-only">Пароль</label>
-          <input type="password" className="form-control" placeholder="Пароль" value={password} onChange={this.handleChange.bind(this, 'password')}/>
-          <button className="btn btn-lg btn-primary btn-block" disabled={disabled} onClick={this.onSigninClick.bind(this)}>Вход</button>
-        </div>
-      </div>
-    );
-  }
-
   handleChange(field, e) {
     const value = e !== undefined && e !== null && !!e.target ? e.target.value : e;
     this.setState({
@@ -43,7 +26,8 @@ export default class LoginPage extends Component {
     });
   }
 
-  onSigninClick() {
+  onSigninClick(e) {
+    e.preventDefault();
     const { login, password } = this.state;
     const { flux } = this.context;
     const user = {
@@ -60,6 +44,23 @@ export default class LoginPage extends Component {
       }
 
     });
+  }
+
+  render() {
+    const { login, password } = this.state;
+    const disabled = login.length === 0 || password.length === 0;
+
+    return (
+      <form style={{ paddingTop: 40 }} onSubmit={this.onSigninClick.bind(this)}>
+        <div className="form-signin">
+          <label className="sr-only">Имя пользователя</label>
+          <input type="text" className="form-control" placeholder="Имя пользователя" value={login} onChange={this.handleChange.bind(this, 'login')}/>
+          <label className="sr-only">Пароль</label>
+          <input type="password" className="form-control" placeholder="Пароль" value={password} onChange={this.handleChange.bind(this, 'password')}/>
+          <button role="button" className="btn btn-lg btn-primary btn-block" disabled={disabled} onClick={this.onSigninClick.bind(this)}>Вход</button>
+        </div>
+      </form>
+    );
   }
 
 }

50ddd154f847423637db444547c4ad3680912cef
Stanislav Skorobovenko <totenraum@gmail.com>
2016-10-12 15:05:31 +0300

    ETS-1504

--- src/components/directories/faxogramm/FaxogrammMissionsFormWrap.jsx
+++ src/components/directories/faxogramm/FaxogrammMissionsFormWrap.jsx
@@ -36,55 +36,72 @@ class FaxogrammMissionsFormWrap extends FormWrap {
       assign_to_waybill: formState.assign_to_waybill,
     };
 
-    const createMissions = async (element) => {
-        try {
-          await flux.getActions('missions').createMissions(element, payload);
-        } catch (e) {
-          if (e && e.message.code === 'no_active_waybill') {
-            confirmDialog({
-              title: `Для ТС не существует активного ПЛ`,
-              body: `Создать черновик ПЛ?`,
-            })
-            .then((async) () => {
-              payload.assign_to_waybill = 'assign_to_draft';
-              await createMissions();
-            })
-            .catch(() => {});
-          }
-          if (e && e.message.code === 'invalid_period') {
-            const waybillNumber = e.message.message.split('№')[1].split(' ')[0];
-            const dateStart = e.message.message.split('(')[1].split(' - ')[0];
-            const dateEnd = e.message.message.split(' - ')[1].slice(0,-1);
-
-            let body = (self) => <div>
-              <label>
-                Задание будет добавлено в ПЛ № {waybillNumber}
-              </label>
-              <div>{e.message.message}</div>
-              Введите даты задания <IntervalPicker
-                interval={self.state.interval}
-                onChange={interval => self.setState({interval})} />
-            </div>;
-
-            confirmDialog({
-              title: `Пересечение времени задания и ПЛ №${waybillNumber}`,
-              body
-            })
-            .then((async) (state) => {
-              payload.date_start = state.interval[0];
-              payload.date_end = state.interval[1];
-              await createMissions();
-            })
-            .catch(() => {});
-          }
-          return;
+    const createMissions = async (element, payload) => {
+      let error = false;
+      try {
+        await flux.getActions('missions').createMissions(element, payload);
+      } catch (e) {
+        error = true;
+        if (e && e.message.code === 'no_active_waybill') {
+          confirmDialog({
+            title: `Для ТС не существует активного ПЛ`,
+            body: `Создать черновик ПЛ?`,
+          })
+          .then((async) () => {
+            let newPayload = {
+              mission_source_id: '4',
+              faxogramm_id: payload.id,
+              date_start: payload.order_date,
+              date_end: payload.order_date_to,
+              assign_to_waybill: 'assign_to_draft',
+            };
+            await createMissions(element, newPayload);
+          })
+          .catch(() => {});
         }
-      };
+        if (e && e.message.code === 'invalid_period') {
+          const waybillNumber = e.message.message.split('№')[1].split(' ')[0];
+          const dateStart = e.message.message.split('(')[1].split(' - ')[0];
+          const dateEnd = e.message.message.split(' - ')[1].slice(0,-1);
 
-    _.keys(formState.missionJournalState.checkedElements)
-      .map(key => formState.missionJournalState.checkedElements[key])
-      .forEach(async (element) => await createMissions({[element.id]: element}));
-    this.props.onFormHide();
+          let body = (self) => <div>
+            <label>
+              Задание будет добавлено в ПЛ № {waybillNumber}
+            </label>
+            <div>{e.message.message}</div>
+            Введите даты задания <IntervalPicker
+              interval={self.state.interval}
+              onChange={interval => self.setState({interval})} />
+          </div>;
+
+          const state = await confirmDialog({
+            title: `Пересечение времени задания и ПЛ №${waybillNumber}`,
+            body
+          });
+          let newPayload = {
+            mission_source_id: '4',
+            faxogramm_id: payload.id,
+            date_start: state.interval[0],
+            date_end: state.interval[1],
+            assign_to_waybill: payload.assign_to_waybill,
+          };
+          await createMissions(element, newPayload);
+        }
+      }
+      return error;
+    };
+
+    const missions = _.keys(formState.missionJournalState.checkedElements)
+      .map(key => formState.missionJournalState.checkedElements[key]);
+
+    let closeForm = true;
+
+    for (let m of missions) {
+      const e = await createMissions({[m.id]: m}, payload);
+      if (e) closeForm = false;
+    }
+
+    closeForm && this.props.onFormHide();
     return;
   }
 

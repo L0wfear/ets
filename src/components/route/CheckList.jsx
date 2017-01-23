fba238a3f8ff1d94bc949cc41cd3bc1f4bcfd1c7
Stanislav Skorobovenko <totenraum@gmail.com>
2017-01-23 18:17:12 +0300

    ETS-2519

---
+++ src/components/route/CheckList.jsx
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+import { polyState } from 'constants/polygons.js';
+import { Button } from 'react-bootstrap';
+import Div from 'components/ui/Div.jsx';
+import each from 'lodash/each';
+import uniqBy from 'lodash/uniqBy';
+
+export default function ODHList(props) {
+  const { checkRoute, name } = props;
+  let { list = [], fail_list = [] } = props;
+  const LIST = list.map((odh, index) => {
+    const speed_type = odh.state === polyState.IDLE ? 'холостой' : 'рабочий';
+    return <li key={index}>{`${odh.name || odh.odh_name || odh.object_name} (${speed_type} ход)`}</li>
+  });
+  const FAIL = fail_list.map((odh, index) => <li key={index}>{`${odh.name || odh.odh_name || odh.object_name}`}</li>);
+
+  return (
+    <Div>
+      <Div hidden={!checkRoute}>
+        <Button onClick={checkRoute} style={{ marginTop: 11 }}>Проверить маршрут</Button>
+      </Div>
+      <Div hidden={!list.length} className="odh-list">
+        <h4>Список {name}</h4>
+        <ul>{LIST}</ul>
+      </Div>
+      <Div hidden={!fail_list.length} className="odh-list">
+        <h4>Список непокрытых {name}</h4>
+        <ul>{FAIL}</ul>
+      </Div>
+    </Div>
+  );
+}
+
+ODHList.propTypes = {
+  list: PropTypes.array,
+  fail_list: PropTypes.array,
+  checkRoute: PropTypes.func,
+  showSelectable: PropTypes.bool,
+};

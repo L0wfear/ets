a37ca34e9474ca494fc96be3a9e98ea1e3f0befd
Stanislav Skorobovenko <totenraum@gmail.com>
2017-03-31 15:34:11 +0300

    DITETS-360

--- src/components/missions/mission/MissionForm.jsx
+++ src/components/missions/mission/MissionForm.jsx
@@ -379,7 +379,7 @@ export class MissionForm extends Form {
           </Row>
           <Row>
             <Col md={12}>
-              <Div hidden={this.state.selectedRoute === null} className="mission-form-map-wrapper">
+              <Div hidden={route ? route.id == null : true} className="mission-form-map-wrapper">
                 <RouteInfo route={this.state.selectedRoute} mapOnly />
               </Div>
             </Col>

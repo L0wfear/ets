1ef0f8c517cd079f9f07a278b799ae55711b0c72
Kirill A. Chernya-Khalitov <voronar@gmail.com>
2017-06-15 21:23:22 +0300

    hotfix/DITETS-1206/v.0.9.64 Fixes cleaning rate square property field

--- src/components/directories/cleaning_rate/CleaningRateTable.jsx
+++ src/components/directories/cleaning_rate/CleaningRateTable.jsx
@@ -1,24 +1,6 @@
 import React from 'react';
 import Table from 'components/ui/table/DataTable.jsx';
 
-function getProperties(value) {
-  const properties = {
-    total_area: 'Общая площадь (кв.м.)',
-    distance: 'Протяженность (п.м.)',
-    roadway_area: 'Площадь проезжей части (кв.м.)',
-    footway_area: 'Площадь тротуаров (кв.м.)',
-    cleaning_area: 'Площадь уборки (кв.м.)',
-    auto_footway_area: 'Площадь механизированной уборки тротуаров (кв.м.)',
-    manual_footway_area: 'Площадь ручной уборки тротуаров (кв.м.)',
-    snow_area: 'Площадь уборки снега (кв.м.)',
-    gutters_length: 'Протяженность лотков (п.м.)',
-    clean_area: 'Общая уборочная площадь (кв.м.)',
-    mechanical_clean_area: 'Площадь механизированной уборки (кв.м.)',
-    station_number: 'Кол-во убираемых остановок (ед.)',
-  };
-  return properties[value];
-}
-
 const tableMeta = {
   cols: [
     {
@@ -34,7 +16,6 @@ const tableMeta = {
       displayName: 'Площадная характеристика',
       filter: {
         type: 'multiselect',
-        labelFunction: v => getProperties(v),
       },
       cssClassName: 'width150',
     },
@@ -52,16 +33,11 @@ const tableMeta = {
 };
 
 export default (props) => {
-  const renderers = {
-    property: ({ data }) => <div>{getProperties(data)}</div>,
-  };
-
   return (
     <Table
       title="Показатели для расчета эффективности работы бригад"
       results={props.data}
       tableMeta={tableMeta}
-      renderers={renderers}
       {...props}
     />
   );

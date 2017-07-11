eddf5ad77440c8a904087cdc5da70413423b79c2
Kirill A. Chernya-Khalitov <voronar@gmail.com>
2017-07-11 15:57:17 +0300

    Changes autobase module hierarchy

---
+++ src/components/directories/autobase/index.js
@@ -0,0 +1,9 @@
+import batteryReg from './battery_registry/BatteryRegList.jsx';
+import batteryBrand from './battery_brand/BatteryBrandList.jsx';
+import batteryManufacturer from './battery_manufacturer/BatteryManufacturerList.jsx';
+
+export {
+  batteryReg,
+  batteryBrand,
+  batteryManufacturer,
+};

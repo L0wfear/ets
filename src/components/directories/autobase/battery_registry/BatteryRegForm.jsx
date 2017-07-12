0dfa0a1687bbca560b0fd707576496f7f9d46e2d
Vladimir Leshchev <vokinda@yandex.ru>
2017-07-12 12:00:48 +0300

    DITETS-633 add lsit, form

--- src/components/directories/autobase/battery_registry/BatteryRegForm.jsx
+++ src/components/directories/autobase/battery_registry/BatteryRegForm.jsx
@@ -8,7 +8,6 @@ import { connectToStores } from 'utils/decorators';
 
 
 // TODO переделать DITETS-633
-const org = Array(4).fill(1).map((d, i) => d = `-- демо Организация ${i}`);
 
 @connectToStores(['objects'])
 export default class BaseBatteryForm extends Form {
@@ -17,46 +16,117 @@ export default class BaseBatteryForm extends Form {
     super(props);
   }
 
-  // wtf - Количество месяцев наработки
-  // https://gost-jira.atlassian.net/wiki/pages/viewpage.action?pageId=91324941
+  async componentDidMount() {
+    const { flux } = this.context;
 
-  getData(state) {
-    const whatShow = ['battery_brand__name', 'battery_manufacturer__name', 'battery__serial_number', 'battery__lifetime_months', 'battery__released_at'];
-    const errors = this.props.formErrors;
+    const batteryBrand = await flux.getActions('autobase').getAutobaseListByType('batteryBrand');
+    const orgName = Array(4).fill(1).map((d, i) => d = `-- демо Организация ${i}`);
 
-    const wh = (this.props.cols.reduce((obj, oneVal) => {
-      if (whatShow.includes(oneVal.name)) {
-        obj.push({
-          type : oneVal.type,
-          label: oneVal.displayName,
-          value: state[oneVal.name],
-          error: errors[oneVal.name],
-          onChange: this.handleChange.bind(this, oneVal.name),
-        });
-      }
-      return obj;
-    }, [], this));
-    return wh;
-  };
+    const batteryBrandList = batteryBrand.data.result.rows;
 
-  getNameOrg(state, fields) {
-    return {
-      label: fields.name_org.displayName,
-      value: state.name_org,
-      type: 'select',
-      options: org.map((el, i) => ({ value: i, label: el })),
-      emptyValue: null,
-      onChange: (...arg) => this.handleChange(fields.name_org.name, ...arg),
-    }
+    this.setState({ batteryBrandList, orgName });
   }
+
+  onChageWrap = name => (...arg) => this.handleChange(name, ...arg);
+
+
+
+  getNameOrg(value, label, error, ORG_NAME_OPTION) {
+    return {
+      label,
+      value,
+      error,
+      type: 'select',
+      options: ORG_NAME_OPTION,
+      emptyValue: null,
+      onChange: this.onChageWrap('name_org'),
+    };
+  }
+
+  getBatteryBrand(value, label, error, BATTERY_BRAND_OPTION) {
+    return {
+      label,
+      value,
+      error,
+      type: 'select',
+      options: BATTERY_BRAND_OPTION,
+      emptyValue: null,
+      onChange: (...arg) => {
+        const [battery_brand__name_id, second_arg] = arg;
+        const { manufacturer_id, label } = second_arg[0].manufacturer_id;
+
+        this.handleChange('battery_brand__name_id', battery_brand__name_id);
+        this.handleChange('battery_brand__name', label);
+        this.handleChange('battery__brand_id', manufacturer_id);
+      },
+    };
+  }
+
+  getBatteryManifactoryName(value, label) {
+    return {
+      label,
+      value,
+      type: 'string',
+      emptyValue: null,
+      disabled: true,
+    };
+  }
+
+  getData(state, fields, errors) {
+    const demo = ['battery__serial_number', 'battery__lifetime_months'].map(el => (
+      {
+        type: fields[el].type,
+        label: fields[el].displayName,
+        value: state[el],
+        error: errors[el],
+        onChange: this.onChageWrap(el),
+      }
+    ));
+    return demo;
+  }
+
+  getDate(state, fields, errors, el) {
+    return {
+      type: fields[el].type,
+      label: fields[el].displayName,
+      date: state[el],
+      time: false,
+      error: errors[el],
+      onChange: this.onChageWrap(el),
+    };
+  }
+
 
   render() {
     const state = this.props.formState;
+    const errors = this.props.formErrors;
+    console.log(errors, state)
     const fields = this.props.cols.reduce((obj, val) => Object.assign(obj, { [val.name]: val }), {});
 
-    const title = !!!state.battery__id ? 'Добавление нового аккумулятора' : 'Изменение существующего аккумулятора';
-    const dataForForm = this.getData(state);
-    const getNameOrg = this.getNameOrg(state, fields);
+    const { orgName = [], batteryBrandList = [] } = this.state;
+
+    const ORG_NAME_OPTION = orgName.map((el, i) => ({ value: i, label: el }));
+    const BATTERY_BRAND_OPTION = batteryBrandList.map(el => ({ value: el.id, label: el.name, manufacturer_id: el.manufacturer_id }));
+    const BATTERY_BRAND_MANUFACTURER_OPTION = batteryBrandList.reduce((obj, el) => Object.assign(obj, { [el.manufacturer_id]: el.manufacturer_name }), {});
+
+    const nameOrg = this.getNameOrg(state.name_org, fields.name_org.displayName, errors.name_org, ORG_NAME_OPTION);
+    const batteryBrand = this.getBatteryBrand(state.battery_brand__name, fields.battery_brand__name.displayName, errors.battery_brand__name, BATTERY_BRAND_OPTION);
+    const batteryManifactoryName = this.getBatteryManifactoryName(BATTERY_BRAND_MANUFACTURER_OPTION[state.battery__brand_id], fields.battery_manufacturer__name.displayName);
+    const dataForForm = this.getData(state, fields, errors);
+    const dateRelase = this.getDate(state, fields, errors, 'battery__released_at');
+
+    // TODO исправить Количество месяцев наработки
+    const countMonthWork = {
+      type: 'string',
+      label: 'Количество месяцев наработки',
+      value: '---',
+      disabled: true,
+    };
+
+    const IS_CREATING = !!!state.id;
+
+    let title = 'Изменение существующего аккумулятора';
+    if (IS_CREATING) title = 'Добавление нового аккумулятора';
 
     return (
       <Modal {...this.props} backdrop="static">
@@ -64,57 +134,50 @@ export default class BaseBatteryForm extends Form {
           <Modal.Title id="contained-modal-title-lg">{ title }</Modal.Title>
         </Modal.Header>
         <Div style={{ padding: 15 }}>
-          <Row key={-1}>
+          <Row>
             <Col md={12}>
               <Field
-              {...getNameOrg}
+              {...nameOrg}
               />
             </Col>
           </Row>
-          {dataForForm.map((d, i) => {
-            switch (d.type) {
-              case 'date':
-                return (
-                  <Row key={i}>
-                    <Col md={12}>
-                      <Field
-                        type={d.type}
-                        label={d.label}
-                        date={d.value}
-                        error={d.error}
-                        onChange={d.onChange}
-                        time={false}
-                      />
-                    </Col>
-                  </Row>
-                );
-              default:
-                return (
-                  <Row key={i}>
-                    <Col md={12}>
-                      <Field
-                        type={d.type}
-                        label={d.label}
-                        value={d.value}
-                        error={d.error}
-                        date={d.value}
-                        onChange={d.onChange}
-                      />
-                    </Col>
-                  </Row>
-                );
-            }
-          })}
-          { !!state.battery__id && <Row>
+          <Row >
             <Col md={12}>
               <Field
-                type={'string'}
-                label={'Количество месяцев наработки'}
-                value={'--'}
-                readOnly={true}
+              {...batteryBrand}
               />
             </Col>
-          </Row> }
+          </Row>
+          <Row >
+            <Col md={12}>
+              <Field
+              {...batteryManifactoryName}
+              />
+            </Col>
+          </Row>
+          {dataForForm.map((el, i) => (
+            <Row key={i}>
+              <Col md={12}>
+                <Field
+                  {...el}
+                />
+              </Col>
+            </Row>
+          ))}
+          <Row >
+            <Col md={12}>
+              <Field
+              {...dateRelase}
+              />
+            </Col>
+          </Row>
+          <Row >
+            <Col md={12}>
+              <Field
+                {...countMonthWork}
+              />
+            </Col>
+          </Row>
         </Div>
         <ModalBody />
         <Modal.Footer>

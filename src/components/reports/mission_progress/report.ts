99fb57edfc0d6eafdf3044ba4523756dda6fd1b0
Kirill A. Chernya-Khalitov <voronar@gmail.com>
2017-07-17 13:33:38 +0300

    DITETS-1253 Adds new mission progress multi-report

---
+++ src/components/reports/mission_progress/report.ts
@@ -0,0 +1,33 @@
+import { withProps } from 'recompose';
+
+import { IReportProps } from 'components/reports/@types/common.h';
+
+import { exportable } from 'utils/decorators';
+import ReportContainer from 'components/reports/common/ReportContainer';
+import ReportHeader from './ReportHeader';
+
+const serviceUrl = 'mission_progress_report';
+const reportUrl = 'mission-progress-report';
+const serviceName = 'MissionProgressReportService';
+
+const schemaMakers = {};
+const renderers = {};
+const tableProps = {};
+
+const reportProps: IReportProps = {
+  title: 'Отчет по уборке территорий',
+  serviceName,
+  enumerated: true,
+  tableProps,
+  reportUrl,
+  serviceUrl,
+  headerComponent: ReportHeader,
+  renderers,
+  schemaMakers,
+};
+
+const ExportableReportContainer = exportable({
+  entity: serviceUrl,
+})(ReportContainer);
+
+export default withProps(reportProps)(ExportableReportContainer);

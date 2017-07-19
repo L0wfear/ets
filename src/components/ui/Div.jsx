0f185aecda0bddb4b53a61d70b928db9b856eb96
Kirill A. Chernya-Khalitov <voronar@gmail.com>
2017-07-19 13:11:13 +0300

    DITETS-1428 Fixes tire registry creating record issue

--- src/components/ui/Div.jsx
+++ src/components/ui/Div.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export const ExtDiv = ({ className = '', hidden = false, ...props }) => (
-  <div {...props} className={`${className} ${hidden ? 'hidden' : ''}`}>
-    {props.children}
-  </div>
+export const ExtDiv = ({ className = '', hidden = false, nonExist = false, ...props }) => (
+  !nonExist
+    ? (
+      <div {...props} className={`${className} ${hidden ? 'hidden' : ''}`}>
+        {props.children}
+      </div>
+    )
+    : <div className="none" />
 );
 
 export default props => props.hidden ? <div className="none" /> : <div {...props}>{props.children}</div>;

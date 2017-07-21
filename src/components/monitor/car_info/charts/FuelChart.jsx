3601907838e8551117fa1bcadddd2c439266e350
Vladimir Leshchev <vokinda@yandex.ru>
2017-07-21 15:34:07 +0300

    DITETS-1399 | DITETS-1409 edit name column and value on card TC

--- src/components/monitor/car_info/charts/FuelChart.jsx
+++ src/components/monitor/car_info/charts/FuelChart.jsx
@@ -13,7 +13,7 @@ const EventTable = props => {
     <tr key={i} onClick={() => props.showOnMap(d.start_point.timestamp, null, true, d)}>
       <td>{d.date}</td>
       <td>{d.type_name}</td>
-      <td>{d.value}</td>
+      <td>{Math.abs(d.event_val)}</td>
     </tr>
   ));
 
@@ -23,7 +23,7 @@ const EventTable = props => {
         <tr>
           <td>Дата и время</td>
           <td>Событие</td>
-          <td>Уровень</td>
+          <td>Объем, л</td>
         </tr>
       </thead>
       <tbody>
@@ -91,7 +91,7 @@ const FuelChartSFC = props => {
         <input readOnly type="checkbox" checked={rawData} />
         Исходные данные датчиков
       </div>
-      {!rawData && <EventTable data={sumEvents} showOnMap={props.showOnMap} />}
+      <EventTable data={sumEvents} showOnMap={props.showOnMap} />
     </div>
   );
 };

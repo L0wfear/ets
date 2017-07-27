3b30c7d19b37dce50db8e00216a46cb6876fec6c
Vladimir Leshchev <vokinda@yandex.ru>
2017-07-27 14:33:22 +0300

    DITETS-519 Adds store to companyStructure & Edits odh/dt registers

--- src/components/directories/geoobjects/dt/DTTable.jsx
+++ src/components/directories/geoobjects/dt/DTTable.jsx
@@ -1,51 +1,64 @@
 import React, { Component } from 'react';
-import Table from 'components/ui/table/DataTable.jsx';
+import { get } from 'lodash';
 
-export const tableMeta = props => ({
-  cols: [
-    {
-      name: 'company_name',
-      displayName: 'Учреждение',
-      type: 'text',
-      display: props ? props.isOkrug : false,
-      filter: props && props.isOkrug ? { type: 'multiselect' } : false,
-    },
-    {
-      name: 'object_address',
-      displayName: 'Название ДТ',
-      type: 'string',
-      filter: {
-        type: 'multiselect',
+import Table from 'components/ui/table/DataTable.jsx';
+import { defaultSelectListMapper } from 'components/ui/input/EtsSelect';
+
+export const tableMeta = ({
+  companyStructureList = [],
+  isOkrug = false,
+ } = {}) => (
+  {
+    cols: [
+      {
+        name: 'company_name',
+        displayName: 'Учреждение',
+        type: 'text',
+        display: isOkrug,
+        filter: isOkrug ? { type: 'multiselect' } : false,
       },
-    },
-    {
-      name: 'total_area',
-      displayName: 'Общая площадь (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'clean_area',
-      displayName: 'Общая уборочная площадь (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'auto_area',
-      displayName: 'Площадь механизированной уборки (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'company_structure_name',
-      displayName: 'Подразделение',
-      type: 'string',
-      filter: {
-        type: 'multiselect',
+      {
+        name: 'object_address',
+        displayName: 'Название ДТ',
+        type: 'string',
+        filter: {
+          type: 'multiselect',
+        },
       },
-    },
-  ],
-});
+      {
+        name: 'total_area',
+        displayName: 'Общая площадь (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'clean_area',
+        displayName: 'Общая уборочная площадь (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'auto_area',
+        displayName: 'Площадь механизированной уборки (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'company_structure_id',
+        displayName: 'Подразделение',
+        type: 'string',
+        filter: {
+          type: 'multiselect',
+          options: companyStructureList.map(defaultSelectListMapper),
+        },
+      },
+    ],
+  }
+);
 
 export default (props) => {
-  const renderers = {};
+  const { companyStructureList = [] } = props;
+
+  const renderers = {
+    company_structure_id: ({ data }) => <div>{get(companyStructureList.find(s => s.id === data), 'name', '---')}</div>,
+  };
 
   return (<Table
     title="Реестр ДТ"
--- src/components/directories/geoobjects/odh/ODHTable.jsx
+++ src/components/directories/geoobjects/odh/ODHTable.jsx
@@ -1,94 +1,107 @@
 import React, { Component } from 'react';
-import Table from 'components/ui/table/DataTable.jsx';
+import { get } from 'lodash';
 
-export const tableMeta = props => ({
-  cols: [
-    {
-      name: 'company_name',
-      displayName: 'Учреждение',
-      type: 'text',
-      display: props ? props.isOkrug : false,
-      filter: props && props.isOkrug ? { type: 'multiselect' } : false,
-    },
-    {
-      name: 'name',
-      displayName: 'Название',
-      type: 'string',
-      filter: {
-        type: 'multiselect',
+import Table from 'components/ui/table/DataTable.jsx';
+import { defaultSelectListMapper } from 'components/ui/input/EtsSelect';
+
+export const tableMeta = ({
+  companyStructureList = [],
+  isOkrug = false,
+ } = {}) => (
+  {
+    cols: [
+      {
+        name: 'company_name',
+        displayName: 'Учреждение',
+        type: 'text',
+        display: isOkrug,
+        filter: isOkrug ? { type: 'multiselect' } : false,
       },
-    },
-    {
-      name: 'clean_category_name',
-      displayName: 'Категория',
-      type: 'string',
-      filter: {
-        type: 'multiselect',
+      {
+        name: 'name',
+        displayName: 'Название',
+        type: 'string',
+        filter: {
+          type: 'multiselect',
+        },
       },
-    },
-    {
-      name: 'total_area',
-      displayName: 'Общая площадь (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'distance',
-      displayName: 'Протяженность (п.м.)',
-      type: 'number',
-    },
-    {
-      name: 'roadway_area',
-      displayName: 'Площадь проезжей части (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'footway_area',
-      displayName: 'Площадь тротуаров (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'cleaning_area',
-      displayName: 'Площадь уборки (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'footway_length',
-      displayName: 'Длина тротуара (п.м.)',
-      type: 'number',
-    },
-    {
-      name: 'auto_footway_area',
-      displayName: 'Площадь механизированной уборки тротуаров (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'manual_footway_area',
-      displayName: 'Площадь ручной уборки тротуаров (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'snow_area',
-      displayName: 'Площадь уборки снега (кв.м.)',
-      type: 'number',
-    },
-    {
-      name: 'gutters_length',
-      displayName: 'Протяженность лотков (п.м.)',
-      type: 'number',
-    },
-    {
-      name: 'company_structure_name',
-      displayName: 'Подразделение',
-      type: 'text',
-      filter: {
-        type: 'multiselect',
+      {
+        name: 'clean_category_name',
+        displayName: 'Категория',
+        type: 'string',
+        filter: {
+          type: 'multiselect',
+        },
       },
-    },
-  ],
-});
+      {
+        name: 'total_area',
+        displayName: 'Общая площадь (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'distance',
+        displayName: 'Протяженность (п.м.)',
+        type: 'number',
+      },
+      {
+        name: 'roadway_area',
+        displayName: 'Площадь проезжей части (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'footway_area',
+        displayName: 'Площадь тротуаров (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'cleaning_area',
+        displayName: 'Площадь уборки (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'footway_length',
+        displayName: 'Длина тротуара (п.м.)',
+        type: 'number',
+      },
+      {
+        name: 'auto_footway_area',
+        displayName: 'Площадь механизированной уборки тротуаров (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'manual_footway_area',
+        displayName: 'Площадь ручной уборки тротуаров (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'snow_area',
+        displayName: 'Площадь уборки снега (кв.м.)',
+        type: 'number',
+      },
+      {
+        name: 'gutters_length',
+        displayName: 'Протяженность лотков (п.м.)',
+        type: 'number',
+      },
+      {
+        name: 'company_structure_id',
+        displayName: 'Подразделение',
+        type: 'text',
+        filter: {
+          type: 'multiselect',
+          options: companyStructureList.map(defaultSelectListMapper),
+        },
+      },
+    ],
+  }
+);
 
 const ODHTable = (props) => {
-  const renderers = {};
+  const { companyStructureList = [] } = props;
+
+  const renderers = {
+    company_structure_id: ({ data }) => <div>{get(companyStructureList.find(s => s.id === data), 'name', '---')}</div>,
+  };
 
   return (<Table
     title="Реестр ОДХ"

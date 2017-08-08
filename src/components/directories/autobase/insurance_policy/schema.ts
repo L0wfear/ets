01c5b8eae4b23be73e253124ff5c9f7f6b339354
Vladimir Leshchev <vokinda@yandex.ru>
2017-08-08 12:56:56 +0300

    DITETS-1567 Adds remove insurance_name and add validate to date_end in form

--- src/components/directories/autobase/insurance_policy/schema.ts
+++ src/components/directories/autobase/insurance_policy/schema.ts
@@ -1,4 +1,7 @@
+import * as moment from 'moment';
+
 import { IValidationSchema } from 'components/ui/form/@types/validation.h';
+import { getRequiredFieldMessage } from 'utils/validate';
 
 export const formValidationSchema: IValidationSchema = {
   properties: [
@@ -11,13 +14,6 @@ export const formValidationSchema: IValidationSchema = {
     {
       key: 'insurer',
       title: 'Страховая организация',
-      type: 'string',
-      required: true,
-      maxLength: 256,
-    },
-    {
-      key: 'insurance_name',
-      title: 'Наименование страхования',
       type: 'string',
       required: true,
       maxLength: 256,
@@ -60,7 +56,36 @@ export const formValidationSchema: IValidationSchema = {
       maxLength: 128,
       min: 0,
     },
+    {
+      key: 'note',
+      title: 'Примечание',
+      type: 'string',
+      maxLength: 2048,
+      min: 0,
+    },
   ],
+  dependencies: {
+    date_end: [
+      {
+        validator(value = null, { date_start = null }) {
+          if (!value) {
+            return getRequiredFieldMessage('Дата окончания действия');
+          }
+
+          if (date_start) {
+            const start = moment(date_start).unix();
+            const end = moment(value).unix();
+
+            return end >= start
+              ? ''
+              : '"Дата окончания действия" должна быть позже "Даты начала действия"';
+          }
+
+          return '';
+        },
+      },
+    ],
+  },
 };
 
 export default formValidationSchema;

2e0b81c94da2be6d5033e2a07abe9ac65829a616
Kirill A. Chernya-Khalitov <voronar@gmail.com>
2017-08-28 17:04:28 +0300

    DITETS-836 Fixes tech. maint. schedule report date column formatting

--- src/components/reports/tech_maintenance_schedule/report.tsx
+++ src/components/reports/tech_maintenance_schedule/report.tsx
@@ -1,7 +1,10 @@
+import * as React from 'react';
 import { withProps } from 'recompose';
 
 import { IReportProps } from 'components/reports/@types/common.h';
+import { ISchemaRenderer, ISchemaMaker } from 'components/ui/table/@types/schema.h';
 
+import DateFormatter from 'components/ui/DateFormatter';
 import { exportable } from 'utils/decorators';
 import ReportContainer from 'components/reports/common/ReportContainer';
 import ReportHeader from './ReportHeader';
@@ -10,9 +13,18 @@ const serviceUrl = 'tech_maintenance_schedule';
 const reportUrl = 'tech-maintenance-schedule';
 const serviceName = 'TechMaintenanceSchedule';
 
-const schemaMakers = {};
+const schemaMakers: ISchemaMaker = {
+  latest_tech_maintenance_date: schema => ({
+    ...schema,
+    filter: {
+      type: 'date',
+    },
+  }),
+};
 
-const renderers = {};
+const renderers: ISchemaRenderer = {
+  latest_tech_maintenance_date: ({ data }) => <DateFormatter date={data} time={false} />,
+};
 
 const reportProps: IReportProps = {
   title: 'График проведения технического обслуживания транспортных средств',

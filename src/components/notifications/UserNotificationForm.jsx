8680ee33b6d88396e91c9881ce2c5ae9bfeee30f
Vladimir Leshchev <vokinda@yandex.ru>
2017-09-01 12:50:43 +0300

    DITETS-1829 Changes href to onClick in userNotificationForm

--- src/components/notifications/UserNotificationForm.jsx
+++ src/components/notifications/UserNotificationForm.jsx
@@ -7,52 +7,76 @@ import DateFormatter from 'components/ui/DateFormatter.jsx';
 import Form from 'components/compositions/Form.jsx';
 import { CAR_TAB_INDEX } from 'components/directories/cars/CarForm';
 
-const MainVehicleDesc = ({ carId, linkText, activeTab, textInfo }) =>
+const LinkA = ({ linkText, handleClick }) =>
+  <a style={{ marginLeft: 5, cursor: 'pointer' }} onClick={handleClick}>{linkText}</a>;
+
+const MainVehicleDesc = ({ linkText, textInfo, handleClick }) =>
   <div>
     <b>Совет:</b> пройдите по ссылке ниже,
     чтобы посмотреть карточку ТС и добавить информацию <span>{textInfo}</span> прямо сейчас
-    <a style={{ marginLeft: 5 }} href={`/#/cars?asuods_id=${carId}&active_tab=${activeTab}`}>{linkText}</a>
+    <LinkA
+      linkText={linkText}
+      handleClick={handleClick}
+    />
   </div>;
 
-const insurance_policy = ({ data: { car_gov_number, car_id } }) =>
-  <MainVehicleDesc
-    carId={car_id}
-    linkText={car_gov_number}
-    activeTab={CAR_TAB_INDEX.insurance_policy}
-    textInfo="по страхованию"
-  />;
-
-const tech_inspection = ({ data: { tech_inspection_reg_number, car_id } }) =>
-  <MainVehicleDesc
-    carId={car_id}
-    linkText={tech_inspection_reg_number}
-    activeTab={CAR_TAB_INDEX.tech_inspection}
-    textInfo="о государственном техосмотре"
-  />;
-
-const tech_maintenance = ({ data: { car_gov_number, car_id } }) =>
-  <MainVehicleDesc
-    carId={car_id}
-    linkText={car_gov_number}
-    activeTab={CAR_TAB_INDEX.tech_maintenance}
-    textInfo="о техническом обслуживании"
-  />;
-
-const repair = ({ data: { car_gov_number, car_id } }) =>
-  <MainVehicleDesc
-    carId={car_id}
-    linkText={car_gov_number}
-    activeTab={CAR_TAB_INDEX.repair}
-    textInfo="о ремонте"
-  />;
-
-const medical_certificate = ({ data: { employee_fio, employee_id } }) =>
+const MainEmployeeDesc = ({ linkText, handleClick }) =>
   <div>
     <b>Совет:</b> пройдите по ссылке ниже,
     чтобы посмотреть карточку водителя и добавить информацию по медицинской справке прямо сейчас
-    <a style={{ marginLeft: 5 }} href={`/#/employees?employee_id=${employee_id}`}>{employee_fio}</a>
+    <LinkA
+      linkText={linkText}
+      handleClick={handleClick}
+    />
   </div>;
 
+const insurance_policy = ({ data: { car_gov_number, car_id }, handleClick }) =>
+  <MainVehicleDesc
+    linkText={car_gov_number}
+    textInfo="по страхованию"
+    handleClick={() => handleClick('cars', {
+      asuods_id: car_id,
+      active_tab: CAR_TAB_INDEX.insurance_policy,
+    })}
+  />;
+
+const tech_inspection = ({ data: { tech_inspection_reg_number, car_id }, handleClick }) =>
+  <MainVehicleDesc
+    linkText={tech_inspection_reg_number}
+    textInfo="о государственном техосмотре"
+    onChandleClicklick={() => handleClick('cars', {
+      asuods_id: car_id,
+      active_tab: CAR_TAB_INDEX.tech_inspection,
+    })}
+  />;
+
+const tech_maintenance = ({ data: { car_gov_number, car_id }, handleClick }) =>
+  <MainVehicleDesc
+    linkText={car_gov_number}
+    textInfo="о техническом обслуживании"
+    handleClick={() => handleClick('cars', {
+      asuods_id: car_id,
+      active_tab: CAR_TAB_INDEX.tech_inspection,
+    })}
+  />;
+
+const repair = ({ data: { car_gov_number, car_id }, handleClick }) =>
+  <MainVehicleDesc
+    linkText={car_gov_number}
+    textInfo="о ремонте"
+    handleClick={() => handleClick('cars', {
+      asuods_id: car_id,
+      active_tab: CAR_TAB_INDEX.tech_inspection,
+    })}
+  />;
+
+const medical_certificate = ({ data: { employee_fio, employee_id }, handleClick }) =>
+  <MainEmployeeDesc
+    linkText={employee_fio}
+    handleClick={() => handleClick('employees', {
+      employee_id,
+    })}
+  />;
 
 const notificationComponents = {
   insurance_policy,
@@ -70,6 +94,16 @@ export default class UserNotificationForm extends Form {
     if (!state.is_read) {
       this.context.flux.getActions('userNotifications').markAsRead([state.id], false);
     }
+  }
+  handleClick = (pathComponent, query) => {
+    const { goHref } = this.props;
+    const queryString = Object.entries(query).reduce((arr, [key, value]) => {
+      arr.push(`${key}=${value}`);
+      return arr;
+    },
+    []).join('&');
+
+    goHref(`${pathComponent}?${queryString}`);
   }
   render() {
     const state = this.props.formState;
@@ -91,7 +125,10 @@ export default class UserNotificationForm extends Form {
               {state.description}
             </Col>
             <Col md={12} style={{ marginTop: 10 }}>
-              <NotificationDesc data={state.data} />
+              <NotificationDesc
+                data={state.data}
+                handleClick={this.handleClick}
+              />
             </Col>
           </Row>
         </ModalBody>

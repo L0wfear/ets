8cdd99cf38cc6aa45447604444ad36db3e9c3746
Vladimir Leshchev <vokinda@yandex.ru>
2017-09-21 13:34:05 +0300

    DITETS-1154 Changes disabledButton in UserNofication

--- src/components/notifications/UserNotificationList.jsx
+++ src/components/notifications/UserNotificationList.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Button, ButtonToolbar } from 'react-bootstrap';
-import map from 'lodash/map';
 
 import { connectToStores, staticProps } from 'utils/decorators';
+import { isEmpty } from 'utils/functions';
 import CheckableElementsList from 'components/CheckableElementsList';
 import UserNotificationFormWrap from './UserNotificationFormWrap';
 import UserNotificationTable from './UserNotificationTable';
@@ -42,19 +42,24 @@ export default class UserNotificationList extends CheckableElementsList {
    */
   getButtons() {
     const { userNotificationList = [] } = this.props;
+    const {
+      checkedElements = {},
+    } = this.state;
+
     const baseButtons = super.getButtons();
-    const checkedItems = Object.entries(this.state.checkedElements).reduce((obj, [key, el]) => {
+    const checkedItems = Object.entries(checkedElements).reduce((obj, [key, el]) => {
       if (!el.is_read) {
         obj.push(parseInt(key, 10));
       }
 
       return obj;
     }, []);
+    const allNotIsRead = !isEmpty(checkedItems) && !userNotificationList.some(oneN => !oneN.is_read);
 
     const buttons = [
       <ButtonToolbar key={baseButtons.length}>
         {checkedItems.length > 0 && <Button onClick={this.handleMarkAsRead.bind(null, checkedItems)}>Отметить как прочитанное</Button>}
-        <Button disabled={!userNotificationList} onClick={this.handleMarkAllAsRead}>Отметить все как прочитанные</Button>
+        <Button disabled={allNotIsRead} onClick={this.handleMarkAllAsRead}>Отметить все как прочитанные</Button>
       </ButtonToolbar>,
       ...baseButtons,
     ];

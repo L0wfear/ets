dc7db88e60bf71d91fc9cbad2a220765e4eb95db
Vladimir Leshchev <vokinda@yandex.ru>
2017-10-16 23:07:28 +0300

    DITETS-1154 For fixed develop

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,24 @@
 import { routToPer } from 'constants/routerAndPermission.ts';
 
 export const requireAuth = flux => (nextState, replaceState) => {
+  console.log('requireAuth', nexttate.location.pathname )
+  
   if (!flux.getStore('session').isLoggedIn() || !flux.getStore('session').getCurrentUser().role) {
+    console.log('requireAuth', 'iAmNotLogined' nexttate.location.pathname )
+  
     replaceState({ nextPathname: nextState.location.pathname }, '/login');
     return;
   }
+  console.log('requireAuth', 'iAmLogined' nexttate.location.pathname )
+  
   if (routToPer[nextState.location.pathname]) {
+    console.log('requireAuth secondIf', routToPer[nextState.location.pathname], nexttate.location.pathname )
+    
     if (!flux.getStore('session').getPermission(routToPer[nextState.location.pathname].p, true)) {
+      console.log('requireAuth thirdIf', flux.getStore('session').getPermission(routToPer[nextState.location.pathname].p, true), nexttate.location.pathname )
+    
       const sessionRedirect = flux.getStore('session').getStableRedirect();
+      console.log(sessionRedirect)
       if (sessionRedirect) {
         replaceState({ nextPathname: nextState.location.pathname }, sessionRedirect);
         return;
@@ -23,7 +34,7 @@ export const requireAuth = flux => (nextState, replaceState) => {
         }
         return obj;
       }, {});
-
+      console.log(routeVal.path)
       if (routeVal) {
         replaceState({ nextPathname: nextState.location.pathname }, routeVal.path);
         return;
@@ -35,9 +46,10 @@ export const requireAuth = flux => (nextState, replaceState) => {
 export const checkLoggedIn = flux => (nextState, replaceState) => {
   const user = flux.getStore('session').getCurrentUser();
   const { role, okrug_id } = user;
-  console.log('s', nextState.location.pathname )
+  console.log('checkLoggedIn', nexttate.location.pathname )
   if (flux.getStore('session').isLoggedIn() && role) {
-    console.log('sd')
+    console.log('checkLoggedIn', 'iAmIsLogined', nexttate.location.pathname )
+  
     if (['dispatcher', 'master'].indexOf(role) > -1 && okrug_id === null) {
       replaceState({}, '/dashboard');
     } else {

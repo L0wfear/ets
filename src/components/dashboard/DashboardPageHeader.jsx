1c109a4747e5d0f497d9e3730f41acda172b6e4f
Vladimir Leshchev <vokinda@yandex.ru>
2017-12-02 01:13:10 +0300

    DITETSSUP-632 Adds get moscow time from backend #3

--- src/components/dashboard/DashboardPageHeader.jsx
+++ src/components/dashboard/DashboardPageHeader.jsx
@@ -9,19 +9,31 @@ import Div from 'components/ui/Div.jsx';
 export default class DashboardPageHeader extends React.Component {
 
   state = {
-    time: moment().format('HH:mm:ss'),
-    date: moment().format('DD MMMM YYYY'),
+    time: null,
+    date: null,
   }
 
   componentDidMount() {
     moment.locale('ru');
     this.context.flux.getActions('dashboard').getMoscowTime().then(({ date }) => {
       this.timeInterval = setInterval(this.updateClock.bind(this), 1000);
-      this.dateInterval = setInterval(this.updateDate.bind(this), 1000 * 60);
 
       this.setState({
         time: moment(date).format('HH:mm:ss'),
         date: moment(date).format('DD MMMM YYYY'),
+        prevDate: date,
+        backDate: date,
+      });
+    }).catch(() => {
+      this.timeInterval = setInterval(this.updateClock.bind(this), 1000);
+
+      const date = new Date();
+
+      this.setState({
+        time: moment(date).format('HH:mm:ss'),
+        date: moment(date).format('DD MMMM YYYY'),
+        prevDate: date,
+        backDate: null,
       });
     });
     this.timeInterval = setInterval(this.updateClock.bind(this), 1000);
@@ -34,15 +46,12 @@ export default class DashboardPageHeader extends React.Component {
   }
 
   updateClock() {
-    const time = moment().format('HH:mm:ss');
-    this.setState({ time });
-  }
+    const prevDate = moment(this.state.prevDate).add(1, 'seconds');
+    const time = prevDate.format('HH:mm:ss');
+    const date = prevDate.format('DD MMMM YYYY');
 
-  updateDate() {
-    const date = moment().format('DD MMMM YYYY');
-    this.setState({ date });
+    this.setState({ prevDate, time, date });
   }
-
   render() {
     return (
       <Row>

eaec35417f42d60140e6dbe5c029ebcc1d36cfce
Evgenii Rabotnikov <evgenii.rabotnikov@gost-group.com>
2018-01-26 14:44:46 +0300

    DITETS-2922 Changes a path to the leakIcon

--- src/constants/leakIcon.js
+++ src/constants/leakIcon.js
@@ -1,14 +1,20 @@
 // тут задаются стили иконок пунктов слива топлива
+
+import leakIconSmall from 'assets/icons/leaks/oil-02(small).png';
+
+const leakImage = new Image();
+leakImage.src = leakIconSmall;
+
 export const leakIcon = {
   'geoobject': new ol.style.Style({
     image: new ol.style.Icon({
-      src: 'src/assets/icons/leaks/oil-02(small).png',
+      src: leakImage.src,
       offset: [0, 0],
     }),
   }),
   'geoobject-selected': new ol.style.Style({
     image: new ol.style.Icon({
-      src: 'src/assets/icons/leaks/oil-02(small).png',
+      src: leakImage.src,
       offset: [0, 0],
       opacity: 0.6,
     }),

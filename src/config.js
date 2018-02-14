6a66234490bf925053f4c2b1f55ee7c049a03f01
Evgenii Rabotnikov <evgenii.rabotnikov@gost-group.com>
2018-02-14 17:38:50 +0300

    DITETS-3004 Changes The variable for the protocol before the domain name for different stands

--- src/config.js
+++ src/config.js
@@ -1,28 +1,31 @@
-const PROTO_FOR_PROD = (window.location.host === 'localhost:3000') ? 'http' : window.location.protocol;
-const PROTO_FOR_STAGE = (window.location.host === 'localhost:3000') ? 'https' : window.location.protocol;
-console.log('PROTO_FOR_STAGE', PROTO_FOR_STAGE);
-const PROTO_FOR_DEV = (window.location.host === 'localhost:3000') ? 'http' : window.location.protocol;
+const PROTO_FOR_PROD = (window.location.host === 'localhost:3000') ? 'http:' : window.location.protocol;
+const PROTO_FOR_STAGE = (window.location.host === 'localhost:3000') ? 'https:' : window.location.protocol;
+const PROTO_FOR_DEV = (window.location.host === 'localhost:3000') ? 'http:' : window.location.protocol;
+
+const DOMAIN_FOR_PROD = (PROTO_FOR_PROD === 'http:') ? 'ets.tech.mos.ru' : 'ets.mos.ru';
+const DOMAIN_FOR_STAGE = (PROTO_FOR_STAGE === 'http:') ? 'ets.tech.mos.ru' : 'ets.mos.ru';
+const DOMAIN_FOR_DEV = 'dev-ets.gost-group.com';
 
 const STAND = process.env.STAND;
-export const PROTO_FOR_ODS_MOS_RU = (STAND === 'prod') ? 'http' : (STAND === 'stage') ? 'https' : 'http';
-const WS_PROTO = `ws${/s/.test(window.location.protocol) ? 's' : ''}`;
+export const PROTO_FOR_ODS_MOS_RU = (STAND === 'prod') ? PROTO_FOR_PROD : (STAND === 'stage') ? PROTO_FOR_STAGE : 'http:';
+const WS_PROTO = `ws${/s/.test(window.location.protocol) ? 's' : ''}:`;
 
 const DOC_URL = {
-  dev: `${PROTO_FOR_DEV}://dev-ets.gost-group.com/docs/`,
-  stage: `${PROTO_FOR_STAGE}://ets.mos.ru/ets-stage/docs/`,
-  prod: `${PROTO_FOR_PROD}://ets.tech.mos.ru/ets-study/docs/`,
+  dev: `${PROTO_FOR_DEV}//${DOMAIN_FOR_DEV}/docs/`,
+  stage: `${PROTO_FOR_STAGE}//${DOMAIN_FOR_STAGE}/ets-stage/docs/`,
+  prod: `${PROTO_FOR_PROD}//${DOMAIN_FOR_PROD}/ets-study/docs/`,
 };
 
 const config = {
-  ws: `${WS_PROTO}://ods.mos.ru/ssd/city-dashboard/stream`,
-  images: `${PROTO_FOR_ODS_MOS_RU}://ods.mos.ru/ssd/ets/data/images/`,
+  ws: `${WS_PROTO}//ods.mos.ru/ssd/city-dashboard/stream`,
+  images: `${PROTO_FOR_ODS_MOS_RU}//ods.mos.ru/ssd/ets/data/images/`,
   docs: DOC_URL[process.env.STAND],
 };
 
 const STANDS = {
-  stage: `${PROTO_FOR_STAGE}://ets.mos.ru/ets-stage/services`,
-  prod: `${PROTO_FOR_PROD}://ets.tech.mos.ru/ets-study/services`,
-  dev: `${PROTO_FOR_DEV}://dev-ets.gost-group.com/services`,
+  stage: `${PROTO_FOR_STAGE}//${DOMAIN_FOR_STAGE}/ets-stage/services`,
+  prod: `${PROTO_FOR_PROD}//${DOMAIN_FOR_PROD}/ets-study/services`,
+  dev: `${PROTO_FOR_DEV}//${DOMAIN_FOR_DEV}/services`,
 };
 
 try {

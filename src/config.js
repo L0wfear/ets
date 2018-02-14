4295a7b574197958e9676b21f6e625fabe67ab9c
Evgenii Rabotnikov <evgenii.rabotnikov@gost-group.com>
2018-02-14 15:20:17 +0300

    DITETS-3004 Changes The variable for the protocol before the domain name for different stands

--- src/config.js
+++ src/config.js
@@ -1,29 +1,33 @@
-const PROTO = window.location.protocol;
+const PROTO_FOR_PROD = (window.location.host === 'localhost:3000') ? 'http:' : window.location.protocol;
+const PROTO_FOR_STAGE = (window.location.host === 'localhost:3000') ? 'https:' : window.location.protocol;
+const PROTO_FOR_DEV = (window.location.host === 'localhost:3000') ? 'http:' : window.location.protocol;
+
+const STAND = process.env.STAND;
+export const PROTO_FOR_ODS_MOS_RU = (STAND === 'prod') ? 'http:' : (STAND === 'stage') ? 'https:' : 'http:';
 const WS_PROTO = `ws${/s/.test(window.location.protocol) ? 's' : ''}:`;
 
 const DOC_URL = {
-  dev: `${PROTO}//dev-ets.gost-group.com/docs/`,
-  stage: `${PROTO}//ets.mos.ru/ets-stage/docs/`,
-  prod: `${PROTO}//ets.tech.mos.ru/ets-study/docs/`,
+  dev: `${PROTO_FOR_DEV}//dev-ets.gost-group.com/docs/`,
+  stage: `${PROTO_FOR_STAGE}//ets.mos.ru/ets-stage/docs/`,
+  prod: `${PROTO_FOR_PROD}//ets.tech.mos.ru/ets-study/docs/`,
 };
 
 const config = {
   ws: `${WS_PROTO}//ods.mos.ru/ssd/city-dashboard/stream`,
-  images: `${PROTO}//ods.mos.ru/ssd/ets/data/images/`,
+  images: `${PROTO_FOR_ODS_MOS_RU}//ods.mos.ru/ssd/ets/data/images/`,
   docs: DOC_URL[process.env.STAND],
 };
 
 const STANDS = {
-  stage: `https://ets.mos.ru/ets-stage/services`,
-
-  prod: `http://ets.tech.mos.ru/ets-study/services`,
-  dev: `http://dev-ets.gost-group.com/services`,
+  stage: `${PROTO_FOR_STAGE}//ets.mos.ru/ets-stage/services`,
+  prod: `${PROTO_FOR_PROD}//ets.tech.mos.ru/ets-study/services`,
+  dev: `${PROTO_FOR_DEV}//dev-ets.gost-group.com/services`,
 };
 
 try {
   const HOST = process.env.APIHOST;
-  const STAND = process.env.STAND;
   if (HOST) {
+    const PROTO = window.location.protocol;
     config.backend = `${PROTO}//${process.env.APIHOST}`;
   } else if (STAND) {
     config.backend = STANDS[STAND] || STANDS.dev;

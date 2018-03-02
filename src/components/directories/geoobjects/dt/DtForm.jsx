6c4eae623068511581949a60d47370d8d29e1ebc
Vladimir Leshchev <vokinda@yandex.ru>
2018-03-02 12:34:12 +0300

    DITETS-2973 Adds notification after change structure in car/dt/odh form

--- src/components/directories/geoobjects/dt/DtForm.jsx
+++ src/components/directories/geoobjects/dt/DtForm.jsx
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { Modal, Row, Col, Button } from 'react-bootstrap';
 import ModalBody from 'components/ui/Modal';
 import Field from 'components/ui/Field.jsx';
 import Form from 'components/compositions/Form.jsx';
 import { connectToStores } from 'utils/decorators';
 import { defaultSelectListMapper } from 'components/ui/input/EtsSelect';
+import { changeCompanyStructureIdNotyfication } from 'utils/notifications';
+
+const STATIC_VAL = {
+  type: 'string',
+  readOnly: true,
+};
+const INPUT_VAL = {
+  company_structure_id: 'custom',
+};
 
 @connectToStores(['geoObjects', 'companyStructure'])
 export default class DtForm extends Form {
+  handleChange = (key, value) => {
+    if (key === 'company_structure_id') {
+      global.NOTIFICATION_SYSTEM.notify(changeCompanyStructureIdNotyfication);
+    }
+
+    super.handleChange(key, value);
+  }
+
   render() {
     const [state, meta] = [this.props.formState, this.props.formMeta];
 
-    const { companyStructureList = [] } = this.state;
+    const { companyStructureList = [] } = this.props;
     const COMPANY_ELEMENTS = companyStructureList.map(defaultSelectListMapper);
-
-    const INPUT_VAL = {
-      company_structure_name: {
-        type: 'select',
-        options: COMPANY_ELEMENTS,
-        emptyValue: null,
-        onChange: (...arg) => this.handleChange('company_structure_id', ...arg),
-      },
-    };
-    const STATIC_VAL = {
-      type: 'string',
-      readOnly: true,
-    };
 
     return (
       <Modal {...this.props} backdrop="static">
@@ -36,16 +40,29 @@ export default class DtForm extends Form {
 
         <ModalBody>
           {meta.cols.map(d =>
-            <Row key={d.name}>
-              <Col md={12}>
-                <Field
-                  label={d.displayName}
-                  value={state[d.name]}
-                  {...(d.name in INPUT_VAL ? INPUT_VAL[d.name] : STATIC_VAL)}
-                />
-              </Col>
-            </Row>
+            !INPUT_VAL[d.name] &&
+              <Row key={d.name}>
+                <Col md={12}>
+                  <Field
+                    label={d.displayName}
+                    value={state[d.name]}
+                    {...STATIC_VAL}
+                  />
+                </Col>
+              </Row>
           )}
+          <Row>
+            <Col md={12}>
+              <Field
+                type="select"
+                label={'Подразделение'}
+                value={state.company_structure_id}
+                options={COMPANY_ELEMENTS}
+                emptyValue={null}
+                onChange={(...arg) => this.handleChange('company_structure_id', ...arg)}
+              />
+            </Col>
+          </Row>
         </ModalBody>
 
         <Modal.Footer>

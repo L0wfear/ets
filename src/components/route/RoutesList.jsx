32bec9ffeaa68f3f52f344a37acb2d4d3057e52b
Evgenii Rabotnikov <evgenii.rabotnikov@gost-group.com>
2018-04-03 16:01:43 +0300

    DITETS-3226 Adds a utils.js and some functions have moved from the MissionFormWra.jsx to the utils.js

--- src/components/route/RoutesList.jsx
+++ src/components/route/RoutesList.jsx
@@ -8,32 +8,9 @@ import * as queryString from 'query-string';
 import Div from 'components/ui/Div.jsx';
 import Filter from 'components/ui/table/filter/Filter.jsx';
 import FilterButton from 'components/ui/table/filter/FilterButton.jsx';
+import { getTypeRoute, makeRoutesListForRender } from 'components/route/utils/utils.js';
 import RouteInfo from './RouteInfo.jsx';
 import RouteFormWrap from './RouteFormWrap.jsx';
-
-const getTypeRoute = (type) => {
-  switch (type) {
-    case 'mixed':
-    case 'simple':
-    case 'vector':
-      return 'Маршруты по ОДХ';
-    case 'simple_dt':
-      return 'Маршруты по ДТ';
-    case 'points':
-      return 'Маршруты по пунктам назначения';
-    default:
-      return 'error';
-  }
-};
-
-const makeRoutesListForRender = (routesListFromStore, technicalOperationsList, STRUCTURES) => {
-  routesListFromStore.forEach((r) => {
-    r.technical_operation_name = _.get(technicalOperationsList.find(t => t.id === r.technical_operation_id), 'name');
-    r.structure_name = _.get(STRUCTURES.find(t => t.value === r.structure_id), 'label');
-    r.type_name = getTypeRoute(r.type);
-  });
-  return routesListFromStore;
-};
 
 class RoutesList extends Component {
 

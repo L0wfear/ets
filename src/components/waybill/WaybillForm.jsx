426c98af2bb46a9adb2d02d807189a591200aa62
Evgenii Rabotnikov <evgenii.rabotnikov@gost-group.com>
2018-04-13 12:25:47 +0300

    Delete one request

--- src/components/waybill/WaybillForm.jsx
+++ src/components/waybill/WaybillForm.jsx
@@ -203,8 +203,7 @@ class WaybillForm extends Form {
       const operations = _.filter(fuelOperationsResponse.result, op => _.find(fuelRates, fr => fr.operation_id === op.id));
       const equipmentFuelRatesResponse = await flux.getActions('fuelRates').getEquipmentFuelRatesByCarModel({ car_id: formState.car_id, datetime: formState.date_create })
       const equipmentFuelRates = equipmentFuelRatesResponse.result.map(({ operation_id, rate_on_date }) => ({ operation_id, rate_on_date }));
-      const equipmentFuelOperations = await flux.getActions('fuelRates').getFuelOperations();
-      const equipmentOperations = _.filter(equipmentFuelOperations.result, op => _.find(equipmentFuelRates, fr => fr.operation_id === op.id));
+      const equipmentOperations = _.filter(fuelOperationsResponse.result, op => _.find(equipmentFuelRates, fr => fr.operation_id === op.id));
       this.setState({ fuelRates, operations, fuel_correction_rate, equipmentFuelRates, equipmentOperations });
       this.getCarDistance(formState);
     } else if (formState.status === 'closed') {

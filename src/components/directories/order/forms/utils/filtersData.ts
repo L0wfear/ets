b7837e365536302ba9f1aa6be3362232d6182f80
Vladimir Leshchev <vokinda@yandex.ru>
2018-04-18 08:14:49 +0300

    DITETS-3331 Changes getFilterDateOrder (now multi template by norm_id)

--- src/components/directories/order/forms/utils/filtersData.ts
+++ src/components/directories/order/forms/utils/filtersData.ts
@@ -12,12 +12,25 @@ export const getFilterDateOrder = (technical_operations, { order_date, order_dat
     } = to;
 
     if (diffDates(new Date(), date_to || order_date_to) < 0) {
-      newObj[norm_id] = {
-        date_to: date_to || order_date_to,
-        date_from: date_from || order_date,
-        num_exec,
-        order_operation_id,
-      };
+      if (!newObj[norm_id]) {
+        newObj[norm_id] = [{
+          date_to: date_to || order_date_to,
+          date_from: date_from || order_date,
+          num_exec,
+          order_operation_id,
+        }];
+      } else {
+        const newNormData = {
+          date_to: date_to || order_date_to,
+          date_from: date_from || order_date,
+          num_exec,
+          order_operation_id,
+        };
+
+        if (newObj[norm_id].some(({ date_to, date_from }) => date_to !== newNormData.date_to || date_from !== newNormData.date_from)) {
+          newObj[norm_id].push(newNormData);
+        }
+      }
     }
 
     return newObj;
@@ -26,23 +39,24 @@ export const getFilterDateOrder = (technical_operations, { order_date, order_dat
 export const getMissionListByFilter = (missionsList, filterData, typeClick) =>
   missionsList.reduce((arr, m) => {
     const { norm_id, passes_count} = m;
-
     if (filterData[norm_id]) {
-      const {
-        date_to,
-        date_from,
-        num_exec,
-        order_operation_id,
-      } = filterData[norm_id];
-
-      if ((typeClick === typeTemplate.missionDutyTemplate || passes_count <= num_exec) && diffDates(new Date(), date_to, 'minutes') < 0) {
-          arr.push({
-          ...m,
+      filterData[norm_id].forEach((toData) => {
+        const {
           date_to,
           date_from,
+          num_exec,
           order_operation_id,
-        });
-      }
+        } = toData;
+
+        if ((typeClick === typeTemplate.missionDutyTemplate || passes_count <= num_exec) && diffDates(new Date(), date_to, 'minutes') < 0) {
+            arr.push({
+            ...m,
+            date_to,
+            date_from,
+            order_operation_id,
+          });
+        }
+      })
     }
 
     return arr;

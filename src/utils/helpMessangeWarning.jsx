7cfab13ad587a8f71d345cf0f1e696fd05c8b5ea
Vladimir Leshchev <vokinda@yandex.ru>
2018-04-19 10:12:56 +0300

    Revert "DITETS-1154 Changes makeReactMessange (with '\n')"
    This reverts commit efaab906771c4ee16ba610b2189b2a1ebb5d01c2.

--- src/utils/helpMessangeWarning.jsx
+++ src/utils/helpMessangeWarning.jsx
@@ -1,24 +1,19 @@
 import * as React from 'react';
 
-export const makeReactMessange = message => (
-  <div>
-    {
-      message.split('\n').map((value, indexRow) => (
-        <div key={indexRow}>
-          {
-            value.split('[href]').map((partMessange, indexCol) => {
-              if (indexCol % 2 === 0) {
-                return <span key={`${indexRow}/${indexCol}`}>{partMessange}</span>;
-              }
-              return <a key={`${indexRow}/${indexCol}`}href={partMessange}>{partMessange}</a>;
-            })
-          }
-        </div>
-        )
-      )
-    }
-  </div>
-);
+export const makeReactMessange = (m) => {
+  const partM = m.split('[href]');
+
+  return (
+    <div>
+      {partM.map((om, i) => {
+        if (i % 2 === 0) {
+          return <span>{om}</span>;
+        }
+        return <a href={om}>{om}</a>;
+      })}
+    </div>
+  );
+};
 
 export default{
   makeReactMessange,

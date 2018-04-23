ea0bc82cca2cc9f403b628f19339c1d2a5306493
Vladimir Leshchev <vokinda@yandex.ru>
2018-04-23 13:55:27 +0300

    DITETSSUP-1184 Добавил метод родителю на который ссылается отнаследованная карточка

--- src/components/missions/mission_template/MissionTemplateFormWrap.jsx
+++ src/components/missions/mission_template/MissionTemplateFormWrap.jsx
@@ -183,6 +183,27 @@ export default class MissionFormWrap extends FormWrap {
     }
   }
 
+  handlMultiFormStateChange = (changesObj) => {
+    let { formErrors } = this.state;
+    const { formState } = this.state;
+
+    Object.entries(changesObj).forEach(([field, e]) => {
+      const value = e !== undefined && e !== null && !!e.target ? e.target.value : e;
+      console.info('Form changed', field, value);
+      formState[field] = value;
+    });
+
+    const newState = {};
+    formErrors = this.validate(formState, formErrors);
+
+    newState.canSave = Object.values(formErrors).reduce((boolean, oneError) => boolean && !oneError, true);
+
+    newState.formState = formState;
+    newState.formErrors = formErrors;
+
+    this.setState(newState);
+  }
+
   render() {
     if (this.props.formType === 'ViewForm') {
       return (
@@ -194,6 +215,7 @@ export default class MissionFormWrap extends FormWrap {
             show={this.props.showForm}
             onHide={this.props.onFormHide}
             template
+            handleMultiFormChange={this.handlMultiFormStateChange}
             {...this.state}
           />
         </Div>

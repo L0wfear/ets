f639d163a2bd1a45212fa7cdeaee44fc04f8a7a2
Evgenii Rabotnikov <evgenii.rabotnikov@gost-group.com>
2018-05-10 12:48:16 +0300

    Fix a merge conflict

--- src/components/missions/mission/MissionsTable.jsx
+++ src/components/missions/mission/MissionsTable.jsx
@@ -11,10 +11,7 @@ export const getTableMeta = ({
   carsList = [],
   structures = [],
   technicalOperationsList = [],
-<<<<<<< HEAD
   municipalFacilityList = [],
-=======
->>>>>>> DITETSSUP-1216-zion_stage
 } = {}) => {
   const tableMeta = {
     cols: [
@@ -134,7 +131,6 @@ export const getTableMeta = ({
         sort: {
           serverFieldName: 'technical_operation_name',
         },
-<<<<<<< HEAD
         filter: {
           type: 'multiselect',
           options: technicalOperationsList.map(operation => ({ value: operation.id, label: operation.name })),
@@ -148,11 +144,6 @@ export const getTableMeta = ({
         filter: {
           type: 'multiselect',
           options: municipalFacilityList.map(({ municipal_facility_id, municipal_facility_name }) => ({ value: municipal_facility_id, label: municipal_facility_name })),
-=======
-        filter: {
-          type: 'multiselect',
-          options: technicalOperationsList.map(operation => ({ value: operation.id, label: operation.name })),
->>>>>>> DITETSSUP-1216-zion_stage
         },
       },
       {

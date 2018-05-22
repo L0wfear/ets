d256107ec3dce43d9a9f795e8788078e2a5b351b
Vladimir Leshchev <vokinda@yandex.ru>
2018-05-22 10:46:42 +0300

    DITETS-3465 ПОправил отображение спинеров на ПЛ

--- src/components/waybill/WaybillForm.jsx
+++ src/components/waybill/WaybillForm.jsx
@@ -203,15 +203,16 @@ class WaybillForm extends Form {
       return;
     }
     const car = _.find(this.props.carsList, c => c.asuods_id === formState.car_id) || {};
-    loadingFields.distance = true;
-    loadingFields.consumption = true;
-    this.setState({ loadingFields });
     const {
       fact_departure_date,
       fact_arrival_date,
     } = formState;
 
     if (car.gps_code && fact_departure_date && fact_arrival_date && diffDates(fact_arrival_date, fact_departure_date) > 0) {
+      loadingFields.distance = true;
+      loadingFields.consumption = true;
+      this.setState({ loadingFields });
+
       flux.getActions('cars').getInfoFromCar(car.gps_code, fact_departure_date, fact_arrival_date)
         .then(({ distance, consumption }) => {
           this.props.handleFormChange('distance', distance);

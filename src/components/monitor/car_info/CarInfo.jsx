42263a6673edc9883497465a59078f961e46d97a
vredsun <Vladimir>
2018-06-13 07:50:13 +0300

    DITETSSUP-1321 Изменил проверку количества дней для загрузки трека (10 дней)

--- src/components/monitor/car_info/CarInfo.jsx
+++ src/components/monitor/car_info/CarInfo.jsx
@@ -298,7 +298,7 @@ export default class CarInfo extends Component {
     changesState.from_dt = changesState.from_dt_;
     changesState.to_dt = changesState.to_dt_;
 
-    if (diffDates(changesState.to_dt_, changesState.from_dt_, 'days') > 3) {
+    if (diffDates(changesState.to_dt_, changesState.from_dt_, 'days') > 10) {
       changesState.errorDates = 'Период формирования трека не должен превышать трое суток';
     } else {
       changesState.errorDates = '';

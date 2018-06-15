39e1b98c579e143432e371c1414c1874fc6324ae
vredsun <Vladimir>
2018-06-15 09:38:21 +0300

    DITETS-3456 Добавил кнопку для возвращения на роль Город

--- src/components/nav-item-role/NavItemRole.tsx
+++ src/components/nav-item-role/NavItemRole.tsx
@@ -9,7 +9,7 @@ class NavItemRole extends React.Component<any, any> {
     return (
       this.props.isGlavControl && this.props.location.pathname !== '/change-company'
       ?
-      <NavItem className={'company-switcher'}>
+      <NavItem id="change-company" className={'company-switcher'}>
         <span className={'company-switcher-label'}>Текущая организация:</span>
         <CompanyOptions history={this.props.history} location={this.props.location} />
       </NavItem>

3c52426ccc7120810469aa86bbb6cca84a99664c
vredsun <Vladimir>
2018-06-26 10:23:01 +0300

    DITETS-1154 Some fix

--- src/components/map/Track.js
+++ src/components/map/Track.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-
 import { get, pick, toArray } from 'lodash';
 import insider from 'point-in-polygon';
 
+import { SpanTitle, ColorLegend } from 'components/map/syled/styled';
 import { getStartOfToday, makeDate, makeTime, secondsToTime } from 'utils/dates';
 import { swapCoords, roundCoordinates } from 'utils/geo';
 import { isEmpty, hexToRgba } from 'utils/functions';
@@ -13,14 +12,6 @@ import { getTrackPointByColor } from 'assets/icons/track/points.js';
 import ParkingIcon from 'assets/icons/track/parking.svg';
 import FuelIcon1 from 'assets/icons/track/oil-01.png';
 import FuelIcon2 from 'assets/icons/track/oil-02.png';
-
-const SpanTitle = styled.span`
-  margin-left: 15px;
-`;
-
-const ColorLegend = styled.div`
-  background-color: ${({ color }) => color};
-`;
 
 const checkPointInGeometry = (point, geometry) => {
   return {
---
+++ src/components/map/syled/styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SpanTitle = styled.span`
+  margin-left: 15px;
+`;
+
+export const ColorLegend = styled.div`
+  background-color: ${({ color }) => color};
+`;

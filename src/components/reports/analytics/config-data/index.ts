dd3674bb285c6220e174bb7a0971651c0ac4211d
vredsun <Vladimir>
2018-07-17 12:11:03 +0300

    DITETS-3880 Поправил path страницы Аналити

--- src/components/reports/analytics/config-data/index.ts
+++ src/components/reports/analytics/config-data/index.ts
@@ -2,7 +2,7 @@ import permissions from 'components/reports/analytics/config-data/permissions';
 import components from 'components/reports/analytics/config-data/components';
 
 export default {
-  path: '/efficiency',
+  path: '/analytics',
   title: 'Аналитика',
   entyity: 'analytical_reports',
   noDotList: false,

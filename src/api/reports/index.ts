42ec6a40ece89579c5b0bb66acdf54e63556f97d
vredsun <Vladimir>
2018-07-19 16:50:09 +0300

    Cherry-pick  DITETS-3805 into release_old

--- src/api/reports/index.ts
+++ src/api/reports/index.ts
@@ -3,12 +3,14 @@ import ETS_API_FACTORY from './../EtsAPIServiceFactory';
 export const MissionProgressReportService = ETS_API_FACTORY.createApiServiceAdapter('mission_progress_report', {});
 export const BrigadeEfficiencyReport = ETS_API_FACTORY.createApiServiceAdapter('reports/efficiency/brigade', {});
 export const EmployeeEfficiencyReport = ETS_API_FACTORY.createApiServiceAdapter('reports/efficiency/employee', {});
+
 // временно
 export const FuelReportService = ETS_API_FACTORY.createApiServiceAdapter('fuel_consumption_new_report');
 export const FuelSummaryReportService = ETS_API_FACTORY.createApiServiceAdapter('fuel_consumption_summary_report', {});
 export const DailyCleaningReportsServiceETS = ETS_API_FACTORY.createApiServiceAdapter('cleaning_status_report', {});
 export const MissionReportsService = ETS_API_FACTORY.createApiServiceAdapter('car_travel_report', {});
 export const CarConditionReport = ETS_API_FACTORY.createApiServiceAdapter('car_condition_report', {});
+export const CarDowntimeAndOvermileageReport = ETS_API_FACTORY.createApiServiceAdapter('car_downtime_and_overmileage_report', {});
 
 export const DailyCleaningReportsServiceCAFAP = ETS_API_FACTORY.createApiServiceAdapter(
   'cleaning_status_cafap_report', {},

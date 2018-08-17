69012088df6bd0fb6eebee53f2d2a26e82924edc
vredsun <Vladimir>
2018-08-17 09:12:03 +0300

    Вернул видимость Отчёта Время движения ТС

--- src/components/reports/operational/car-movement-time-report/config-data/permissions.ts
+++ src/components/reports/operational/car-movement-time-report/config-data/permissions.ts
@@ -1,5 +1,5 @@
 export default {
-  list: 'car_movement_time_report.false',
+  list: 'car_movement_time_report.list',
   create: 'car_movement_time_report.create',
   read: 'car_movement_time_report.read',
   update: 'car_movement_time_report.update',

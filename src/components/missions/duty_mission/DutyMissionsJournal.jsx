add888102dee4e7b2989c7f596561f5000dcb774
MrrAmiD <nadymov2910@gmail.com>
2018-09-21 14:58:28 +0300

    DITETS-4300 Пофиксил баг с отображением лодера "Загрузка", исправил ошибку с удалением записей

--- src/components/missions/duty_mission/DutyMissionsJournal.jsx
+++ src/components/missions/duty_mission/DutyMissionsJournal.jsx
@@ -141,7 +141,7 @@ export default class DutyMissionsJournal extends CheckableElementsList {
         checkedElements: {},
         selectedElement: null,
       });
-    }).catch(({ errorIsShow }) => !errorIsShow && global.NOTIFICATION_SYSTEM.notify(getWarningNotification('Удалились только задания со статусом "Не назначено"!')));
+    }).catch(({ errorIsShow }) => global.NOTIFICATION_SYSTEM.notify(getWarningNotification('Удалились только задания со статусом "Не назначено"! 2')));
   }
 
   completeCheckedElements = async () => {
@@ -208,8 +208,8 @@ export default class DutyMissionsJournal extends CheckableElementsList {
 
       try {
         await Promise.all(allQuerys);
-      } catch ({ errorIsShow }) {
-        !errorIsShow && global.NOTIFICATION_SYSTEM.notify(getWarningNotification('Удалились только задания со статусом "Не назначено"!'));
+      } catch (e) {
+        global.NOTIFICATION_SYSTEM.notify(getWarningNotification('Удалились только задания со статусом "Не назначено"!'));
       }
 
       this.refreshList(this.state);

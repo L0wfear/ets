cab6eaf5a3c7f398e7e848630519c7d2186729e3
vredsun <Vladimir>
2018-10-02 13:04:44 +0300

    Вернул конфиг для разработки на место на место

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,43 +1,46 @@
 const path = require('path');
 const webpack = require('webpack');
-const notifyStats = require('./utils/notifyStats');
-const CleanPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const versionUtils = require('./utils/version');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const stand = process.env.STAND || 'production';
-const useSourceMaps = process.env.USE_SOURCE_MAPS || false;
+const notifyStats = require('./utils/notifyStats');
+const version = require('./utils/getVersion');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const host = 'localhost';
+const port = 3000;
+
+const stand = process.env.STAND || 'development';
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'eval',
   context: path.resolve(__dirname, '..'),
   entry: {
     'app': [
-      './src/index.js'
+      'webpack-dev-server/client?http://' + host + ':' + port,
+      'webpack/hot/only-dev-server',
+      './src/index.js',
+      './src/assets/main.scss'
     ],
   },
   output: {
     path: path.join(__dirname, '..', 'dist'),
-    filename: '[name].[hash].js',
-    /* если оставить publicPath то HtmlWebpackPlugin будет вставлять скрипты со ссылкой на него
-     * а нам это не нужно т.к. index.html генерится в dist
-     */
-    // publicPath: '/dist/',
+    publicPath: '/dist/',
+    filename: '[name].js',
   },
   module: {
     rules: [
-      { 
-        test: /\.jsx?$/,
+      {
+        test: /\.jsx?$/, 
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: [
+          'react-hot-loader',
+          'babel-loader',
+        ],
       },
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: [
-          'babel-loader',
+          'react-hot-loader',
+          // 'babel-loader',
           {
             loader: 'ts-loader',
             options: {
@@ -47,78 +50,17 @@ module.exports = {
           },
         ],
       },
-      { 
-        test: /\.hbs?$/,
-        use: 'handlebars-loader'
-      },
-      { 
-        test: /\.(png|jpe?g|gif)$/,
+      {
+        test: /\.(jpe?g|png|gif)$/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 1000000,
-              name: 'images/[name].[ext]',
+              mimetype: 'images/[name].[ext]',
             },
-          },
+          }
         ],
-      },
-      {
-        test: /\.(eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
-              name: 'fonts/[name].[ext]',
-            },
-          },
-        ],
-      },
-      {
-        test: /^((?!\.module).)*\.s?css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'resolve-url-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: /\.module\.s?css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 2,
-                localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
-              }
-            },
-            'resolve-url-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ]
-        }),
-      },
-      {
-        test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        use: [
-          'file-loader',
-        ]
       },
       {
         test: /\.svg/,
@@ -130,6 +72,84 @@ module.exports = {
             }
           }
       },
+      {
+        test: /\.(eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              mimetype: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+      { 
+        test: /^((?!\.module).)*\.s?css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'resolve-url-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.module\.s?css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 2,
+              localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        use: [
+          'file-loader',
+        ]
+      },
+      {
+        test: /ol-base\.js/,
+        use: [
+          {
+            loader: 'imports-loader',
+            options: {
+              define: false,
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
@@ -138,35 +158,17 @@ module.exports = {
   },
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
-    new CleanPlugin(['dist'], {
-      root: path.resolve(__dirname, '..')
-    }),
-    new webpack.DefinePlugin({
-      __CLIENT__: true,
-      __SERVER__: false,
-      __DEVELOPMENT__: false,
-      __DEVTOOLS__: false
-    }),
-    // ignore dev config
-    new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
+    new webpack.HotModuleReplacementPlugin(),
     // set global vars
     new webpack.DefinePlugin({
       'process.env': {
         // Useful to reduce the size of client-side libraries, e.g. react
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         STAND: JSON.stringify(stand),
-        VERSION: JSON.stringify(versionUtils.version)
-      }
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        pure_funcs: ['console.log'],
-        warnings: false,
+        VERSION: JSON.stringify(version)
       },
-      sourceMap: useSourceMaps,
-      mangle: false,
     }),
+    new webpack.NoEmitOnErrorsPlugin(),
     new CopyWebpackPlugin([
       {
         from: path.join(__dirname, '..', 'src', 'assets', 'fonts'),
@@ -176,21 +178,14 @@ module.exports = {
         from: path.join(__dirname, '..', 'src', 'assets', 'images'),
         to: 'images'
       },
-      {
-        from: path.join(__dirname, '..', 'webpack', 'otherToDist', 'robots.txt'),
-      },
-      {
-        from: path.join(__dirname, '..', 'webpack', 'otherToDist', 'construct'),
-        to: 'construct'
-      },
     ]),
-    new ExtractTextPlugin('./css/[name].[hash].css'),
-    new HtmlWebpackPlugin({
-      title: 'ЕТС',
-      template: path.resolve(__dirname, 'templates', 'index.hbs')
+    new webpack.DefinePlugin({
+      __CLIENT__: true,
+      __SERVER__: false,
+      __DEVELOPMENT__: true,
     }),
-    function () {
+    function onDone() {
       this.plugin('done', notifyStats);
     },
-  ]
+  ],
 };

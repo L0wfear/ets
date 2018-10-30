71987469e7da4f200d5a4264711fd283d6f5b1c2
MrrAmiD <nadymov2910@gmail.com>
2018-10-30 10:24:28 +0300

    DITETSSUP-1709 Вместо рег. номера отображались id

--- src/components/directories/autobase/battery_registry/vehicle-block/table-schema.tsx
+++ src/components/directories/autobase/battery_registry/vehicle-block/table-schema.tsx
@@ -99,7 +99,7 @@ const PropOnDateRenderer: React.SFC<IPropsDataTableInputRenderer> = ({ value, on
   />;
 
 export const renderers: TRendererFunction = (props, onListItemChange) => {
-  const vehicleList = props.batteryAvailableCarList.map(({ car_id, gov_number }) => ({ label: gov_number, value: car_id }));
+  const vehicleList = [...props.batteryAvailableCarList, ...props.inputList].map(({ car_id, gov_number }) => ({ label: gov_number, value: car_id }));
 
   return {
     car_id: rowMeta =>

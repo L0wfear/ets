d6aeb43f5424acc63e5348c45400b81a96b22fe8
MrrAmiD <nadymov2910@gmail.com>
2018-11-01 18:06:00 +0300

    DITETSSUP-1724 Обновил хендлер для фильтров

--- src/stores/ObjectsStore.js
+++ src/stores/ObjectsStore.js
@@ -25,7 +25,7 @@ export default class ObjectsStore extends Store {
     this.register(carActions.getTrack, this.handleGetTrack);
 
     this.register(objectsActions.getCars, this.handleGetCars);
-    this.register(objectsActions.getSomeCars, this.handleGetCars);
+    this.register(objectsActions.getSomeCars, this.handleGetSomeCars);
     this.register(objectsActions.getModels, this.handleGetModels);
     this.register(objectsActions.getSpecialModels, this.handleGetSpecialModels);
     this.register(objectsActions.getTypes, this.handleGetTypes);
@@ -140,7 +140,7 @@ export default class ObjectsStore extends Store {
     this.setState({ companyStructureLinearList: companyStructureLinear });
   }
 
-  handleGetCars(cars) { // Обработчик используется для getSomeCars() и для getCars()
+  handleGetCars(cars) {
     const carsList = cars.result.map((c) => {
       const model = _.find(this.state.modelsList, m => m.id === c.model_id);
       c.model = model ? model.title : 'Н/Д';
@@ -150,6 +150,18 @@ export default class ObjectsStore extends Store {
     });
     const carsIndex = _.keyBy(carsList, 'asuods_id');
     this.setState({ carsList, carsIndex });
+  }
+
+  handleGetSomeCars(cars) {
+    const carsFilterList = cars.result.map((c) => {
+      const model = _.find(this.state.modelsList, m => m.id === c.model_id);
+      c.model = model ? model.title : 'Н/Д';
+      const type = _.find(this.state.typesList, t => t.asuods_id === c.type_id);
+      c.type = type ? type.title : 'Н/Д';
+      return c;
+    });
+    const carsIndex = _.keyBy(carsFilterList, 'asuods_id');
+    this.setState({ carsFilterList, carsIndex });
   }
 
   handleGetTrack(track) {

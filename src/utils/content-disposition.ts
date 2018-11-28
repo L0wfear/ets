2aec0f3bc6072d09b768a2ebe38c60651e61be1a
vredsun <Vladimir>
2018-11-28 17:28:49 +0300

    DITETS-4293 Русификация файлов

--- src/utils/content-disposition.ts
+++ src/utils/content-disposition.ts
@@ -1,11 +1,11 @@
-import utf8 from 'utf8';
-
 function parseFilename(contentDisposition) {
   let filename;
+
   try {
     const result = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(contentDisposition);
+
     if (result && result[1]) {
-      filename = utf8.decode(result[1]).replace(/"/g, '');
+      filename = decodeURIComponent(result[1]);
     }
   } catch (e) {
     filename = null;

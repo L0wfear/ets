32a4fe69b1d2d3b736598bc9d0390f99d932403e
vredsun <Vladimir>
2018-12-06 11:10:07 +0300

    DITETSSUP-1946 Добавил вывод чёрточек и фильтрацию опций в отчётах

--- src/components/reports/regulated/fuel_consumption_summary/report.tsx
+++ src/components/reports/regulated/fuel_consumption_summary/report.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { withProps } from 'recompose';
 
 import { IReportProps } from 'components/reports/@types/common.h';
@@ -6,13 +7,17 @@ import { exportable } from 'utils/decorators';
 import ReportContainer from 'components/reports/common/ReportContainer';
 import ReportHeader from './ReportHeader';
 
-const serviceUrl = 'fuel_consumption_new_report';
-const reportUrl = 'fuel-consumption-report';
-const serviceName = 'FuelReportService';
+const serviceUrl = 'fuel_consumption_summary_report';
+const reportUrl = 'fuel-consumption-summary-report';
+const serviceName = 'FuelSummaryReportService';
 
 const schemaMakers = {};
 
-const renderers = {};
+export const renderers = ({
+  okrug_name: ({ data }) => <div>{data || '-'}</div>,
+  district_name: ({ data }) => <div>{data || '-'}</div>,
+  structure_name: ({ data }) => <div>{data || '-'}</div>,
+});
 
 const tableProps = {
   rowNumberLabel: '№ п/п',
@@ -20,7 +25,7 @@ const tableProps = {
 };
 
 const reportProps: IReportProps = {
-  title: 'Отчёт по топливу',
+  title: 'Сводный отчет расхода топлива',
   serviceName,
   enumerated: true,
   tableProps,
@@ -29,7 +34,6 @@ const reportProps: IReportProps = {
   headerComponent: ReportHeader,
   renderers,
   schemaMakers,
-  notUseServerSummerTable: true,
 };
 
 const ExportableReportContainer = exportable({

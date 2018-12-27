2c6765cdb55689ac93f126c25c76b2d6018c4d17
vredsun <Vladimir>
2018-12-27 11:31:44 +0300

    DITETS-1154 Update develop

--- src/components/error_boundary_registry/ErrorBoundaryRegistry.tsx
+++ src/components/error_boundary_registry/ErrorBoundaryRegistry.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import * as Raven from 'raven-js';
-import { ErrorBoundaryRegistryContainer, ErrorCenter } from './styled/index';
+import { ErrorBoundaryRegistryContainer, ErorText, ErorTextTimeOut, ButtonRefreshRegistry } from './styled/index';
 
-class ErrorBoundaryRegistry extends React.Component<any, { hasError: boolean }> {
+class ErrorBoundaryRegistry extends React.Component<any, { hasError: boolean, countLeft: number, intervalId: any }> {
   state = {
     hasError: false,
+    countLeft: 5,
+    intervalId: null,
   };
 
   static getDerivedStateFromError(error) {
@@ -17,15 +19,55 @@ class ErrorBoundaryRegistry extends React.Component<any, { hasError: boolean }> 
     console.log(error); //tslint:disable-line
     console.log(info); //tslint:disable-line
     Raven.captureException(new Error(error));
+    this.setState({
+      intervalId: setInterval(this.updateCountLeft, 1000),
+    });
+  }
+
+  refreshNow = () => {
+    const { intervalId } = this.state;
+    clearInterval(intervalId);
+
+    this.setState({
+      hasError: false,
+      intervalId: null,
+      countLeft: 5,
+    });
+  }
+
+  updateCountLeft = () => {
+    this.setState((state) => {
+      const countLeft = state.countLeft - 1;
+
+      if (countLeft === 0) {
+        clearInterval(state.intervalId);
+        return {
+          hasError: false,
+          intervalId: null,
+          countLeft: 5,
+        };
+      }
+      return {
+        ...state,
+        countLeft,
+      };
+    });
   }
 
   render() {
+    const { countLeft } = this.state;
+
     return this.state.hasError
       ? (
         <ErrorBoundaryRegistryContainer>
-          <ErrorCenter>
+          <ErorText font_size={0} />
+          <ErorText font_size={40}>
             Произошла непредвиденная ошибка
-          </ErrorCenter>
+          </ErorText>
+          <ErorTextTimeOut font_size={30}>
+            {`Обновление через ${countLeft}`}
+            <ButtonRefreshRegistry onClick={this.refreshNow}>Обновить сейчас</ButtonRefreshRegistry>
+          </ErorTextTimeOut>
         </ErrorBoundaryRegistryContainer>
       )
       : (
--- src/components/error_boundary_registry/styled/index.ts
+++ src/components/error_boundary_registry/styled/index.ts
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
 import { EtsPageWrap } from 'global-styled/global-styled';
+import * as Button from 'react-bootstrap/lib/Button';
 
 export const ErrorBoundaryRegistryContainer = styled(EtsPageWrap)`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   height: 100%;
 `;
 
-export const ErrorCenter = styled.div`
-  font-size: 40px;
+export const ErorText = styled.div<{ font_size: number }>`
+  font-size: ${({ font_size }) => font_size}px;
+`;
+
+export const ErorTextTimeOut = styled(ErorText)`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`;
+
+export const ButtonRefreshRegistry = styled(Button)`
+  &&& {
+    font-size: 20px;
+    margin: 0 10px;
+  }
 `;

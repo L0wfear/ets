6f2c0b3c7ef89c12230e575c79b399b4dc39cf4c
vredsun <Vladimir>
2019-01-14 22:09:42 +0300

    DITETS-5022 Изменил отображение и выбор операций в таксировке ПЛ

--- src/components/waybill/utils.ts
+++ src/components/waybill/utils.ts
@@ -186,19 +186,13 @@ export const getFuelRatesByCarModel = (action, { car_id, date_create: datetime }
   action({ car_id, datetime })
     .then(({ result: fuelRatesList}) => (
       fuelRatesList.filter(({ winter_rate, summer_rate }) => currentSeason === 'winter' ? isNumber(winter_rate) : isNumber(summer_rate))
-    ))
-    .then((fuelRatesList) => ({
-      fuelRates: fuelRatesList.map(({ operation_id, rate_on_date }) => ({ operation_id, rate_on_date })),
-      fuelRatesIndex: fuelRatesList.reduce((newObj, { operation_id, ...other }) => ({ ...newObj, [operation_id]: { operation_id, ...other }}), {}),
-    }));
+    ));
+
 export const getEquipmentFuelRatesByCarModel = (action, { car_id, date_create: datetime }, currentSeason) =>
   action({ car_id, datetime })
     .then(({ result: equipmentFuelRatesList }) => (
       equipmentFuelRatesList.filter(({ winter_rate, summer_rate }) => currentSeason === 'winter' ? isNumber(winter_rate) : isNumber(summer_rate))
-    )).then((equipmentFuelRatesList) => ({
-      equipmentFuelRates: equipmentFuelRatesList.map(({ operation_id, rate_on_date }) => ({ operation_id, rate_on_date })),
-      equipmentFuelRatesIndex: equipmentFuelRatesList.reduce((newObj, { operation_id, ...other }) => ({ ...newObj, [operation_id]: { operation_id, ...other }}), {}),
-    }));
+    ));
 
 export const checkMissionSelectBeforeClose = (formState, missionsIndex, order_mission_source_id, orderAction) =>
   Promise.all<any>(

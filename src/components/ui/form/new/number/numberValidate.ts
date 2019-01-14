87056d10cf123d0102fbc133e5e99c27b3fcf253
vredsun <Vladimir>
2019-01-14 13:39:18 +0300

    DITETS-1154 Исправил валидацию чисел

--- src/components/ui/form/new/number/numberValidate.ts
+++ src/components/ui/form/new/number/numberValidate.ts
@@ -10,7 +10,7 @@ export const validateNumber = <F, P>(fieldData: NumberPropertie<F>, formState: F
     title,
   } = fieldData;
 
-  if (fieldData.required && !value && isNumber(value) ? value !== 0 : false) {
+  if (fieldData.required && isNullOrUndefined(value)) {
     return `Поле "${title}" должно быть заполнено`;
   }
 

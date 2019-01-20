ffcc87bfadcfd7dd6bb95b82bfdca55501d006ba
vredsun <Vladimir>
2019-01-20 09:37:47 +0300

    DITETS-1154 Убрал отслеживание функции getGeozoneByTypeWithGeometry

--- src/stores/GeoObjectsStore.js
+++ src/stores/GeoObjectsStore.js
@@ -22,38 +22,9 @@ export default class GeoObjectsStore extends Store {
 
     const geoObjectsActions = flux.getActions('geoObjects');
 
-    this.register(geoObjectsActions.getGeozoneByTypeWithGeometry, this.handleGetGeozonesByTypeWithGeometry);
     this.register(geoObjectsActions.getGeozoneByType, this.handleGetGeozonesByType);
 
     this.state = cloneDeep(initialState);
-  }
-
-  handleGetGeozonesByTypeWithGeometry(response) { // данные гео объектов, полученные с сервера, записываются в отдельные свойства this.state
-    const { data = {} } = response;
-    const { rows = [] } = data.result;
-    const type = response.type || rows[0].type;
-    const polys = {};
-    rows.forEach((geozone) => {
-      let shape = {};
-      try {
-        shape = JSON.parse(geozone.shape);
-      } catch (e) {
-        shape = geozone.shape;
-      }
-
-      geozone.featureType = type || geozone.type;
-      delete geozone.shape;
-      polys[geozone.global_id || geozone.id || geozone.sensor_id] = Object.assign({}, {
-        shape,
-        data: geozone,
-        state: 1,
-      });
-    });
-    const polysByType = `${type}Polys`;
-
-    this.setState({
-      [polysByType]: polys,
-    });
   }
 
   handleGetGeozonesByType(response) {

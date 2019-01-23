fd02ac58494375ecceecd4860096a14fc02869f6
mrAmiD <nadymov2910@gmail.com>
2019-01-23 13:14:46 +0300

    DITETSSUP-2042 В виджете "ТС в работе", подрубил поле "ТС, не передающие сигнал" к сокету

--- src/components/dashboard/menu/cards/car-in-work-overall/CarInWorkOverall.h.ts
+++ src/components/dashboard/menu/cards/car-in-work-overall/CarInWorkOverall.h.ts
@@ -5,6 +5,7 @@ import { InitialStateDashboard } from 'components/dashboard/redux-main/modules/d
 
 export type StatePropsCarInOveral = {
   items: InitialStateDashboard['car_in_work_overall']['data']['items'];
+  // carsByStatus: any; // заменить? поискать, может уже есть типчик
 };
 export type DispatchPropsCarInOveral = {
   setInfoData: (infoData: CarInWorkOverallInfoDataType) => any;
@@ -19,4 +20,7 @@ export type PropsCarInWorkOverall = (
 );
 
 export type StateCarInWorkOverall = {
+  ws: any;
+  carsTrackState: any;
+  countNotInTouch: number | {};
 };

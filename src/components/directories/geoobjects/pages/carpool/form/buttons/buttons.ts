69fbcd7638f5c7ebdae9e32b640b423904fccbdc
vredsun <Vladimir>
2019-01-28 16:08:07 +0300

    DITETS-1154 Remove old carpool

--- src/components/directories/geoobjects/pages/carpool/form/buttons/buttons.ts
+++
@@ -1,6 +0,0 @@
-import * as Button from 'react-bootstrap/lib/Button';
-import withRequirePermissionsNew from 'components/util/RequirePermissionsNewRedux';
-
-export const ButtonUpdateMainCarpool = withRequirePermissionsNew({
-  permissions: 'carpool.update',
-})(Button);

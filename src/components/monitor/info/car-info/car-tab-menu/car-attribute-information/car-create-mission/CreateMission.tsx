96c85b96c1e962ed2aa7123e2bb12153145d7cc1
vredsun <Vladimir>
2019-01-29 12:45:52 +0300

    DITETS-5082 Исправил создание задания с карты

--- src/components/monitor/info/car-info/car-tab-menu/car-attribute-information/car-create-mission/CreateMission.tsx
+++ src/components/monitor/info/car-info/car-tab-menu/car-attribute-information/car-create-mission/CreateMission.tsx
@@ -4,10 +4,13 @@ import MissionFormWrap from 'components/missions/mission/MissionFormWrap';
 import { connect } from 'react-redux';
 import { getDefaultMission } from 'stores/MissionsStore';
 import { FlexCenterButton } from 'components/monitor/info/car-info/car-tab-menu/car-attribute-information/car-create-mission/styled/index';
+import memoizeOne from 'memoize-one';
+import { fetchCarInfo } from 'components/monitor/info/car-info/redux-main/modules/actions-car-info';
 
 type PropsCarMissions = {
   gps_code: number;
   carActualGpsNumberIndex: any;
+  fetchMissionsData: any;
 };
 
 type defaultMissionI = {
@@ -23,50 +26,83 @@ type defaultMissionI = {
 type StateCarMissions = {
   showMissionFormWrap: boolean;
   element: any;
-  carActualGpsNumberArray: any[];
 };
 
 class CreateMission extends React.Component<PropsCarMissions, StateCarMissions> {
   state = {
     showMissionFormWrap: false,
     element: null,
-    carActualGpsNumberArray: [],
   };
 
-  showMissionFormWrap = () => {
-    this.setState({ showMissionFormWrap: true });
-  }
-  componentDidMount() {
-    const { gps_code, carActualGpsNumberIndex } = this.props;
+  handleClickCreateMission = () => {
+    const {
+      gps_code,
+      carActualGpsNumberIndex,
+    } = this.props;
+
     const carInfo = carActualGpsNumberIndex[gps_code];
-    const carActualGpsNumberArray = Object.values(carActualGpsNumberIndex);
+
     const defaultMission: defaultMissionI = getDefaultMission();
+
     const element = {
       car_id: carInfo.asuods_id,
       type_id: carInfo.type_id,
       ...defaultMission,
     };
+
     this.setState({
+      showMissionFormWrap: true,
       element,
-      carActualGpsNumberArray,
     });
   }
 
-  handleFormHideMissionFormWrap = () => {
-    this.setState({ showMissionFormWrap: false });
+  handleHideMissionForm = () => {
+    const {
+      gps_code,
+      carActualGpsNumberIndex,
+    } = this.props;
+
+    this.props.fetchMissionsData({
+      asuods_id: carActualGpsNumberIndex[gps_code].asuods_id,
+      gps_code,
+    });
+
+    this.setState({
+      showMissionFormWrap: false,
+      element: null,
+    });
   }
 
+  makeCarActualGpsNumberArray = (
+    memoizeOne(
+      (carActualGpsNumberIndex) => (
+        Object.values(carActualGpsNumberIndex)
+      ),
+    )
+  );
+
   render() {
-    const { element, carActualGpsNumberArray } = this.state;
+    const {
+      carActualGpsNumberIndex,
+    } = this.props;
+
+    const {
+      element,
+      showMissionFormWrap,
+    } = this.state;
+
+    const carActualGpsNumberArray = this.makeCarActualGpsNumberArray(
+      carActualGpsNumberIndex,
+    );
 
     return (
       <FlexCenterButton>
-        <ButtonCreateMission onClick={this.showMissionFormWrap} >
+        <ButtonCreateMission onClick={this.handleClickCreateMission} >
           Создать децентрализованное задание
         </ButtonCreateMission>
         <MissionFormWrap
-          onFormHide={this.handleFormHideMissionFormWrap}
-          showForm={this.state.showMissionFormWrap}
+          onFormHide={this.handleHideMissionForm}
+          showForm={showMissionFormWrap}
           element={element}
           carsList={carActualGpsNumberArray}
           fromWaybill
@@ -79,7 +115,18 @@ class CreateMission extends React.Component<PropsCarMissions, StateCarMissions> 
 const mapStateToProps = (state) => ({
   carActualGpsNumberIndex: state.monitorPage.carActualGpsNumberIndex,
 });
+const mapDispatchToProps = (dispatch) => ({
+  fetchMissionsData: (props) => (
+    dispatch(
+      fetchCarInfo({
+        asuods_id: props.asuods_id,
+        gps_code: props.gps_code,
+      }),
+    )
+  ),
+});
 
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
 ) (CreateMission);

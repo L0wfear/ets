b8bc97f1e453813e24730d8ea11047d35554a294
vredsun <Vladimir>
2019-02-11 01:18:18 +0300

    DITETS-1154 Add run eslint before commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,118 @@
-var path = require('path');
-
 module.exports = {
-    "extends": "airbnb",
-    "env": {
-        node: true,
-        browser: true
+  "env": {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  parser: "babel-eslint",
+  "globals": {
+    window: true,
+    document: true,
+    confirmDialog: true,
+    __DEVELOPMENT__: true,
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+    "jsx": true
+  },
+  "ecmaVersion": 2018,
+  "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "babel",
+  ],
+  "rules": {
+    "indent": [ //  табы
+      "error",
+      2,
+      {
+        SwitchCase: 1,
       },
-      "globals": {
-        window: true,
-        document: true,
-        confirmDialog: true,
-        __DEVELOPMENT__: true,
-      },
-      "parser": "babel-eslint",
-      "rules": {
-        "arrow-parens": ["error", "always"],
-        "strict": "off",
-        "no-tabs": "error",
-        "no-continue": "off",
-        "no-mixed-spaces-and-tabs": "off",
-        "no-confusing-arrow": "off",
-        "quote-props": "off", // временно
-        "no-underscore-dangle": ["error", { "allowAfterThis": true }],
-        "class-methods-use-this": "off",
-        "max-len": ["off", 80],
-        "camelcase": ["off"], // временно
-        "no-param-reassign": ["off"], // временно
-        "no-unused-expressions": ["error", { "allowShortCircuit": true }],
-        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
-        "jsx-a11y/no-static-element-interactions": ["off"],
-        // react
-        "react/forbid-prop-types": ["off"],
-        "react/destructuring-assignment": [false]
-      },
-    "settings": {
-        "import/resolver": {
-            "webpack": {
-                "config": path.resolve(__dirname, 'webpack', 'webpack.dev.js'),
-            },
-        },
-    },
+    ],
+    "no-multiple-empty-lines": [
+      "error",
+      { "max": 1, "maxEOF": 1 }
+    ],
+    "linebreak-style": [  // LF
+      "error",
+      "unix"
+    ],
+    "object-property-newline": [
+      1,
+      { "allowAllPropertiesOnSameLine": true }
+    ], // деструктуризация в строки
+    "quotes": [ //  кавычки
+      "error",
+      "single"
+    ],
+    "semi": [ // ; в конце каждой строки
+      "error",
+      "always"
+    ],
+    "arrow-parens": [ // аргументы анонимной всегда в скобки
+      "error",
+      "always",
+    ],
+    "no-tabs": "error",
+    "no-continue": "error",
+    "no-mixed-spaces-and-tabs": "error",
+    "no-confusing-arrow": [ // return или () в анонимной функции
+      1,
+      { "allowParens": false },
+    ],
+    "quote-props": [ // одинаковый формат имён у свойств объекта
+      "error",
+      "consistent-as-needed",
+    ],
+    "no-underscore-dangle": [ // обращение к "_скрытым" свойствам
+      "error",
+      { "allowAfterThis": true },
+    ],
+    "class-methods-use-this": 1,  // если у функции нет обращения к контексту, то такая функция должна быть статичной
+    "max-len": [
+      1,
+      130,
+    ],
+    "camelcase": [
+      1,
+      { ignoreDestructuring: true },
+    ],
+    "no-param-reassign": [ // скажем нет мутации
+      "error",
+      { "props": false },
+    ],
+    "no-unused-expressions": [
+      "error",
+      { "allowShortCircuit": true },
+    ],
+    "no-unused-vars": [
+      "error",
+      { "ignoreRestSiblings": true },
+    ],
+    "no-plusplus": [ // против a++
+      "error",
+      { "allowForLoopAfterthoughts": true },
+    ],
+    "object-curly-spacing": [
+      "error",
+      "always",
+    ],
+    "react/prop-types": [1],
+    "react/forbid-prop-types": [0],
+    "react/display-name": [0],
+    "react/destructuring-assignment": [0],
+    "babel/new-cap": 1,
+    "babel/camelcase": 1,
+    "babel/no-invalid-this": 1,
+    "babel/object-curly-spacing": 0,
+    "babel/quotes": 0,
+    "babel/semi": 1,
+    "babel/no-unused-expressions": 1,
+    "babel/valid-typeof": 1
+  },
 };

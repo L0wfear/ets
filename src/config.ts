40bd7b34806207a79201fc0a9b179fd8bf9b2dc1
vredsun <Vladimir>
2019-02-21 16:19:55 +0300

    DITETS-1154 Поправил конфиг сокета для  ets_stage

--- src/config.ts
+++ src/config.ts
@@ -61,14 +61,14 @@ const config = {
 const notification_config = {
   develop: {
     dev: 'ws://dev2-ets.gost-group.com/services/notification_ws',
-    gost_stage: 'wss://ets-stage.gost-group.com/services/notification_ws',
+    gost_stage: 'ws://ets-stage.gost-group.com/services/notification_ws',
     ets_test: 'wss://ets-test.mos.ru/services/notification_ws',
     ets_hotfix: 'wss://ets-hotfix.mos.ru/services/notification_ws',
     prod: 'wss://ets.mos.ru/services/notification_ws',
   },
   origin: {
     dev: `ws://${HOST}${PATHNAME}services/notification_ws`,
-    gost_stage: `wss://${HOST}${PATHNAME}services/notification_ws`,
+    gost_stage: `ws://${HOST}${PATHNAME}services/notification_ws`,
     ets_test: `wss://${HOST}${PATHNAME}services/notification_ws`,
     ets_hotfix: `wss://${HOST}${PATHNAME}services/notification_ws`,
     prod: `wss://${HOST}${PATHNAME}services/notification_ws`,

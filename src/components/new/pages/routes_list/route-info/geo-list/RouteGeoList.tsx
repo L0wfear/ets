fb671e0dd9258881216af03ccf898cf76d5b4c06
mrAmiD <nadymov2910@gmail.com>
2019-03-01 17:36:53 +0300

    DITETSSUP-1853 Доработка карточки задания

--- src/components/new/pages/routes_list/route-info/geo-list/RouteGeoList.tsx
+++ src/components/new/pages/routes_list/route-info/geo-list/RouteGeoList.tsx
@@ -35,11 +35,7 @@ const makeNameByProps = (type: PropsRouteGeoList['type']) => {
 class RouteGeoList extends React.PureComponent<PropsRouteGeoList, {}> {
   render() {
     const { props } = this;
-    const {
-      object_list,
-      draw_object_list,
-      makeFailList,
-    } = props;
+    const { object_list, draw_object_list, makeFailList } = props;
 
     const {
       nameObjectList,
@@ -52,100 +48,87 @@ class RouteGeoList extends React.PureComponent<PropsRouteGeoList, {}> {
       const objectIndex = keyBy(object_list, 'object_id');
       const drawObjectListIndex = keyBy(draw_object_list, 'object_id');
 
-      fail_list = Object.values(props.polys).filter(({ id }) => !objectIndex[id] && !drawObjectListIndex[id]);
+      fail_list = Object.values(props.polys).filter(
+        ({ id }) => !objectIndex[id] && !drawObjectListIndex[id],
+      );
     }
 
     return (
       <RouteGeoListContainer height={this.props.height}>
-        {
-          object_list && object_list.length
-          ? (
-            <div>
-              <NameListLineContainer>
-                <TitleList>{nameObjectList}</TitleList>
-                {
-                  object_list.map(({ type, ...data }, index) => {
-                    let title = data.name;
-                    let key = data.object_id;
+        {object_list && object_list.length ? (
+          <div>
+            <NameListLineContainer>
+              <TitleList>{nameObjectList}</TitleList>
+              {object_list.map(({ type, ...data }, index) => {
+                let title = data.name;
+                let key = data.object_id;
 
-                    if (type === 'dt' || type === 'odh') {
-                      title = `${title} (${data.state === polyState.SELECTED_IDLING ? 'холостой' : 'рабочий'} ход)`;
-                    }
-                    if (type === 'points') {
-                      const pnNumber = `Пункт назначения №${index + 1}`;
-                      if (title) {
-                        title = `${pnNumber} (${title})`;
-                      } else {
-                        title = pnNumber;
-                      }
-                      key = index;
-                    }
-
-                    return (
-                      <li key={key}>
-                        {title}
-                      </li>
-                    );
-                  })
+                if (type === 'dt' || type === 'odh') {
+                  title = `${title} (${
+                    data.state === polyState.SELECTED_IDLING
+                      ? 'холостой'
+                      : 'рабочий'
+                  } ход)`;
                 }
-              </NameListLineContainer>
-            </div>
-          )
-          : (
-            <DivNone />
-          )
-        }
-        {
-          draw_object_list && draw_object_list.length
-          ? (
-            <div>
-              <NameListLineContainer>
-                <TitleList>{nameDrawObjectList}</TitleList>
-                {
-                  uniqBy(draw_object_list, (o) => o.name + o.state).map(({type, ...data }, key) => {
-                    let title = data.name;
-
-                    if (type === 'dt' || type === 'odh') {
-                      title = `${title} (${data.state === polyState.SELECTED_IDLING ? 'холостой' : 'рабочий'} ход)`;
-                    }
-                    if (type === 'points') {
-                      title = `Пункт назначения №${key + 1} (${title})`;
-                    }
-
-                    return (
-                      <li key={key}>
-                        {title}
-                      </li>
-                    );
-                  })
+                if (type === 'points') {
+                  const pnNumber = `Пункт назначения №${index + 1}`;
+                  if (title) {
+                    title = `${pnNumber} (${title})`;
+                  } else {
+                    title = pnNumber;
+                  }
+                  key = index;
                 }
-              </NameListLineContainer>
-            </div>
-          )
-          : (
+
+                return <li key={key}>{title}</li>;
+              })}
+            </NameListLineContainer>
+          </div>
+        ) : (
+          <>
+            <TitleList>Объекты отсутствуют в маршруте</TitleList>
             <DivNone />
-          )
-        }
-        {
-          makeFailList && fail_list.length
-          ? (
-            <div>
-              <NameListLineContainer>
+          </>
+        )}
+        {draw_object_list && draw_object_list.length ? (
+          <div>
+            <NameListLineContainer>
+              <TitleList>{nameDrawObjectList}</TitleList>
+              {uniqBy(draw_object_list, (o) => o.name + o.state).map(
+                ({ type, ...data }, key) => {
+                  let title = data.name;
+
+                  if (type === 'dt' || type === 'odh') {
+                    title = `${title} (${
+                      data.state === polyState.SELECTED_IDLING
+                        ? 'холостой'
+                        : 'рабочий'
+                    } ход)`;
+                  }
+                  if (type === 'points') {
+                    title = `Пункт назначения №${key + 1} (${title})`;
+                  }
+
+                  return <li key={key}>{title}</li>;
+                },
+              )}
+            </NameListLineContainer>
+          </div>
+        ) : (
+          <DivNone />
+        )}
+        {makeFailList && fail_list.length ? (
+          <div>
+            <NameListLineContainer>
               <TitleList>{nameFailList}</TitleList>
-                {
-                  fail_list.map((data) => (
-                    <li key={data.id}>
-                      {data.name}
-                    </li>
-                  ))
-                }
-              </NameListLineContainer>
-            </div>
-          )
-          : (
-            <DivNone />
-          )
-        }
+              {fail_list.map((data) => (
+                <li key={data.id}>{data.name}</li>
+              ))}
+            </NameListLineContainer>
+          </div>
+        ) : (
+          <DivNone />
+        )}
       </RouteGeoListContainer>
     );
   }

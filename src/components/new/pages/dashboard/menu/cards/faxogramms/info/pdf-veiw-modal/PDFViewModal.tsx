219ca6017988fa3a5ec8a1be604b3b53f1dbd402
vredsun <Vladimir>
2019-03-03 23:12:31 +0300

    DITETS-5367 Try catch отображение пдф

--- src/components/new/pages/dashboard/menu/cards/faxogramms/info/pdf-veiw-modal/PDFViewModal.tsx
+++ src/components/new/pages/dashboard/menu/cards/faxogramms/info/pdf-veiw-modal/PDFViewModal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as Modal from 'react-bootstrap/lib/Modal';
 import PDF from 'react-pdf-js';
+import * as Raven from 'raven-js';
 
 import ModalBody from 'components/ui/Modal';
 import Preloader from 'components/ui/new/preloader/Preloader';
@@ -10,16 +11,29 @@ import {
   StatePDFViewModal,
 } from 'components/new/pages/dashboard/menu/cards/faxogramms/info/pdf-veiw-modal/PDFViewModal.h';
 
-class PDFViewModal extends React.Component<PropsPDFViewModal, StatePDFViewModal> {
+class PDFViewModal extends React.Component<
+  PropsPDFViewModal,
+  StatePDFViewModal
+> {
   state = {
     url: '',
   };
 
   componentDidMount() {
-    window.URL.revokeObjectURL(this.state.url);
-    if (this.props.blob) {
-      const url = window.URL.createObjectURL(this.props.blob);
-      this.setState({ url });
+    try {
+      window.URL.revokeObjectURL(this.state.url);
+      if (this.props.blob) {
+        const url = window.URL.createObjectURL(this.props.blob);
+        this.setState({ url });
+      }
+    } catch (error) {
+      global.NOTIFICATION_SYSTEM.notify(
+        'Ошибка формирования pdf',
+        'error',
+        'tr',
+      );
+      Raven.captureException(new Error(error));
+      this.props.onHide();
     }
   }
 
@@ -27,11 +41,11 @@ class PDFViewModal extends React.Component<PropsPDFViewModal, StatePDFViewModal>
     return (
       <Modal show onHide={this.props.onHide}>
         <ModalBody bsClass="null">
-          {
-            !this.state.url
-            ? <Preloader typePreloader="mainpage" />
-            : <PDF file={this.state.url} />
-          }
+          {!this.state.url ? (
+            <Preloader typePreloader="mainpage" />
+          ) : (
+            <PDF file={this.state.url} />
+          )}
         </ModalBody>
       </Modal>
     );

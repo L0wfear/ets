92f2777bbead88dcbd5455190d2735d210a57546
vredsun <vokinda@yandex.ru>
2019-03-18 01:30:52 +0300

    DITETS-5461 Fix z-index for order history

---
+++ src/components/directories/order/order_history/HistoryOrderList.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { Col } from 'react-bootstrap';
+
+export const HistoryOrderListPanelTitleCol = styled(Col)`
+  cursor: pointer;
+  padding: 20px;
+`;
+
+export const HistoryOrderListPanelTitleH4 = styled.h4`
+  display: flex;
+  justify-content: space-between;
+  margin: 0px;
+  z-index: 10;
+`;
+
+export const HistoryOrderListPanelTitleColSelect = styled(Col)`
+  display: flex;
+  z-index: 10;
+`;

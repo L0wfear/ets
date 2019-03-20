5614568515907c4216f184d3aba79906082413a4
vredsun <vokinda@yandex.ru>
2019-03-20 13:44:22 +0300

    DITETS-1154 открыл cars_count_deviation на прод

--- src/components/reports/operational/cars_count_deviation/config-data/permissions.ts
+++ src/components/reports/operational/cars_count_deviation/config-data/permissions.ts
@@ -1,7 +1,5 @@
 export default {
-  list: `cars_count_deviation.${
-    process.env.STAND !== 'prod' || __DEVELOPMENT__ ? 'list' : 'false'
-  }`,
+  list: 'cars_count_deviation.list',
   create: 'cars_count_deviation.create',
   read: 'cars_count_deviation.read',
   update: 'cars_count_deviation.update',

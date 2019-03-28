657cdd1b5e386e86c9bd065d3587dc25d309e2ea
vredsun <vokinda@yandex.ru>
2019-03-28 12:17:52 +0300

    DITETS-5467 Поправил ожидаемы формат для фильтров заявок

--- src/components/new/pages/edc_request/_config-data/registry-config.ts
+++ src/components/new/pages/edc_request/_config-data/registry-config.ts
@@ -57,9 +57,8 @@ export const config: TypeConfigData<EdcRequest> = {
         title: 'Категория',
         getRegistryData: {
           entity: 'edc/defect_category',
-          typeAns: 'result',
-          valueKey: 'defect_category_id',
-          labelKey: 'deffect_category_name',
+          valueKey: 'edc_id',
+          labelKey: 'name',
         },
       },
       {
@@ -68,9 +67,8 @@ export const config: TypeConfigData<EdcRequest> = {
         title: 'Дефект',
         getRegistryData: {
           entity: 'edc/defect',
-          typeAns: 'result',
-          valueKey: 'defect_id',
-          labelKey: 'defect_name',
+          valueKey: 'edc_id',
+          labelKey: 'name',
         },
       },
       {
@@ -104,83 +102,103 @@ export const config: TypeConfigData<EdcRequest> = {
         {
           key: 'status_name',
           title: 'Статус',
+          width: 100,
         },
         {
           key: 'request_number',
           title: 'Заявка №',
+          width: 100,
         },
         {
           key: 'create_date',
           title: 'Дата',
           format: 'datetime',
+          width: 200,
         },
         {
           key: 'deffect_category_name',
           title: 'Категория',
+          width: 200,
         },
         {
           key: 'defect_name',
           title: 'Дефект',
+          width: 200,
         },
         {
           key: 'emergency_name',
           title: 'Срочность',
+          width: 150,
         },
         {
           key: 'mission_numbers_text',
           title: 'Задание №',
+          width: 100,
         },
         {
           key: 'duty_mission_numbers_text',
           title: 'Наряд-задание №',
+          width: 100,
         },
         {
           key: 'ods',
           title: 'ОДС',
+          width: 100,
         },
         {
           key: 'address_name',
           title: 'Адрес',
+          width: 100,
         },
         {
           key: 'district_name',
           title: 'Район',
+          width: 100,
         },
         {
           key: 'okrug_name',
           title: 'Округ',
+          width: 100,
         },
         {
           key: 'contractor_name',
           title: 'Организация',
+          width: 200,
         },
         {
           key: 'main_phone',
           title: 'Осн.телефон',
+          width: 100,
         },
         {
           key: 'additional_phone',
           title: 'Доп.телефон',
+          width: 100,
         },
         {
           key: 'name',
           title: 'Контактное лицо',
+          width: 200,
         },
         {
           key: 'desired_time_from',
           title: 'Желательное время с:',
+          width: 100,
         },
         {
           key: 'desired_time_to',
           title: 'Желательное время по:',
+          width: 100,
         },
         {
           key: 'desired_date',
           title: 'Желательная дата',
+          width: 200,
         },
         {
           key: 'request_send_at',
           title: 'Передано в ППОЗ',
+          width: 100,
         },
       ],
     },

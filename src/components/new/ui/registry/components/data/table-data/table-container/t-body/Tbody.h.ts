3f5b4375944000c43d13698c4eea08dc1fe6d3b6
vredsun <vokinda@yandex.ru>
2019-04-12 15:54:02 +0300

    DITETS-5700 Fix серверное пагинации

--- src/components/new/ui/registry/components/data/table-data/table-container/t-body/Tbody.h.ts
+++ src/components/new/ui/registry/components/data/table-data/table-container/t-body/Tbody.h.ts
@@ -2,6 +2,7 @@ import { DispatchProp } from 'react-redux';
 import { OneRegistryData } from 'components/new/ui/registry/module/registry';
 
 export interface StatePropsTbody {
+  Service: any;
   processedArray: OneRegistryData['list']['processed']['processedArray'];
   paginator: OneRegistryData['list']['paginator'];
   uniqKey: OneRegistryData['list']['data']['uniqKey'];

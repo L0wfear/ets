9199ebc451e7202a9e8cda665b444509869e23a7
vredsun <vokinda@yandex.ru>
2019-05-16 16:49:50 +0300

    DITETS-5922 Добавил останку всплытия двойного клика по форме

--- src/components/new/ui/modal/Modal.tsx
+++ src/components/new/ui/modal/Modal.tsx
@@ -4,10 +4,18 @@ import * as BootstrapModal from 'react-bootstrap/lib/Modal';
 const EtsModal: React.FC<
   BootstrapModal.ModalProps
 > = (props) => {
+  const handleDoubleClick = React.useCallback(
+    (event) => {
+      event.stopPropagation();
+    },
+    [],
+  );
   return (
-    <BootstrapModal
-      {...props}
-    />
+    <BootstrapModal  {...props}>
+      <div onDoubleClick={handleDoubleClick}>
+        {props.children}
+      </div>
+    </BootstrapModal>
   );
 };
 

96b45ec0bdfb107dfbaf354ba3e6bb9f6395665c
vredsun <vokinda@yandex.ru>
2019-05-22 18:50:54 +0300

    DITETS-59 Изменил выбор подразделений у геообъектов

--- src/components/new/pages/nsi/geoobjects/pages/odh/OdhForm/fields/company_structure_id/FieldCompanyStructureId.tsx
+++ src/components/new/pages/nsi/geoobjects/pages/odh/OdhForm/fields/company_structure_id/FieldCompanyStructureId.tsx
@@ -3,6 +3,7 @@ import { ExtField } from 'components/ui/new/field/ExtField';
 import { changeCompanyStructureIdNotyfication } from 'utils/notifications';
 import { Odh } from 'redux-main/reducers/modules/geoobject/actions_by_type/odh/@types';
 import useCompanyStructureLinearForUserOptions from 'components/new/utils/hooks/services/useOptions/useCompanyStructureLinearForUserOptions';
+import { isNumber } from 'util';
 
 type FieldCompanyStructureIdProps = {
   value: Odh['company_structures'];
@@ -22,14 +23,36 @@ const FieldCompanyStructureId: React.FC<FieldCompanyStructureIdProps> = React.me
       (key, _, options: typeof companyStructureOptionData.options) => {
         global.NOTIFICATION_SYSTEM.notify(changeCompanyStructureIdNotyfication);
 
+        const setIds = options.reduce(
+          (set, { rowData }) => {
+            set.add(rowData.id);
+
+            if (isNumber(rowData.parent_id)) {
+              set.add(rowData.parent_id);
+            }
+
+            return set;
+          },
+          new Set(),
+        );
+
         props.handleChange({
-          [key]: options.map(({ rowData }) => ({
-            id: rowData.id,
-            name: rowData.name,
-          })),
+          [key]: companyStructureOptionData.options.reduce(
+            (newArr, { rowData }) => {
+              if (setIds.has(rowData.id)) {
+                newArr.push({
+                  id: rowData.id,
+                  name: rowData.name,
+                });
+              }
+
+              return newArr;
+            },
+            [],
+          ),
         });
       },
-      [props.handleChange],
+      [props.handleChange, companyStructureOptionData],
     );
 
     const value = React.useMemo(

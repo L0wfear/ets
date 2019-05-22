18c2f684d60172d0968b491e66256efa74802d8e
vredsun <Vladimir>
2019-05-22 12:47:08 +0300

    DITETS-5945 Поправил стиль ячейки таблицы

--- src/components/new/ui/registry/components/data/table-data/table-container/t-body/tr-tbody/tr-td/styled/styled.ts
+++ src/components/new/ui/registry/components/data/table-data/table-container/t-body/tr-tbody/tr-td/styled/styled.ts
@@ -10,7 +10,7 @@ export const EtsTbodyTrTd = styled.td`
   &&& {
     padding: 8px;
     border: 1px solid white;
-    vertical-align: baseline;
+    vertical-align: top;
     input {
       cursor: pointer;
     }

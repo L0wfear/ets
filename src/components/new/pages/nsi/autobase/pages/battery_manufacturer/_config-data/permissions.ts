d7c92bdfd1416cf4cc498ff8d534b32a36ee102f
vredsun <vokinda@yandex.ru>
2019-06-07 12:24:28 +0300

    DITETS-6087 Поправил нейминг прав на создание

--- src/components/new/pages/nsi/autobase/pages/battery_manufacturer/_config-data/permissions.ts
+++ src/components/new/pages/nsi/autobase/pages/battery_manufacturer/_config-data/permissions.ts
@@ -1,6 +1,6 @@
 const batteryManufacturerPermissions = {
   list: 'autobase_battery_manufacturer.list',
-  create: 'autobase_battery_manufacturer.list',
+  create: 'autobase_battery_manufacturer.create',
   read: 'autobase_battery_manufacturer.read',
   update: 'autobase_battery_manufacturer.update',
   delete: 'autobase_battery_manufacturer.delete',
--- src/components/new/pages/nsi/autobase/pages/repair_company/_config-data/permissions.ts
+++ src/components/new/pages/nsi/autobase/pages/repair_company/_config-data/permissions.ts
@@ -1,6 +1,6 @@
 const repairCompanyPermissions = {
   list: 'autobase_company.list',
-  create: 'autobase_company.list',
+  create: 'autobase_company.create',
   read: 'autobase_company.read',
   update: 'autobase_company.update',
   delete: 'autobase_company.delete',

bbdab5ff0cdcc8c6bb7647a9d02d14d4727f9149
vredsun <vokinda@yandex.ru>
2019-06-17 21:59:47 +0300

    DITETS-6141 Убрал overflow у контейнера формы

--- src/components/new/ui/@bootstrap/02-modal_container/EtsModalContainer.tsx
+++ src/components/new/ui/@bootstrap/02-modal_container/EtsModalContainer.tsx
@@ -28,7 +28,6 @@ export const ModalFormStyled = styled.div<{ show: boolean; bsSize?: 'large' | 's
   margin-top: 30px;
   margin-left: 30px;
   margin-right: 30px;
-  overflow: auto;
 
   pointer-events: ${({ show }) => !show ? 'none' : 'all'};
 

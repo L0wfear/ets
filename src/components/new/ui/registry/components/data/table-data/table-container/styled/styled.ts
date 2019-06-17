a8eb56a4ee665273b607c74553de8ff27fdc277b
vredsun <vokinda@yandex.ru>
2019-06-17 21:44:00 +0300

    DITETS-6147 Поправил стиль для таблиц на ПЛ

--- src/components/new/ui/registry/components/data/table-data/table-container/styled/styled.ts
+++ src/components/new/ui/registry/components/data/table-data/table-container/styled/styled.ts
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 export const EtsTableWrapNoScroll = styled.div`
   margin: 5px;
   min-height: 100px;
-  overflow-x: auto;
 `;
 
 export const EtsTableWrap = styled(EtsTableWrapNoScroll)<{ addToMinusHeight?: number }>`
-  overflow-y: auto;
+  overflow: auto;
   max-height: ${({ addToMinusHeight }) => `calc(100vh - ${250 + (addToMinusHeight || 0)}px)`};
 `;
 

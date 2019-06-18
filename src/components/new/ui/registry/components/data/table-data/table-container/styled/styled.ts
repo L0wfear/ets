72f780c0f5d2e60c701fb663b865b8d488608c7b
vredsun <vokinda@yandex.ru>
2019-06-18 10:32:07 +0300

    Update registry style

--- src/components/new/ui/registry/components/data/table-data/table-container/styled/styled.ts
+++ src/components/new/ui/registry/components/data/table-data/table-container/styled/styled.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const EtsTableWrapNoScroll = styled.div`
-  margin: 5px;
+  margin: 2.5px;
   min-height: 100px;
 `;
 
 export const EtsTableWrap = styled(EtsTableWrapNoScroll)<{ addToMinusHeight?: number }>`
   overflow: auto;
-  max-height: ${({ addToMinusHeight }) => `calc(100vh - ${250 + (addToMinusHeight || 0)}px)`};
+  max-height: ${({ addToMinusHeight }) => `calc(100vh - ${240 + (addToMinusHeight || 0)}px)`};
 `;
 
 export const EtsTable = styled.table<{ fixedWidth: boolean }>`

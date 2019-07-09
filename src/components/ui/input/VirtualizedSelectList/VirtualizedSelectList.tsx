bbd8ba93673966b53f863170d06a0451ab64c6fd
MrAmiD <nadymov2910@gmail.com>
2019-07-09 16:23:51 +0300

    DITETS-6359 Добавил стилизацию для длинных строк

--- src/components/ui/input/VirtualizedSelectList/VirtualizedSelectList.tsx
+++ src/components/ui/input/VirtualizedSelectList/VirtualizedSelectList.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import { List } from 'react-virtualized';
+import { VirtualizedOption } from './styled/styled';
 
 const VirtualizedSelectList = (props: any) => {
   const rows = props.children;
   const rowRenderer = ({ key, index, style }) => (
-    <div key={key} style={style}>{rows[index]}</div>
+    <VirtualizedOption title={props.options[index].label} key={key} style={style}>{rows[index]}</VirtualizedOption>
   );
-
   const rowHeight = 36;
-  const optionsCount = props.options.length; // <<< Если нет данных, проверить, что что-то есть!!!
-  const menuHeight = optionsCount < 10 ? optionsCount * rowHeight : rowHeight * 10;
+  const optionsCount = props.options.length - props.getValue().length; // <<< Если нет данных, проверить, что что-то есть!!!
+  const menuHeight = optionsCount < 10
+    ? (optionsCount) * rowHeight
+    : rowHeight * 10;
 
   return (
     <List

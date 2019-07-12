8fa82dd32f49dbe316c7530ab47a9379b495a1c5
vredsun <vokinda@yandex.ru>
2019-07-12 10:13:28 +0300

    DITETS-6249 Исправляет валидацию полей у инспекции автобазы

--- src/components/new/pages/inspection/autobase/form/view_inspect_autobase_form/schema.ts
+++ src/components/new/pages/inspection/autobase/form/view_inspect_autobase_form/schema.ts
@@ -105,6 +105,7 @@ const dataSchema: SchemaType<InspectAutobase['data'], PropsViewInspectAutobaseWi
     cnt_repair_posts: {
       title: 'Количество постов для обслуживания, ремонта техники (шт.)',
       type: 'number',
+      minNotEqual: 0,
       dependencies: [
         (value, { lack_repair_areas }, { type }) => {
           if (type === INSPECT_TYPE_FORM.list) {
@@ -119,6 +120,7 @@ const dataSchema: SchemaType<InspectAutobase['data'], PropsViewInspectAutobaseWi
     },
     repair_posts_in_poor_condition: {
       title: 'Постов в неудовлетворительном состоянии (шт.)',
+      min: 0,
       type: 'number',
     },
     lack_of_storage_facilities: {

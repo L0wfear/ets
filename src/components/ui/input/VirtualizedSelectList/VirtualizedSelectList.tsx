3781d03b897150d998cd4454d07efe86b3d48d53
mrAmiD <nadymov2910@gmail.com>
2019-07-30 15:47:31 +0300

    DITETS-1154 Поправил react-virtualized, и стили

--- src/components/ui/input/VirtualizedSelectList/VirtualizedSelectList.tsx
+++ src/components/ui/input/VirtualizedSelectList/VirtualizedSelectList.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
 import { List } from 'react-virtualized';
 import { VirtualizedOption } from './styled/styled';
+import { isNullOrUndefined } from 'util';
+import { NoOptionsMessage } from 'components/ui/input/ReactSelect/styled/styled';
 
 const VirtualizedSelectList = (props: any) => {
   const rows = props.children;
+  const noOptionsMessage = props.noOptionsMessage();
   const rowRenderer = ({ key, index, style }) => (
-    <VirtualizedOption title={props.options[index].label} key={key} style={style}>{rows[index]}</VirtualizedOption>
+    isNullOrUndefined(rows.length)
+      ? <NoOptionsMessage {...props}>{noOptionsMessage}</NoOptionsMessage>
+      : <VirtualizedOption title={props.options[index].label} key={key} style={style}>{rows[index]}</VirtualizedOption>
   );
   const rowHeight = 36;
   const optionsCount = props.options.length - props.getValue().length;
-  const menuHeight = optionsCount < 10
-    ? (optionsCount) * rowHeight
+  const rowCount = isNullOrUndefined(rows.length)
+    ? 1
+    : !isNullOrUndefined(rows.length) && rows.length < 10
+      ? rows.length
+      : optionsCount;
+
+  const menuHeight = ( optionsCount < 10 || rows.length < 10 || isNullOrUndefined(rows.length) )
+    ? rowCount * rowHeight
     : rowHeight * 10;
 
   return (
@@ -19,7 +30,7 @@ const VirtualizedSelectList = (props: any) => {
       width={800}
       height={menuHeight}
       rowHeight={rowHeight}
-      rowCount={optionsCount}
+      rowCount={rowCount}
       rowRenderer={rowRenderer}
     />
   );

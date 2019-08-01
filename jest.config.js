4b06e82fc370cad6e2aa31f021172e068d813ad5
vredsun <vokinda@yandex.ru>
2019-08-02 01:21:02 +0300

    Добавил коммент для сбора статистики тестов по всему проекту

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   moduleDirectories: ['node_modules', 'src'],
   collectCoverage: true,
+  // collectCoverageFrom: ['src/components/**/*.{ts,js}'],
   globals: {
     window: true,
   },

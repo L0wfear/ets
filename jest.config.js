f9d26f17ad4d9abe1e1684234b918954825d823b
vredsun <vokinda@yandex.ru>
2019-08-02 17:23:19 +0300

    Обновил конфиг тестов

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   moduleDirectories: ['node_modules', 'src'],
   collectCoverage: true,
+  coverageDirectory: './coverageDirectory',
+  reporters: [ 'default', 'jest-junit' ],
   // collectCoverageFrom: ['src/components/**/*.{ts,js}'],
   globals: {
     window: true,

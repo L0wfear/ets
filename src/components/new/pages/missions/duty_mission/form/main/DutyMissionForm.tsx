5b095d229062ce2c6c3aba96b9ff47ab7bc3abee
vredsun <vokinda@yandex.ru>
2019-08-06 18:19:30 +0300

    DITETS-6156 Переименовывает название ПФ Наряд-задания

--- src/components/new/pages/missions/duty_mission/form/main/DutyMissionForm.tsx
+++ src/components/new/pages/missions/duty_mission/form/main/DutyMissionForm.tsx
@@ -210,7 +210,7 @@ class DutyMissionForm extends React.PureComponent<PropsDutyMissionForm, any> {
       if (printFormData) {
         saveData(
           printFormData.blob,
-          `Печатная форма наряд-задания №${result.number}.pdf`,
+          `Наряд-задание №${result.number}.pdf`,
         );
       }
     }

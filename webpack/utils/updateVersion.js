1dd755c53465987a71a6bf75cb11ab0e651140c2
vredsun <vokinda@yandex.ru>
2019-08-06 10:14:11 +0300

    Add autobump

--- webpack/utils/updateVersion.js
+++ webpack/utils/updateVersion.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var fs = require('fs');
 var package = path.join(__dirname, '..', '..', 'package.json');
+var packageLock = path.join(__dirname, '..', '..', 'package-lock.json');
 
-var data = JSON.parse(
+var dataPackage = JSON.parse(
   fs.readFileSync(
     package,
     'utf8',
@@ -13,8 +14,25 @@ var [
   zero,
   release,
   patch,
-] = data.version.split('.').map(number => Number(number));
+] = dataPackage.version.split('.').map(number => Number(number));
 
-data.version = `${zero}.${release}.${patch + 1}`;
+dataPackage.version = `${zero}.${release}.${patch + 1}`;
 
-fs.writeFileSync(package, `${JSON.stringify(data, null, 4)}\n`);
+fs.writeFileSync(package, `${JSON.stringify(dataPackage, null, 4)}\n`);
+
+var dataPackageLock = JSON.parse(
+  fs.readFileSync(
+    packageLock,
+    'utf8',
+    ),
+);
+
+var [
+  zero,
+  release,
+  patch,
+] = dataPackageLock.version.split('.').map(number => Number(number));
+
+dataPackageLock.version = `${zero}.${release}.${patch + 1}`;
+
+fs.writeFileSync(packageLock, `${JSON.stringify(dataPackageLock, null, 4)}\n`);

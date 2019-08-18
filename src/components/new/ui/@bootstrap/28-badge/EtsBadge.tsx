3456ca45202ae22f96f80fb83c32a9526ca175d8
vredsun <vokinda@yandex.ru>
2019-08-18 15:52:08 +0300

    Update NotificationBadge and EtsBadge

--- src/components/new/ui/@bootstrap/28-badge/EtsBadge.tsx
+++ src/components/new/ui/@bootstrap/28-badge/EtsBadge.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import * as Badge from 'react-bootstrap/lib/Badge';
 
-export const BadgeStyled = styled(Badge)``;
+export const BadgeStyled = styled.span`
+  display: inline-block;
+  min-width: 10px;
+  padding: 3px 7px;
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 1;
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  background-color: #777;
+  border-radius: 10px;
+`;
 
-export type EtsBadgeProps = any;
+export type EtsBadgeProps = {};
 
 const EtsBadge: React.FC<EtsBadgeProps> = React.memo(
   (props) => {

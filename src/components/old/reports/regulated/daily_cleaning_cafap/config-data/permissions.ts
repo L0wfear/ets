16f969ef03d53aa7314ea33848b937834370d45f
mrAmiD <nadymov2910@gmail.com>
2019-08-22 11:25:26 +0300

    Поменял права для отчетов, для возможности просмотра /cleaning_status_cafap_report/ /cleaning_status_report

--- src/components/old/reports/regulated/daily_cleaning_cafap/config-data/permissions.ts
+++ src/components/old/reports/regulated/daily_cleaning_cafap/config-data/permissions.ts
@@ -1,7 +1,12 @@
 export default {
-  list: 'cleaning_status_cafap_report.list',
-  create: 'cleaning_status_cafap_report.create',
-  read: 'cleaning_status_cafap_report.read',
-  update: 'cleaning_status_cafap_report.update',
-  delete: 'cleaning_status_cafap_report.delete',
+  // list: 'cleaning_status_cafap_report.list',
+  // create: 'cleaning_status_cafap_report.create',
+  // read: 'cleaning_status_cafap_report.read',
+  // update: 'cleaning_status_cafap_report.update',
+  // delete: 'cleaning_status_cafap_report.delete',
+  list: 'mission.list',
+  create: 'mission.create',
+  read: 'mission.read',
+  update: 'mission.update',
+  delete: 'mission.delete',
 };
--- src/components/old/reports/regulated/daily_cleaning_ets/config-data/permissions.ts
+++ src/components/old/reports/regulated/daily_cleaning_ets/config-data/permissions.ts
@@ -1,7 +1,12 @@
 export default {
-  list: 'cleaning_status_report.list',
-  create: 'cleaning_status_report.create',
-  read: 'cleaning_status_report.read',
-  update: 'cleaning_status_report.update',
-  delete: 'cleaning_status_report.delete',
+  // list: 'cleaning_status_report.list',
+  // create: 'cleaning_status_report.create',
+  // read: 'cleaning_status_report.read',
+  // update: 'cleaning_status_report.update',
+  // delete: 'cleaning_status_report.delete',
+  list: 'mission.list',
+  create: 'mission.create',
+  read: 'mission.read',
+  update: 'mission.update',
+  delete: 'mission.delete',
 };

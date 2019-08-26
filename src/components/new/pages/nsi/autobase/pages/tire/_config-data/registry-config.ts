61f2548a9261852ec1ef519560fef7b01373ec1c
vredsun <vokinda@yandex.ru>
2019-08-26 11:58:34 +0300

    DITETS-6759 Переименовывает поле odometr_diff для реестраи карточки шин

--- src/components/new/pages/nsi/autobase/pages/tire/_config-data/registry-config.ts
+++ src/components/new/pages/nsi/autobase/pages/tire/_config-data/registry-config.ts
@@ -130,7 +130,7 @@ export const getToConfig = (is_current_structure: boolean): TypeConfigData<Tire>
           },
           {
             key: 'odometr_diff',
-            title: 'Пробег, км',
+            title: 'Общий пробег, км',
             width: 150,
           },
           {

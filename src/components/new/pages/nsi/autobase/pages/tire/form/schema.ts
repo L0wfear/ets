41e4f54b9f5811618e3a3c7abdd9c51d0d37da38
vredsun <vokinda@yandex.ru>
2019-09-03 12:11:16 +0300

    DITETS-6791 Исправляет валдиацию по датам на формах аккумуляторов и шин

--- src/components/new/pages/nsi/autobase/pages/tire/form/schema.ts
+++ src/components/new/pages/nsi/autobase/pages/tire/form/schema.ts
@@ -2,23 +2,8 @@ import { SchemaType } from 'components/old/ui/form/new/@types/validate.h';
 import { PropsTire } from 'components/new/pages/nsi/autobase/pages/tire/form/@types/TireForm';
 import { Tire } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
 import memoizeOne from 'memoize-one';
-import { diffDates, diffDatesByDays } from 'components/@next/@utils/dates/dates';
-
-const validateDateInsideOther = (date, tire_to_car: Tire['tire_to_car']) => {
-  if (!date) {
-    return false;
-  }
-
-  return tire_to_car.some(
-    ({ installed_at, uninstalled_at }) => {
-
-      return (
-        diffDates(date, installed_at) > 0
-        && diffDates(date, uninstalled_at) < 0
-      );
-    },
-  );
-};
+import { diffDatesByDays } from 'components/@next/@utils/dates/dates';
+import { validateDateInsideOther } from 'components/new/pages/nsi/autobase/pages/battery_registry/form/schema';
 
 export const tireFormSchema: SchemaType<Tire, PropsTire> = {
   properties: {
@@ -63,7 +48,7 @@ export const tireFormSchema: SchemaType<Tire, PropsTire> = {
                   !d.installed_at
                   ? 'Поле "Дата монтажа" должно быть заполнено'
                   : (
-                    validateDateInsideOther(d.installed_at, tire_to_car)
+                    validateDateInsideOther(d, tire_to_car)
                       ? 'Поле "Дата монтажа" не должно пересекаться с другими записями'
                       : ''
                   )
@@ -74,7 +59,7 @@ export const tireFormSchema: SchemaType<Tire, PropsTire> = {
                     : (
                       d.uninstalled_at
                         ? (
-                          validateDateInsideOther(d.uninstalled_at, tire_to_car)
+                          validateDateInsideOther(d, tire_to_car)
                             ? 'Поле "Дата демонтажа" не должно пересекаться с другими записями'
                             : (
                               diffDatesByDays(d.installed_at, d.uninstalled_at) > 0

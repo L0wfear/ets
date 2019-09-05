51151ba106c0ec3934237e41db63db1eddaa079a
vredsun <vokinda@yandex.ru>
2019-09-05 16:50:12 +0300

    Add support IE

--- src/components/new/ui/@bootstrap/grid_bootstrap/table/table.ts
+++ src/components/new/ui/@bootstrap/grid_bootstrap/table/table.ts
@@ -10,9 +10,19 @@ type Props = {
 
 export const EtsGridTable = styled.div<Props>`
   &&& {
+    display: -ms-grid;
     display: grid;
     border-collapse: collapse;
     min-width: 100%;
+    -ms-grid-columns: ${({ table_width, fields }) => {
+      // const table_width = fields.reduce((summ, { width }) => summ + (width || 150), 0);
+
+      return fields.reduce(
+        (str, { width }) => `${str}
+          minmax(${width || 150}px, ${(width || 150) / (table_width / fields.length)}fr)`,
+        '',
+      );
+    }};
     grid-template-columns: ${({ table_width, fields }) => {
       // const table_width = fields.reduce((summ, { width }) => summ + (width || 150), 0);
 

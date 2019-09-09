6e56faf543b199f9652715bd27f07466ec9c5439
vredsun <vokinda@yandex.ru>
2019-09-09 17:52:51 +0300

    Update branch

--- src/components/new/utils/context/loading/LoadingProvider.tsx
+++ src/components/new/utils/context/loading/LoadingProvider.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
 import LoadingContext, { getLoadingContextDefaultValue } from './LoadingContext';
+import { etsUseDispatch } from 'components/@next/ets_hoc/etsUseDispatch';
 
 type OwnProps = {};
 type Props = OwnProps & {};
 
 const LoadingProvider: React.FC<Props> = React.memo(
   (props) => {
-    const dispatch = useDispatch();
+    const dispatch = etsUseDispatch();
     const value = React.useMemo<ReturnType<typeof getLoadingContextDefaultValue>>(
       () => {
         return getLoadingContextDefaultValue(dispatch);

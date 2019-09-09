6011f278091867ce015e35deff0c1f7f492e39f3
vredsun <Vladimir>
2019-09-09 17:46:49 +0300

    Update loading provider for development

--- src/components/new/utils/context/loading/LoadingPovider.tsx
+++ src/components/new/utils/context/loading/LoadingPovider.tsx
@@ -1,25 +1,18 @@
 import * as React from 'react';
-import { connect, DispatchProp } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import LoadingContext, { getLoadingContextDefaultValue } from './LoadingContext';
-import { ReduxState } from 'redux-main/@types/state';
 
-type LoadingProviderStateProps = {};
-type LoadingProviderDispatchProps = DispatchProp;
-type LoadingProviderOwnProps = any;
-type LoadingProviderMergedProps = (
-  LoadingProviderStateProps
-  & LoadingProviderDispatchProps
-  & LoadingProviderOwnProps
-);
-type LoadingProviderProps = LoadingProviderMergedProps;
+type OwnProps = {};
+type Props = OwnProps & {};
 
-const LoadingProvider: React.FC<LoadingProviderProps> = React.memo(
+const LoadingProvider: React.FC<Props> = React.memo(
   (props) => {
+    const dispatch = useDispatch();
     const value = React.useMemo<ReturnType<typeof getLoadingContextDefaultValue>>(
       () => {
-        return getLoadingContextDefaultValue(props.dispatch);
+        return getLoadingContextDefaultValue(dispatch);
       },
-      [props.dispatch],
+      [],
     );
 
     return (
@@ -30,6 +23,4 @@ const LoadingProvider: React.FC<LoadingProviderProps> = React.memo(
   },
 );
 
-export default connect<LoadingProviderStateProps, LoadingProviderDispatchProps, LoadingProviderOwnProps, ReduxState>(
-  null,
-)(LoadingProvider);
+export default LoadingProvider;

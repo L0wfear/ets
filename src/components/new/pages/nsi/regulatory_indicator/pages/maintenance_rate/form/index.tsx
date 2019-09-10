6f69b23f3ca756f6b7e7999ef51d0c7b51e463b1
vredsun <vokinda@yandex.ru>
2019-09-11 00:01:35 +0300

    Fix paginator

--- src/components/new/pages/nsi/regulatory_indicator/pages/maintenance_rate/form/index.tsx
+++ src/components/new/pages/nsi/regulatory_indicator/pages/maintenance_rate/form/index.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import LoadingComponent from 'components/old/ui/PreloaderMainPage';
-import { DivNone } from 'global-styled/global-styled';
-import { PropsMaintenanceRateFormLazy } from 'components/new/pages/nsi/regulatory_indicator/pages/maintenance_rate/form/@types/MaintenanceRateForm';
-import { compose } from 'recompose';
-import withFormRegistrySearch from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearch';
-import { connect } from 'react-redux';
-import { getServiceData } from 'components/new/ui/registry/module/selectors-registry';
-import { getRegistryState } from 'redux-main/reducers/selectors';
-import { ReduxState } from 'redux-main/@types/state';
 import { get } from 'lodash';
+
+import LoadingComponent from 'components/old/ui/PreloaderMainPage';
+import { PropsMaintenanceRateFormLazy } from 'components/new/pages/nsi/regulatory_indicator/pages/maintenance_rate/form/@types/MaintenanceRateForm';
+import withFormRegistrySearch from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearch';
+import { getServiceData } from 'components/new/ui/registry/module/selectors-registry';
+import { etsUseSelector } from 'components/@next/ets_hoc/etsUseDispatch';
 
 const MaintenanceRateForm = React.lazy(() => (
   import(/* webpackChunkName: "maintenance_rate_form" */'components/new/pages/nsi/regulatory_indicator/pages/maintenance_rate/form/MaintenanceRateForm')
@@ -18,32 +15,21 @@ const MaintenanceRateFormLazy: React.FC<PropsMaintenanceRateFormLazy> = React.me
   (props) => {
     const page = props.registryKey || props.page;
     const path = `${props.path ? `${props.path}-` : ''}maintenance-rate-form`;
+    const type = etsUseSelector((state) => get(getServiceData(state, props.registryKey), 'getRegistryData.payload.type', null));
 
-    return props.element ?
-      (
+    return props.element && (
         <React.Suspense fallback={<LoadingComponent />}>
           <MaintenanceRateForm
             element={props.element}
             handleHide={props.onFormHide}
 
-            type={props.type}
+            type={type}
             page={page}
             path={path}
           />
         </React.Suspense>
-      )
-      :
-      (
-        <DivNone />
       );
   },
 );
 
-export default compose<any, any>(
-  withFormRegistrySearch({}),
-  connect<any, any, { registryKey: string }, ReduxState>(
-    (state, { registryKey }) => ({
-      type: get(getServiceData(getRegistryState(state), registryKey), 'getRegistryData.payload.type', null),
-    }),
-  ),
-)(MaintenanceRateFormLazy);
+export default withFormRegistrySearch({})(MaintenanceRateFormLazy);

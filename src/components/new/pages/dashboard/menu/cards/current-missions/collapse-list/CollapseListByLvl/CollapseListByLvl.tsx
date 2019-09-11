c4b5597e756aec3b401b2eb1884f6b70010e73a2
vredsun <vokinda@yandex.ru>
2019-09-11 23:47:27 +0300

    Update types in dashboard

--- src/components/new/pages/dashboard/menu/cards/current-missions/collapse-list/CollapseListByLvl/CollapseListByLvl.tsx
+++ src/components/new/pages/dashboard/menu/cards/current-missions/collapse-list/CollapseListByLvl/CollapseListByLvl.tsx
@@ -1,49 +1,48 @@
 import * as React from 'react';
 
-import {
-  PropsCollapseListByLvl,
-  StateCollapseListByLvl,
-} from 'components/new/pages/dashboard/menu/cards/current-missions/collapse-list/CollapseListByLvl/CollapseListByLvl.h';
 import CollapseListFirstLvl from 'components/new/pages/dashboard/menu/cards/current-missions/collapse-list/CollapseListByLvl/CollapseListFirstLvl/CollapseListFirstLvl';
 import CollapseText from 'components/old/ui/collapse/text/CollapseText';
 
 import { CollapseTitleContainer } from 'components/new/pages/dashboard/menu/cards/current-missions/collapse-list/CollapseListByLvl/styled/styled';
 
+import {
+  CurrentMissionsItemsType,
+} from 'components/new/pages/dashboard/redux-main/modules/dashboard/@types/current-mission.h';
+
+type Props = {
+  collapsetItems: CurrentMissionsItemsType[];
+  handleClick: (lastSubItem: ValuesOf<ValuesOf<CurrentMissionsItemsType['subItems']>['subItems']>) => any;
+  classNameContainer?: string;
+};
+
 const components = {
   CollapseTitleContainer,
 };
 
-class CollapseListByLvl extends React.PureComponent<PropsCollapseListByLvl, StateCollapseListByLvl> {
-  rednerMap = ({ subItems, ...item }, index) => {
-    const { props } = this;
-
-    return (
-      <li key={index} >
-        <CollapseText
-          title={item.title}
-          noClickOnTitle={!subItems.length}
-          components={components}
-        >
-          <CollapseListFirstLvl
-            collapsetItems={subItems}
-            handleClick={props.handleClick}
-            index={index}
-          />
-        </CollapseText>
-      </li>
-    );
-  }
-  render() {
-    const {
-      props,
-    } = this;
-
+const CollapseListByLvl: React.FC<Props> = React.memo(
+  (props) => {
     return (
       <ul>
-        { props.collapsetItems.map(this.rednerMap) }
+        {
+          props.collapsetItems.map(({ subItems, ...item }, index) => (
+            <li key={index} >
+              <CollapseText
+                title={item.title}
+                noClickOnTitle={!subItems.length}
+                components={components}
+              >
+                <CollapseListFirstLvl
+                  collapsetItems={subItems}
+                  handleClick={props.handleClick}
+                  index={index}
+                />
+              </CollapseText>
+            </li>
+          ))
+        }
       </ul>
     );
-  }
-}
+  },
+);
 
 export default CollapseListByLvl;

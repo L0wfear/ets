a2de3e441090bbc888d4b56a1de06dd62b526c90
vredsun <vokinda@yandex.ru>
2019-09-14 21:45:07 +0300

    DITETS-1154 Move stateProgramFormLazy in withFormRegistrySearchNew

--- src/components/new/pages/nsi/repair/pages/state_program/form/StateProgramForm.tsx
+++ src/components/new/pages/nsi/repair/pages/state_program/form/StateProgramForm.tsx
@@ -1,103 +1,83 @@
 import * as React from 'react';
+import { compose } from 'recompose';
 
 import EtsBootstrap from 'components/new/ui/@bootstrap';
-import { compose } from 'recompose';
 import withForm from 'components/old/compositions/vokinda-hoc/formWrap/withForm';
 
 import ModalBodyPreloader from 'components/old/ui/new/preloader/modal-body/ModalBodyPreloader';
-import { ReduxState } from 'redux-main/@types/state';
-import { connect } from 'react-redux';
 import {
-  OwnStateProgramProps,
   PropsStateProgram,
-  StatePropsStateProgram,
-  DispatchPropsStateProgram,
   PropsStateProgramWithForm,
 } from 'components/new/pages/nsi/repair/pages/state_program/form/@types/StateProgramForm';
-import { DivNone } from 'global-styled/global-styled';
 import stateProgramPermissions from '../_config-data/permissions';
 import { stateProgramFormSchema } from './schema';
 import { getDefaultStateProgramElement } from './utils';
 import { StateProgram } from 'redux-main/reducers/modules/repair/state_program/@types/stateProgram';
 import { actionCreateStateProgram, actionUpdateStateProgram } from 'redux-main/reducers/modules/repair/state_program/actions_state_program';
 import useStateProgramStatusOptions from './use/useStateProgramStatusOptions';
-import { actionLoadStateProgramStatus } from 'redux-main/reducers/modules/repair/state_program_status/actions_state_program_status';
 import ExtField from 'components/@next/@ui/renderFields/Field';
 
-const StateProgramForm: React.FC<PropsStateProgram> = (props) => {
-  const {
-    formState: state,
-    formErrors: errors,
-    page,
-    path,
-    isPermitted,
-  } = props;
+const StateProgramForm: React.FC<PropsStateProgram> = React.memo(
+  (props) => {
+    const {
+      formState: state,
+      formErrors: errors,
+      page,
+      path,
+      isPermitted,
+    } = props;
 
-  const statusOptions = useStateProgramStatusOptions(
-    props.actionLoadStateProgramStatus,
-    page, path,
-  );
+    const statusOptions = useStateProgramStatusOptions(
+      props,
+    );
 
-  const title = 'Государственная программа ремонта';
+    const title = 'Государственная программа ремонта';
 
-  return (
-    <EtsBootstrap.ModalContainer id="modal-state_program" show onHide={props.hideWithoutChanges}>
-      <EtsBootstrap.ModalHeader closeButton>
-        <EtsBootstrap.ModalTitle>{ title }</EtsBootstrap.ModalTitle>
-      </EtsBootstrap.ModalHeader>
-      <ModalBodyPreloader page={page} path={path} typePreloader="mainpage">
-        <EtsBootstrap.Row>
-          <EtsBootstrap.Col md={12}>
-            <ExtField
-              type="string"
-              label="Наименование государственной программы"
-              error={errors.name}
-              value={state.name}
-              onChange={props.handleChange}
-              boundKeys="name"
-            />
-          </EtsBootstrap.Col>
-          <EtsBootstrap.Col md={6}>
-            <ExtField
-              type="select"
-              label="Статус"
-              error={errors.status_id}
-              value={state.status_id}
-              options={statusOptions}
-              onChange={props.handleChange}
-              clearable={false}
-              boundKeys="status_id"
-            />
-          </EtsBootstrap.Col>
-        </EtsBootstrap.Row>
-      </ModalBodyPreloader>
-      <EtsBootstrap.ModalFooter>
-        {
-          isPermitted // либо обновление, либо создание
-          ? (
-            <EtsBootstrap.Button disabled={!props.canSave} onClick={props.defaultSubmit}>Сохранить</EtsBootstrap.Button>
-          )
-          : (
-            <DivNone />
-          )
-        }
-        <EtsBootstrap.Button onClick={props.hideWithoutChanges}>Отменить</EtsBootstrap.Button>
-      </EtsBootstrap.ModalFooter>
-    </EtsBootstrap.ModalContainer>
-  );
-};
+    return (
+      <EtsBootstrap.ModalContainer id="modal-state_program" show onHide={props.hideWithoutChanges}>
+        <EtsBootstrap.ModalHeader closeButton>
+          <EtsBootstrap.ModalTitle>{ title }</EtsBootstrap.ModalTitle>
+        </EtsBootstrap.ModalHeader>
+        <ModalBodyPreloader page={page} path={path} typePreloader="mainpage">
+          <EtsBootstrap.Row>
+            <EtsBootstrap.Col md={12}>
+              <ExtField
+                type="string"
+                label="Наименование государственной программы"
+                error={errors.name}
+                value={state.name}
+                onChange={props.handleChange}
+                boundKeys="name"
+              />
+            </EtsBootstrap.Col>
+            <EtsBootstrap.Col md={6}>
+              <ExtField
+                type="select"
+                label="Статус"
+                error={errors.status_id}
+                value={state.status_id}
+                options={statusOptions}
+                onChange={props.handleChange}
+                clearable={false}
+                boundKeys="status_id"
+              />
+            </EtsBootstrap.Col>
+          </EtsBootstrap.Row>
+        </ModalBodyPreloader>
+        <EtsBootstrap.ModalFooter>
+          {
+            isPermitted && (
+              <EtsBootstrap.Button disabled={!props.canSave} onClick={props.defaultSubmit}>Сохранить</EtsBootstrap.Button>
+            )
+          }
+          <EtsBootstrap.Button onClick={props.hideWithoutChanges}>Отменить</EtsBootstrap.Button>
+        </EtsBootstrap.ModalFooter>
+      </EtsBootstrap.ModalContainer>
+    );
+  },
+);
 
-export default compose<PropsStateProgram, OwnStateProgramProps>(
-  connect<StatePropsStateProgram, DispatchPropsStateProgram, OwnStateProgramProps, ReduxState>(
-    null,
-    (dispatch: any) => ({
-      actionLoadStateProgramStatus: (...arg) => (
-        dispatch(
-          actionLoadStateProgramStatus(...arg),
-        )
-      ),
-    }),
-  ),
+export default compose<PropsStateProgram, PropsStateProgramWithForm>(
   withForm<PropsStateProgramWithForm, StateProgram>({
     uniqField: 'id',
     createAction: actionCreateStateProgram,

21983a8bac4bf1166aeca0da032efa215a9b2754
vredsun <vokinda@yandex.ru>
2019-09-14 19:17:58 +0300

    DITETS-1154 Move tireForm in withFormRegistrySearchNew

--- src/components/new/pages/nsi/autobase/pages/tire/form/@types/TireForm.ts
+++ src/components/new/pages/nsi/autobase/pages/tire/form/@types/TireForm.ts
@@ -2,29 +2,14 @@ import { Tire } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
 import { OutputWithFormProps } from 'components/old/compositions/vokinda-hoc/formWrap/withForm';
 import { IStateAutobase } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
 import { WithSearchProps } from 'components/new/utils/hooks/hoc/withSearch';
-
-export type OnFormHideType = (isSubmitted: boolean, result?: any) => void;
-
-export type PropsTireFormLazy = {
-  element: Partial<Tire>;
-  onFormHide: OnFormHideType
-
-  registryKey?: string;
-  page?: string;
-  path?: string;
-};
+import { WithFormRegistrySearchAddProps } from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearchNew';
 
 export type StatePropsTire = {
   tireModelList: IStateAutobase['tireModelList'];
   tireSizeList: IStateAutobase['tireSizeList'];
   isPermitterToUpdateInitialMileage: boolean;
 };
-export type OwnTireProps = {
-  element: Partial<Tire>;
-  handleHide: OnFormHideType
-  page: string;
-  path?: string;
-};
+export type OwnTireProps = WithFormRegistrySearchAddProps<Tire>;
 
 export type PropsTireWithForm = (
   StatePropsTire

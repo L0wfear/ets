9844b0c1d6feebf5691b142e2f340cfc965885f9
vredsun <vokinda@yandex.ru>
2019-09-14 19:26:32 +0300

    DITETS-1154 Move sparePartFrom in withFormRegistrySearchNew

--- src/components/new/pages/nsi/autobase/pages/spare_part/form/index.tsx
+++ src/components/new/pages/nsi/autobase/pages/spare_part/form/index.tsx
@@ -1,41 +1,12 @@
 import * as React from 'react';
-import LoadingComponent from 'components/old/ui/PreloaderMainPage';
-import ErrorBoundaryForm from 'components/new/ui/error_boundary_registry/ErrorBoundaryForm';
 
-import { DivNone } from 'global-styled/global-styled';
-
-import { PropsSparePartFormLazy } from 'components/new/pages/nsi/autobase/pages/spare_part/form/@types/SparePartForm';
-import withFormRegistrySearch from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearch';
+import { withFormRegistrySearchNew, WithFormRegistrySearchProps } from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearchNew';
+import { SparePart } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
 
 const SparePartFrom = React.lazy(() => (
   import(/* webpackChunkName: "spare_part_form" */ 'components/new/pages/nsi/autobase/pages/spare_part/form/SparePartForm')
 ));
 
-const SparePartFormLazy: React.FC<PropsSparePartFormLazy> = React.memo(
-  (props) => {
-    const page = props.registryKey || props.page;
-    const path = `${props.path ? `${props.path}-` : ''}spare-part-form`;
-
-    return (
-      props.element
-        ? (
-          <ErrorBoundaryForm>
-            <React.Suspense fallback={<LoadingComponent />}>
-              <SparePartFrom
-                element={props.element}
-                handleHide={props.onFormHide}
-
-                page={page}
-                path={path}
-              />
-            </React.Suspense>
-          </ErrorBoundaryForm>
-        )
-        : (
-          <DivNone />
-        )
-    );
-  },
-);
-
-export default withFormRegistrySearch({})(SparePartFormLazy);
+export default withFormRegistrySearchNew<WithFormRegistrySearchProps<SparePart>, SparePart>({
+  add_path: 'spare-part',
+})(SparePartFrom);

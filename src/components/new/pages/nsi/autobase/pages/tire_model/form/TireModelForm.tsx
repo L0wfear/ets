d4b65755fc83777da95fd554681ac1301f76d746
vredsun <vokinda@yandex.ru>
2019-09-14 19:21:26 +0300

    DITETS-1154 Move tireModelForm in withFormRegistrySearchNew

--- src/components/new/pages/nsi/autobase/pages/tire_model/form/TireModelForm.tsx
+++ src/components/new/pages/nsi/autobase/pages/tire_model/form/TireModelForm.tsx
@@ -1,120 +1,110 @@
 import * as React from 'react';
+import { compose } from 'recompose';
 
 import EtsBootstrap from 'components/new/ui/@bootstrap';
 import ExtField from 'components/@next/@ui/renderFields/Field';
-import { compose } from 'recompose';
 import withForm from 'components/old/compositions/vokinda-hoc/formWrap/withForm';
 
 import { defaultSelectListMapper } from 'components/old/ui/input/ReactSelect/utils';
 import ModalBodyPreloader from 'components/old/ui/new/preloader/modal-body/ModalBodyPreloader';
-import { ReduxState } from 'redux-main/@types/state';
-import { connect } from 'react-redux';
 import {
-  OwnTireModelProps,
   PropsTireModel,
-  StatePropsTireModel,
   PropsTireModelWithForm,
 } from 'components/new/pages/nsi/autobase/pages/tire_model/form/@types/TireModelForm';
 import { TireModel } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
-import { DivNone } from 'global-styled/global-styled';
 import { getAutobaseState } from 'redux-main/reducers/selectors';
 import { getDefaultTireModelElement } from './utils';
 import { tireModelFormSchema } from './schema';
 import tireModelPermissions from '../_config-data/permissions';
 import { tireManufacturerGetAndSetInStore } from 'redux-main/reducers/modules/autobase/actions_by_type/tire_manufacturer/actions';
 import { autobaseCreateTireModel, autobaseUpdateTireModel } from 'redux-main/reducers/modules/autobase/actions_by_type/tire_model/actions';
+import { etsUseSelector, etsUseDispatch } from 'components/@next/ets_hoc/etsUseDispatch';
 
-const TireModelForm: React.FC<PropsTireModel> = (props) => {
-  const {
-    formState: state,
-    formErrors: errors,
-    page,
-    path,
-    tireManufacturerList,
-  } = props;
+const TireModelForm: React.FC<PropsTireModel> = React.memo(
+  (props) => {
+    const {
+      formState: state,
+      formErrors: errors,
+      page,
+      path,
+      IS_CREATING,
+      isPermitted,
+    } = props;
 
-  const IS_CREATING = !state.id;
+    const dispatch = etsUseDispatch();
+    const tireManufacturerList = etsUseSelector((stateRedux) => getAutobaseState(stateRedux).tireManufacturerList);
 
-  const title = !IS_CREATING ? 'Изменение записи' : 'Создание записи';
-  const isPermitted = !IS_CREATING ? props.isPermittedToUpdate : props.isPermittedToCreate;
+    const title = !IS_CREATING ? 'Изменение записи' : 'Создание записи';
 
-  React.useEffect(
-    () => {
-      props.dispatch(
-        tireManufacturerGetAndSetInStore(
-          {},
-          props,
-        ),
-      );
-    },
-    [],
-  );
+    React.useEffect(
+      () => {
+        dispatch(
+          tireManufacturerGetAndSetInStore(
+            {},
+            props,
+          ),
+        );
+      },
+      [],
+    );
 
-  const tireManufacturerOptions = React.useMemo(
-    () => (
-      tireManufacturerList.map(defaultSelectListMapper)
-    ),
-    [tireManufacturerList],
-  );
+    const tireManufacturerOptions = React.useMemo(
+      () => (
+        tireManufacturerList.map(defaultSelectListMapper)
+      ),
+      [tireManufacturerList],
+    );
 
-  return (
-    <EtsBootstrap.ModalContainer id="modal-tire_model" show onHide={props.hideWithoutChanges}>
-      <EtsBootstrap.ModalHeader closeButton>
-        <EtsBootstrap.ModalTitle>{ title }</EtsBootstrap.ModalTitle>
-      </EtsBootstrap.ModalHeader>
-      <ModalBodyPreloader page={page} path={path} typePreloader="mainpage">
-        <EtsBootstrap.Row>
-          <EtsBootstrap.Col md={6}>
-            <ExtField
-              id="name"
-              type="string"
-              label="Модель шины"
-              value={state.name}
-              error={errors.name}
-              disabled={!isPermitted}
-              onChange={props.handleChange}
-              boundKeys="name"
-              modalKey={path}
-            />
-          </EtsBootstrap.Col>
-          <EtsBootstrap.Col md={6}>
-            <ExtField
-              id="tire_manufacturer_id"
-              type="select"
-              label="Производитель шины"
-              options={tireManufacturerOptions}
-              value={state.tire_manufacturer_id}
-              error={errors.tire_manufacturer_id}
-              disabled={!isPermitted}
-              onChange={props.handleChange}
-              boundKeys="tire_manufacturer_id"
-              clearable={false}
-              modalKey={path}
-            />
-          </EtsBootstrap.Col>
-        </EtsBootstrap.Row>
-      </ModalBodyPreloader>
-      <EtsBootstrap.ModalFooter>
-      {
-        isPermitted // либо обновление, либо создание
-        ? (
-          <EtsBootstrap.Button disabled={!props.canSave} onClick={props.defaultSubmit}>Сохранить</EtsBootstrap.Button>
-        )
-        : (
-          <DivNone />
-        )
-      }
-      </EtsBootstrap.ModalFooter>
-    </EtsBootstrap.ModalContainer>
-  );
-};
+    return (
+      <EtsBootstrap.ModalContainer id="modal-tire_model" show onHide={props.hideWithoutChanges}>
+        <EtsBootstrap.ModalHeader closeButton>
+          <EtsBootstrap.ModalTitle>{ title }</EtsBootstrap.ModalTitle>
+        </EtsBootstrap.ModalHeader>
+        <ModalBodyPreloader page={page} path={path} typePreloader="mainpage">
+          <EtsBootstrap.Row>
+            <EtsBootstrap.Col md={6}>
+              <ExtField
+                id="name"
+                type="string"
+                label="Модель шины"
+                value={state.name}
+                error={errors.name}
+                disabled={!isPermitted}
+                onChange={props.handleChange}
+                boundKeys="name"
+                modalKey={path}
+              />
+            </EtsBootstrap.Col>
+            <EtsBootstrap.Col md={6}>
+              <ExtField
+                id="tire_manufacturer_id"
+                type="select"
+                label="Производитель шины"
+                options={tireManufacturerOptions}
+                value={state.tire_manufacturer_id}
+                error={errors.tire_manufacturer_id}
+                disabled={!isPermitted}
+                onChange={props.handleChange}
+                boundKeys="tire_manufacturer_id"
+                clearable={false}
+                modalKey={path}
+              />
+            </EtsBootstrap.Col>
+          </EtsBootstrap.Row>
+        </ModalBodyPreloader>
+        <EtsBootstrap.ModalFooter>
+        {
+          isPermitted && (
+            <EtsBootstrap.Button disabled={!props.canSave} onClick={props.defaultSubmit}>Сохранить</EtsBootstrap.Button>
+          )
+        }
+        </EtsBootstrap.ModalFooter>
+      </EtsBootstrap.ModalContainer>
+    );
+  },
+);
 
-export default compose<PropsTireModel, OwnTireModelProps>(
-  connect<StatePropsTireModel, {}, OwnTireModelProps, ReduxState>(
-    (state) => ({
-      tireManufacturerList: getAutobaseState(state).tireManufacturerList,
-    }),
-  ),
+export default compose<PropsTireModel, PropsTireModelWithForm>(
   withForm<PropsTireModelWithForm, TireModel>({
     uniqField: 'id',
     createAction: autobaseCreateTireModel,
--- src/components/new/pages/nsi/autobase/pages/tire_model/form/index.tsx
+++ src/components/new/pages/nsi/autobase/pages/tire_model/form/index.tsx
@@ -1,41 +1,12 @@
 import * as React from 'react';
-import LoadingComponent from 'components/old/ui/PreloaderMainPage';
-import ErrorBoundaryForm from 'components/new/ui/error_boundary_registry/ErrorBoundaryForm';
 
-import { DivNone } from 'global-styled/global-styled';
-
-import { PropsTireModelFormLazy } from 'components/new/pages/nsi/autobase/pages/tire_model/form/@types/TireModelForm';
-import withFormRegistrySearch from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearch';
+import { withFormRegistrySearchNew, WithFormRegistrySearchProps } from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearchNew';
+import { TireModel } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
 
 const TireModelFrom = React.lazy(() => (
   import(/* webpackChunkName: "tire_model_form" */ 'components/new/pages/nsi/autobase/pages/tire_model/form/TireModelForm')
 ));
 
-const TireModelFormLazy: React.FC<PropsTireModelFormLazy> = React.memo(
-  (props) => {
-    const page = props.registryKey || props.page;
-    const path = `${props.path ? `${props.path}-` : ''}tire-model-form`;
-
-    return (
-      props.element
-        ? (
-          <ErrorBoundaryForm>
-            <React.Suspense fallback={<LoadingComponent />}>
-              <TireModelFrom
-                element={props.element}
-                handleHide={props.onFormHide}
-
-                page={page}
-                path={path}
-              />
-            </React.Suspense>
-          </ErrorBoundaryForm>
-        )
-        : (
-          <DivNone />
-        )
-    );
-  },
-);
-
-export default withFormRegistrySearch({})(TireModelFormLazy);
+export default withFormRegistrySearchNew<WithFormRegistrySearchProps<TireModel>, TireModel>({
+  add_path: 'tire-model',
+})(TireModelFrom);

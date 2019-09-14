3df4a6f707824fccf50ebce8ba8d624388867e23
vredsun <vokinda@yandex.ru>
2019-09-14 17:12:54 +0300

    DITETS-1154 Move materialConsumptionRateForm in withFormRegistrySearchNew

--- src/components/new/pages/nsi/regulatory_indicator/pages/material_consumption_rate/form/index.tsx
+++ src/components/new/pages/nsi/regulatory_indicator/pages/material_consumption_rate/form/index.tsx
@@ -1,36 +1,11 @@
 import * as React from 'react';
-import LoadingComponent from 'components/old/ui/PreloaderMainPage';
-import { DivNone } from 'global-styled/global-styled';
-import { PropsMaterialConsumptionRateFormLazy } from 'components/new/pages/nsi/regulatory_indicator/pages/material_consumption_rate/form/@types/MaterialConsumptionRateForm';
-import withFormRegistrySearch from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearch';
+import { withFormRegistrySearchNew, WithFormRegistrySearchProps } from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearchNew';
+import { MaterialConsumptionRate } from 'redux-main/reducers/modules/material_consumption_rate/@types/materialConsumptionRate.h';
 
 const MaterialConsumptionRateForm = React.lazy(() => (
   import(/* webpackChunkName: "material_consumption_rate_form" */ 'components/new/pages/nsi/regulatory_indicator/pages/material_consumption_rate/form/MaterialConsumptionRateForm')
 ));
 
-const MaterialConsumptionRateFormLazy: React.FC<PropsMaterialConsumptionRateFormLazy> = React.memo(
-  (props) => {
-    const page = props.registryKey || props.page;
-    const path = `${props.path ? `${props.path}-` : ''}consumption-rate-form`;
-
-    return (
-      props.element ?
-        (
-          <React.Suspense fallback={<LoadingComponent />}>
-            <MaterialConsumptionRateForm
-              element={props.element}
-              handleHide={props.onFormHide}
-
-              page={page}
-              path={path}
-            />
-          </React.Suspense>
-        )
-        : (
-          <DivNone />
-        )
-    );
-  },
-);
-
-export default withFormRegistrySearch({})(MaterialConsumptionRateFormLazy);
+export default withFormRegistrySearchNew<WithFormRegistrySearchProps<MaterialConsumptionRate>, MaterialConsumptionRate>({
+  add_path: 'consumption-rate',
+})(MaterialConsumptionRateForm);

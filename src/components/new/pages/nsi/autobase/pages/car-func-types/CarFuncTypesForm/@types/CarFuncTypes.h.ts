3342e192c9c44e218b41570275396d6e591dc541
vredsun <vokinda@yandex.ru>
2019-09-14 19:45:34 +0300

    DITETS-1154 Move carFuncTypesForm in withFormRegistrySearchNew

--- src/components/new/pages/nsi/autobase/pages/car-func-types/CarFuncTypesForm/@types/CarFuncTypes.h.ts
+++ src/components/new/pages/nsi/autobase/pages/car-func-types/CarFuncTypesForm/@types/CarFuncTypes.h.ts
@@ -1,33 +1,8 @@
 import { CarFuncTypes } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
 import { OutputWithFormProps } from 'components/old/compositions/vokinda-hoc/formWrap/withForm';
-import { DispatchProp } from 'react-redux';
+import { WithFormRegistrySearchAddProps } from 'components/old/compositions/vokinda-hoc/formWrap/withFormRegistrySearchNew';
 
-export type OnFormHideType = (isSubmitted: boolean, result?: any) => void;
-
-export type PropsCarFuncTypesFormWrap = {
-  showForm: boolean;
-  element: CarFuncTypes | null;
-  onFormHide: OnFormHideType;
-
-  loadingPageName?: string;
-  registryKey?: string;
-  path?: string;
-};
-
-export type StatePropsCarFuncTypes = {};
-export type DispatchPropsCarFuncTypes = DispatchProp;
-export type OwnCarFuncTypesProps = {
-  element: CarFuncTypes | null;
-  handleHide: OnFormHideType
-  page: string;
-  path?: string;
-};
-
-export type PropsCarFuncTypesWithForm = (
-  StatePropsCarFuncTypes
-  & DispatchPropsCarFuncTypes
-  & OwnCarFuncTypesProps
-);
+export type PropsCarFuncTypesWithForm = WithFormRegistrySearchAddProps<CarFuncTypes>;
 
 export type PropsCarFuncTypes = OutputWithFormProps<
   PropsCarFuncTypesWithForm,
@@ -35,5 +10,3 @@ export type PropsCarFuncTypes = OutputWithFormProps<
   [ CarFuncTypes ],
   any
 >;
-export type StateCarFuncTypes = {
-};

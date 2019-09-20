ec7653ef4990d62fe705f7c2b4bcb015be7087e2
vredsun <vokinda@yandex.ru>
2019-09-20 19:00:27 +0300

    DITETS-6886 Обновил рендер большого количества полей в выпадающем списке

--- src/components/ui/input/VirtualizedSelectList/VirtualizedSelectList.tsx
+++ src/components/ui/input/VirtualizedSelectList/VirtualizedSelectList.tsx
@@ -2,29 +2,35 @@ import * as React from 'react';
 import { List } from 'react-virtualized';
 import { VirtualizedOption } from './styled/styled';
 
-const styleList = { width: '100%' };
-
-const VirtualizedSelectList = (props: any) => {
-  const rows = props.children;
-  const rowRenderer = ({ key, index, style }) => (
-    <VirtualizedOption title={rows[index].label} key={key} style={style}>{rows[index]}</VirtualizedOption>
-  );
-  const rowHeight = 36;
-  const childrenCount = rows.length - props.getValue().length;
-  const menuHeight = childrenCount < 10
-    ? (childrenCount) * rowHeight
-    : rowHeight * 10;
-
-  return (
-    <List
-      style={styleList}
-      width={800}
-      height={menuHeight}
-      rowHeight={rowHeight}
-      rowCount={childrenCount}
-      rowRenderer={rowRenderer}
-    />
-  );
+type Props = {
+  children: any[]; // Возможно реакт
+  [k: string]: any;
 };
 
-export default React.memo(VirtualizedSelectList);
+const styleList = { width: '100%' };
+const rowHeight = 36;
+
+const VirtualizedSelectList = React.memo(
+  (props: Props) => {
+    const rows = props.children;
+    const rowRenderer = ({ key, index, style }) => (
+      <VirtualizedOption title={rows[index].label} key={key} style={style}>{rows[index]}</VirtualizedOption>
+    );
+
+    const childrenCount = rows.length;
+    const menuHeight = childrenCount * rowHeight;
+
+    return (
+      <List
+        style={styleList}
+        width={800}
+        height={menuHeight}
+        rowHeight={rowHeight}
+        rowCount={childrenCount}
+        rowRenderer={rowRenderer}
+      />
+    );
+  },
+);
+
+export default VirtualizedSelectList;

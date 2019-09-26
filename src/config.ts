d57451f2eab107e8496c6c698f7829aecb97459f
vredsun <vokinda@yandex.ru>
2019-09-26 17:46:02 +0300

    Обновляет пакеты и изменяет конфиги для разработки

--- src/config.ts
+++ src/config.ts
@@ -5,48 +5,39 @@ const PROTO = window.location.protocol;
 const HOST = window.location.host;
 const PATHNAME = window.location.pathname;
 
-const WS_PROTO = (
-  PROTO === 'https:'
-    ? 'wss:'
-    : 'ws:'
-);
 const STAND = process.env.STAND;
 
 const ADMIN_URL = {
   develop: {
-    dev: 'http://dev2-ets.gost-group.com/admin',
+    dev: 'https://dev2-ets.gost-group.com/admin',
     gost_stage: 'https://ets-stage.gost-group.com/admin/',
     ets_test: 'https://ets-test.mos.ru/admin/',
     ets_hotfix: 'https://ets-hotfix.mos.ru/admin/',
-    prod: 'http://ets.mos.ru/admin/',
+    prod: 'https://ets.mos.ru/admin/',
   },
   origin: `${PROTO}//${HOST}${PATHNAME}admin/`,
 };
 
 const DOC_URL = {
   develop: {
-    dev: 'http://dev2-ets.gost-group.com/docs/',
+    dev: 'https://dev2-ets.gost-group.com/docs/',
     gost_stage: 'https://ets-stage.gost-group.com/docs/',
     ets_test: 'https://ets-test.mos.ru/docs/',
     ets_hotfix: 'https://ets-hotfix.mos.ru/docs/',
-    prod: 'http://ets.mos.ru/docs/',
+    prod: 'https://ets.mos.ru/docs/',
   },
   origin: `${PROTO}//${HOST}${PATHNAME}docs/`,
 };
 
 const config = {
   develop: {
-    ws: `${WS_PROTO}//ets${
-      STAND !== 'prod' ? '-test' : ''
-    }.mos.ru/services/stream`,
+    ws: `wss//ets${STAND !== 'prod' ? '-test' : ''}.mos.ru/services/stream`,
     images: 'https://ets.mos.ru/ets/data/images/',
     docs: DOC_URL.develop[process.env.STAND],
     admin: ADMIN_URL.develop[process.env.STAND],
   },
   origin: {
-    ws: `${WS_PROTO}//ets${
-      STAND !== 'prod' ? '-test' : ''
-    }.mos.ru/services/stream`,
+    ws: `wss//ets${STAND !== 'prod' ? '-test' : ''}.mos.ru/services/stream`,
     images: `https://ets.mos.ru/ets/data/images/`,
     docs: DOC_URL.origin,
     admin: ADMIN_URL.origin,
@@ -61,12 +52,12 @@ const notification_config = {
     ets_hotfix: 'wss://ets-hotfix.mos.ru/services/notification_ws',
     prod: 'wss://ets.mos.ru/services/notification_ws',
   },
-  origin: `${WS_PROTO}//${HOST}${PATHNAME}services/notification_ws`,
+  origin: `wss//${HOST}${PATHNAME}services/notification_ws`,
 };
 
 export const configApi = {
   develop: {
-    dev: 'http://dev2-ets.gost-group.com/services',
+    dev: 'https://dev2-ets.gost-group.com/services',
     gost_stage: 'https://ets-stage.gost-group.com/services',
     ets_test: 'https://ets-test.mos.ru/services',
     ets_hotfix: 'https://ets-hotfix.mos.ru/services',

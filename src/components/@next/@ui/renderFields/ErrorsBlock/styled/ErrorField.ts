6c351e8b9ab91466a918a406023ed5585063bc37
vredsun <vokinda@yandex.ru>
2019-10-03 16:00:56 +0300

    DITETS-6708 Fix error in table

--- src/components/@next/@ui/renderFields/ErrorsBlock/styled/ErrorField.ts
+++ src/components/@next/@ui/renderFields/ErrorsBlock/styled/ErrorField.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { UiConstants } from 'components/@next/@ui/renderFields/UiConstants';
 
 export const ErrorField = styled.div`
+  white-space: pre-wrap;
   color: ${UiConstants.colorError};
   font-size: 12px;
   padding: 2px 5px;

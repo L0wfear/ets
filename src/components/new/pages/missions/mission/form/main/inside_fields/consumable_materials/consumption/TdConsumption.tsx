e1f75bb877237a70232a17bd263a140a45d80e98
vredsun <vokinda@yandex.ru>
2019-10-09 18:31:15 +0300

    DITETS-7096 Изменяет отображение попапа при смене типа расчёта "Итого" в расходном материале

--- src/components/new/pages/missions/mission/form/main/inside_fields/consumable_materials/consumption/TdConsumption.tsx
+++ src/components/new/pages/missions/mission/form/main/inside_fields/consumable_materials/consumption/TdConsumption.tsx
@@ -67,15 +67,13 @@ const TdConsumption: React.FC<Props> = React.memo(
             return;
           }
         } else {
-          if (changed_consumable_materials.mission_progress_fact_value !== changed_consumable_materials.fact_value) {
-            try {
-              await global.confirmDialog({
-                title: 'Внимание!',
-                body: 'Поле "Расход (итого)" станет недоступным для редактирования и будет пересчитываться автоматически.\n Продолжить?',
-              });
-            } catch {
-              return;
-            }
+          try {
+            await global.confirmDialog({
+              title: 'Внимание!',
+              body: 'Поле "Расход (итого)" станет недоступным для редактирования и будет пересчитываться автоматически.\n Продолжить?',
+            });
+          } catch {
+            return;
           }
 
           changed_consumable_materials = mergeConsumableMaterials(

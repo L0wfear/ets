145dfab8a0cc7614dcabea8bab4f9f353b0ac8a1
mrAmiD <nadymov2910@gmail.com>
2019-12-02 17:31:40 +0300

    DITETSSUP-2156 Откат

--- src/components/old/reports/regulated/fuel_consumption/config-data/permissions.ts
+++ src/components/old/reports/regulated/fuel_consumption/config-data/permissions.ts
@@ -4,5 +4,4 @@ export default {
   read: 'fuel_consumption_report.read',
   update: 'fuel_consumption_report.update',
   delete: 'fuel_consumption_report.delete',
-  list_without_sensor: 'fuel_consumption_report_without_sensor.list',
 };

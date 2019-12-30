6298b6b9b6a10b26a0634e3331dd7fc21557d2fd
vredsun <vokinda@yandex.ru>
2019-12-30 13:53:02 +0300

    Добавляет тесты для validate_permissions

--- src/components/@next/@utils/formatData/formatDataOptions.ts
+++ src/components/@next/@utils/formatData/formatDataOptions.ts
@@ -1,14 +1,14 @@
 import { Car } from 'redux-main/reducers/modules/autobase/@types/autobase.h';
-import { get } from 'lodash';
 
-export const carActualOptionLabelGarage = ( // <рег номер ТС [гаражный номер/Марка шасси/Модель/Тип ТС]>
+// <рег номер ТС [гаражный номер/Марка шасси/Модель/Тип ТС]>
+export const carActualOptionLabelGarage = (
   rowData: Car,
 ) => {
-  const gov_number = get(rowData, 'gov_number'); // рег номер ТС
-  const garage_number = get(rowData, 'garage_number'); // гаражный номер
-  const model_name = get(rowData, 'model_name'); // Марка шасси
-  const special_model_name = get(rowData, 'special_model_name'); // Модель
-  const type_name = get(rowData, 'type_name'); // Тип ТС
+  const gov_number = rowData?.gov_number;                   // рег номер ТС
+  const garage_number = rowData?.garage_number;             // гаражный номер
+  const model_name = rowData?.model_name;                   // Марка шасси
+  const special_model_name = rowData?.special_model_name;   // Модель
+  const type_name = rowData?.type_name;                     // Тип ТС
 
   return `${gov_number} [${garage_number || ''}${ garage_number ? '/' : ''}${model_name || ''}${ model_name ? '/' : ''}${special_model_name || ''}${type_name ? '/' : ''}${type_name || ''}]`;
 };

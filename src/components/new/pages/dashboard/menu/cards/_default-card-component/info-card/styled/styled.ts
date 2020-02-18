3dee0e0f35d813e998117b876a256bbcc9e3af88
mrAmiD <nadymov2910@gmail.com>
2020-02-18 12:41:58 +0300

    DITETS19-1783 Рабочий стол. В разделах виджета "Выданные путевые листы (выполненные)" записи не сгруппированы по дате выдачи ПЛ.

--- src/components/new/pages/dashboard/menu/cards/_default-card-component/info-card/styled/styled.ts
+++ src/components/new/pages/dashboard/menu/cards/_default-card-component/info-card/styled/styled.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { EtsDashboardCardContainer } from 'components/new/ui/@bootstrap/29-dashboard_card/EtsDashboardCard';
+import { fadeInAnimation } from 'global-styled/global-animation';
 
 export const InfoCardWrapContainer = styled.div`
   position: absolute;
@@ -20,7 +21,7 @@ export const CardInfoContainer = styled.div`
   pointer-events: all;
   position: absolute;
   z-index: 1;
-  /* background: red; */
+  animation: ${fadeInAnimation} .2s ease-in;
   ${EtsDashboardCardContainer} {
     box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 0px 0px, rgba(0, 0, 0, 0.1) 0px 1px 15px 0px;
   }

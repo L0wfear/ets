fee793669846a4f0c412fa1b51507e4e38981c5b
mrAmiD <nadymov2910@gmail.com>
2020-04-14 17:47:33 +0300

    DITETS19-2091 В раздел “Отчеты - Аналитика” (/analytics) необходимо строку “Маршруты” переименовать в “Маршруты (действующие)”

--- src/components/old/reports/analytics/Analytics.tsx
+++ src/components/old/reports/analytics/Analytics.tsx
@@ -43,7 +43,7 @@ type Props = (
 
 class Analytics extends React.Component<Props, any> {
   reports = [
-    'Маршруты',
+    'Маршруты (действующие)',
     'Задания',
     'Путевые листы',
     'Транспортные средства',

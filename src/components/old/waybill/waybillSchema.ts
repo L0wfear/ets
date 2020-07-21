4d3624449a9b306d47e6252295bc9ab1732dc9fc
Olga Fedorova <olga.fedorova@wayray.com>
2020-07-21 13:15:18 +0300

    DITETS19-2551 Одновременно добавление файлов к счетчику моточасов и одометру + моточасов оборудования

--- src/components/old/waybill/waybillSchema.ts
+++ src/components/old/waybill/waybillSchema.ts
@@ -426,43 +426,19 @@ export const waybillSchema: SchemaType<Waybill, WaybillFormWrapProps> = {
       type: 'multiValueOfArray',
       dependencies: [
         (files, formData) => {
-          if (formData.is_edited_odometr) {
-            if (!files) {
-              return {
-                odometr: 'Поле "Файл" должно быть заполнено',
-                motohours: 'Поле "Файл" должно быть заполнено',
-                motohours_equip: 'Поле "Файл" должно быть заполнено',
-              };
-            }
-            if (!(files && files.some(({ kind }) => kind === 'odometr'))) {
-              return {
-                odometr: 'Поле "Файл" должно быть заполнено'
-              };
-            }
+          if (!files) {
+            return {
+              odometr: 'Поле "Файл" должно быть заполнено',
+              motohours: 'Поле "Файл" должно быть заполнено',
+              motohours_equip: 'Поле "Файл" должно быть заполнено',
+            };
           }
-          if (formData.is_edited_motohours) {
-            if (!files) {
-              return {
-                motohours: 'Поле "Файл" должно быть заполнено',
-              };
-            }
-            if (!(files && files.some(({ kind }) => kind === 'motohours'))) {
-              return {
-                motohours: 'Поле "Файл" должно быть заполнено',
-              };
-            }
-          }
-          if (formData.is_edited_motohours_equip) {
-            if (!files) {
-              return {
-                motohours_equip: 'Поле "Файл" должно быть заполнено',
-              };
-            }
-            if (!(files && files.some(({ kind }) => kind === 'motohours_equip'))) {
-              return {
-                motohours_equip: 'Поле "Файл" должно быть заполнено',
-              };
-            }
+          if (formData.is_edited_odometr || formData.is_edited_motohours || formData.is_edited_motohours_equip) {
+            return {
+              odometr: !(files && files.some(({ kind }) => kind === 'odometr')) ? 'Поле "Файл" должно быть заполнено' : false,
+              motohours: !(files && files.some(({ kind }) => kind === 'motohours')) ? 'Поле "Файл" должно быть заполнено': false,
+              motohours_equip: !(files && files.some(({ kind }) => kind === 'motohours_equip')) ? 'Поле "Файл" должно быть заполнено' : false,
+            };
           }
           return false;
         },

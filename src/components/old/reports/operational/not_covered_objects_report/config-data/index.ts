8e5a23a730fe3a09e47fb0e9f5fc6b2893634168
Rafael <rafik.mgdesyan@gost-group.com>
2020-07-29 15:24:28 +0300

    DITETS20A-120 корректировка url

--- src/components/old/reports/operational/not_covered_objects_report/config-data/index.ts
+++ src/components/old/reports/operational/not_covered_objects_report/config-data/index.ts
@@ -2,9 +2,9 @@ import permissions from 'components/old/reports/operational/not_covered_objects_
 import component from 'components/old/reports/operational/not_covered_objects_report/config-data/components';
 
 export default {
-  path: '/not_covered_objects_report',
+  path: '/not-covered-objects-report',
   title: 'ОДХ/ДТ и элементы, не назначенные на централизованные задания',
-  entyity: 'not-covered-objects-report',
+  entyity: 'not_covered_objects_report',
 
   component,
   permissions,

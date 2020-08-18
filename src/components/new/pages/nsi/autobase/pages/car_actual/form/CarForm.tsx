3002cc3447db37ff15fa5dd41c044478b0eac47e
mrAmiD <nadymov2910@gmail.com>
2020-08-18 18:10:17 +0300

    revert

--- src/components/new/pages/nsi/autobase/pages/car_actual/form/CarForm.tsx
+++ src/components/new/pages/nsi/autobase/pages/car_actual/form/CarForm.tsx
@@ -34,19 +34,6 @@ const CarForm: React.FC<PropsCar> = React.memo(
 
     const isPassport = state.is_gibdd_passport|| state.is_gtn_passport || state.is_gims_passport;
 
-    const validPassportData: CarWrap['passport_data'] | any = React.useMemo(() => {
-      if (state.is_gibdd_passport && state.is_gtn_passport) {
-        return state.passport_data;
-      }
-      if (state.is_gibdd_passport && state.passport_data.type === 'GIBDD') {
-        return state.passport_data;
-      }
-      if (state.is_gtn_passport && state.passport_data.type === 'GTN') {
-        return state.passport_data;
-      }
-      return [];
-    }, [state.passport_data, state.is_gibdd_passport, state.is_gtn_passport]);
-
     const contextValue: CarActualRegistryFormContextType = React.useMemo(
       () => {
         return {
@@ -66,7 +53,7 @@ const CarForm: React.FC<PropsCar> = React.memo(
             <CarFormBodyHeader isPassport={isPassport} isPermitted={isPermitted} />
             <CarFormBodyContainer
               isPermitted={isPermitted}
-              formState={{...state, passport_data: validPassportData}}
+              formState={state}
               formErrors={errors}
               onChange={props.handleChange}
               onChangeBoolean={props.handleChangeBoolean}

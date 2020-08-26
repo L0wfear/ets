77839dbd98f79da3807a6aba9ce282e936782b47
Olga Fedorova <olga.fedorova@wayray.com>
2020-08-26 22:50:52 +0300

    DITETS20A-150 Нет валидации при выгрузке отчета по выработке ТС

--- src/components/new/ui/registry/components/data/header/buttons/component-button/button-by-type/waybill/print_form/waybill_interval_print_form/WaybillIntervalPrintForm.tsx
+++ src/components/new/ui/registry/components/data/header/buttons/component-button/button-by-type/waybill/print_form/waybill_interval_print_form/WaybillIntervalPrintForm.tsx
@@ -17,6 +17,9 @@ import { saveData } from 'utils/functions';
 import DatePickerRange from 'components/new/ui/date_picker/DatePickerRange';
 import EtsBootstrap from 'components/new/ui/@bootstrap';
 import { actionGetBlobWaybillReport } from 'redux-main/reducers/modules/waybill/waybill_actions';
+import { etsUseSelector } from 'components/@next/ets_hoc/etsUseDispatch';
+import { getSessionState } from 'redux-main/reducers/selectors';
+import { diffDates, createValidDateTime, getToday9am, getTomorrow9am } from 'components/@next/@utils/dates/dates';
 
 const WaybillIntervalPrintForm: React.FC<PropsWaybillIntervalPrint> = React.memo(
   (props) => {
@@ -26,10 +29,20 @@ const WaybillIntervalPrintForm: React.FC<PropsWaybillIntervalPrint> = React.memo
       page, path,
     } = props;
 
+    const [datesData, setDatesData] = React.useState({
+      date_start: state.date_start || createValidDateTime(getToday9am()),
+      date_end: state.date_end || createValidDateTime(getTomorrow9am()),
+      error_date_start: '',
+      error_date_end: '',
+    });
+    const companies = etsUseSelector((state) => getSessionState(state).userData.companies);
+
     const title = 'Печать отчета по выработке ТС';
 
     const handleSubmit = React.useCallback(
       async () => {
+        const { date_start, date_end } = datesData;
+
         global.NOTIFICATION_SYSTEM.notifyWithObject({
           title: 'Загрузка печатной формы',
           level: 'info',
@@ -41,8 +54,8 @@ const WaybillIntervalPrintForm: React.FC<PropsWaybillIntervalPrint> = React.memo
         });
 
         const payload: Partial<WaybillsReportForm> = {
-          date_start: state.date_start,
-          date_end: state.date_end,
+          date_start: createValidDateTime(date_start),
+          date_end: createValidDateTime(date_end),
         };
 
         const result = await props.submitAction(
@@ -59,7 +72,37 @@ const WaybillIntervalPrintForm: React.FC<PropsWaybillIntervalPrint> = React.memo
         }
         global.NOTIFICATION_SYSTEM.removeNotification('waybilPrintForm');
       },
-      [props.filter, state],
+      [props.filter, state, datesData],
+    );
+
+    const handleChange = React.useCallback(
+      (field, value) => {
+        setDatesData(
+          (oldState) => {
+            const newState = {
+              ...oldState,
+              [field]: value,
+            };
+
+            const diffMonths = diffDates(newState.date_end, newState.date_start, 'months', true);
+
+            newState.error_date_start = (
+              (!newState.date_start ? 'Дата должна быть заполнена' : '')
+              || (((companies.length > 1) && diffMonths >= 3) ? 'Период формирования выгрузки не должен превышать 3 месяца' : '')
+              || (diffMonths >= 12 ? 'Период формирования выгрузки не должен превышать 1 год' : '')
+            );
+
+            newState.error_date_end = (
+              (diffDates(newState.date_start, newState.date_end) >= 0 ? '"Дата по" должна быть позже "Даты с"' : '')
+              || (!newState.date_end ? 'Дата должна быть заполнена' : '')
+              || (((companies.length > 1) && diffMonths >= 3 || diffMonths >= 12) ? ' ' : '')
+            );
+
+            return newState;
+          },
+        );
+      },
+      [datesData],
     );
 
     return (
@@ -74,13 +117,13 @@ const WaybillIntervalPrintForm: React.FC<PropsWaybillIntervalPrint> = React.memo
                 allWidth={false}
                 label="Период формирования"
                 date_start_id="date_start"
-                date_start_value={state.date_start}
-                date_start_error={errors.date_start}
+                date_start_value={datesData.date_start}
+                date_start_error={datesData.error_date_start}
                 date_end_id="date_end"
-                date_end_value={state.date_end}
-                date_end_error={errors.date_end}
+                date_end_value={datesData.date_end}
+                date_end_error={datesData.error_date_end}
 
-                onChange={props.handleChange}
+                onChange={handleChange}
               />
             </EtsBootstrap.Col>
             <EtsBootstrap.Col md={12}>
@@ -97,7 +140,7 @@ const WaybillIntervalPrintForm: React.FC<PropsWaybillIntervalPrint> = React.memo
           </EtsBootstrap.Row>
         </ModalBodyPreloader>
         <EtsBootstrap.ModalFooter>
-          <EtsBootstrap.Button disabled={!props.canSave} onClick={handleSubmit}>Ок</EtsBootstrap.Button>
+          <EtsBootstrap.Button disabled={!props.canSave || Boolean(datesData.error_date_start || datesData.error_date_end)} onClick={handleSubmit}>Ок</EtsBootstrap.Button>
           <EtsBootstrap.Button onClick={props.hideWithoutChanges}>Отменить</EtsBootstrap.Button>
         </EtsBootstrap.ModalFooter>
       </EtsBootstrap.ModalContainer>

5d672703395aea44c131ae20373937350245b8fe
Rafael <rafamg05@gmail.com>
2020-10-05 23:22:58 +0300

    DITETS19-2785 реестр периодических поверок тахографов

--- src/components/new/pages/nsi/autobase/pages/tachograph_periodic_verification/_config-data/permissions.ts
+++ src/components/new/pages/nsi/autobase/pages/tachograph_periodic_verification/_config-data/permissions.ts
@@ -1,18 +1,9 @@
-// const tachographPermissions = {
-//   list: 'tachograph_periodic_verification.list',
-//   read: 'tachograph_periodic_verification.read',
-//   create: 'tachograph_periodic_verification.create',
-//   update: 'tachograph_periodic_verification.update',
-//   delete: 'tachograph_periodic_verification.delete',
-// };
-
-// export default tachographPermissions;
-const penaltyPermissions = {
-  list: 'waybill.list',
-  read: 'waybill.read',
-  create: 'waybill.create',
-  update: 'waybill.update',
-  delete: 'waybill.delete',
+const tachographPermissions = {
+  list: 'tachograph_periodic_verification.list',
+  read: 'tachograph_periodic_verification.read',
+  create: 'tachograph_periodic_verification.create',
+  update: 'tachograph_periodic_verification.update',
+  delete: 'tachograph_periodic_verification.delete',
 };
 
-export default penaltyPermissions;
+export default tachographPermissions;

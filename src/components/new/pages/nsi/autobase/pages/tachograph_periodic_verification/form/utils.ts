a8b26b593ca0e98b2746cf8688d3193dde91647d
mrAmiD <nadymov2910@gmail.com>
2020-10-21 17:35:02 +0300

    add tsc

--- src/components/new/pages/nsi/autobase/pages/tachograph_periodic_verification/form/utils.ts
+++ src/components/new/pages/nsi/autobase/pages/tachograph_periodic_verification/form/utils.ts
@@ -22,7 +22,6 @@ export const defaultTachograph: Tachograph = {
   calibration_type_name: '',
   files: [],
   dataForValidation: null,
-  tachograph_brand_id: null,
 };
 
 export const getDefaultTachographElement = (element: Partial<Tachograph>): Tachograph => {

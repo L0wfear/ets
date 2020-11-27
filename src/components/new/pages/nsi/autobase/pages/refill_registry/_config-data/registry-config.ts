8c6f8b658ccf09c2572bb3fcece783ed4c95b471
Rafael <rafamg05@gmail.com>
2020-11-27 19:08:45 +0300

    DITETS20S-674 поменял car_gov_number на car_gov_number_text, изменил хэндлер для фильтра по рег номеру

--- src/components/new/pages/nsi/autobase/pages/refill_registry/_config-data/registry-config.ts
+++ src/components/new/pages/nsi/autobase/pages/refill_registry/_config-data/registry-config.ts
@@ -100,12 +100,12 @@ export const getToConfig = (): TypeConfigData<Refill> => {
           step: 1,
         },
         {
-          valueKey: 'car_gov_number',
+          valueKey: 'car_gov_number_text',
           title: 'Рег. номер ТС',
           type: 'multiselect',
           getRegistryData: {
-            entity: 'car_actual',
-            groupName: 'car_actual',
+            entity: 'filters/refill_registry/cars',
+            groupName: 'filters/refill_registry/cars',
             valueKey: 'gov_number',
             labelKey: 'gov_number',
           },
@@ -241,7 +241,7 @@ export const getToConfig = (): TypeConfigData<Refill> => {
             width: 150,
           },
           {
-            key: 'car_gov_number',
+            key: 'car_gov_number_text',
             title: 'Рег. номер ТС',
             width: 150,
           },

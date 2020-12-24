fdc6c84d24220533e15b948dea3ffba58b065d42
Rafael <rafamg05@gmail.com>
2020-12-24 16:14:35 +0300

    DITETS19-3392 добавил блок с логированием в карточку ТК

---
+++ src/components/new/pages/nsi/autobase/pages/fuel_cards/form/logs_block/_config-data/registry-config.ts
@@ -0,0 +1,42 @@
+import { TypeConfigData } from 'components/new/ui/registry/module/@types/registry';
+import fuelCardsPermissions from 'components/new/pages/nsi/autobase/pages/fuel_cards/_config-data/permissions';
+import { FuelCardLog } from '../@types/FuelCardLogs';
+
+export const registryKey = 'Logs';
+
+export const getToConfig = (id: number): TypeConfigData<FuelCardLog> => {
+  return {
+    Service: {
+      getRegistryData: {
+        entity: 'fuel_card_logs',
+        payload: {
+          id
+        }
+      },
+    },
+    registryKey,
+    list: {
+      permissions: fuelCardsPermissions,
+      meta: {
+        fields: [
+          {
+            key: 'action_at',
+            title: 'Дата и время изменения',
+            format: 'datetime',
+            width: 250,
+          },
+          {
+            key: 'source',
+            title: 'Источник',
+            width: 150,
+          },
+          {
+            key: 'repr',
+            title: 'Информация',
+            width: 300,
+          },
+        ],
+      },
+    },
+  };
+};

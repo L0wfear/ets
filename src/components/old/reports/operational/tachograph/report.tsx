49f45354fc55213ad2962795ee11da792295d857
Rafael <rafamg05@gmail.com>
2021-02-19 19:37:47 +0300

    DITETS19-3603 отсутствуют прочерки в ячейках

--- src/components/old/reports/operational/tachograph/report.tsx
+++ src/components/old/reports/operational/tachograph/report.tsx
@@ -5,16 +5,22 @@ import { ISchemaMaker } from 'components/old/ui/table/@types/schema.h';
 import { exportable } from 'utils/decorators';
 import ReportContainer from 'components/old/reports/common/ReportContainer';
 import ReportHeader from 'components/old/reports/operational/tachograph/ReportHeader';
+import { isNullOrUndefined } from 'util';
 
 const serviceUrl = 'autobase/reports/tachograph';
 const reportUrl = 'tachograph';
 const serviceName = 'TachographReport';
 
-const schemaMakers: ISchemaMaker = {
+const schemaMakers: ISchemaMaker = {};
 
+const renderers = {
+  okrug_name: ({ data }) => !isNullOrUndefined(data) ? data : '-',
+  district_name: ({ data }) => !isNullOrUndefined(data) ? data : '-',
 };
-
-const renderers = {};
+const summaryRenderes = {
+  okrug_name: ({ data }) => !isNullOrUndefined(data) ? data : '-',
+  district_name: ({ data }) => !isNullOrUndefined(data) ? data : '-',
+};
 
 const reportProps: IReportProps = {
   title: 'Отчет по тахографам',
@@ -29,6 +35,8 @@ const reportProps: IReportProps = {
   enumerated: true,
   schemaMakers,
   notUseServerSummerTableForPrint: true,
+  summaryRenderes,
+
 };
 
 const ExportableReportContainer = exportable({

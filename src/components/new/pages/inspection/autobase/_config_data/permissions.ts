999109afb5ddbc82d489e50e2ba06d4a392c2fb0
Rafael <rafamg05@gmail.com>
2021-02-25 22:35:03 +0300

    DITETS20A-205 добавил параметры для дизейбла кнопки удаления записи

--- src/components/new/pages/inspection/autobase/_config_data/permissions.ts
+++ src/components/new/pages/inspection/autobase/_config_data/permissions.ts
@@ -3,7 +3,7 @@ const inspectAutobasePermissions = {
   create: 'inspect.autobase.create',
   read: 'inspect.autobase.read',
   update: 'inspect.autobase.update',
-  delete: 'inspect.autobase.list',
+  delete: 'inspect.autobase.delete',
 
   update_closed: 'inspect.autobase_closed.update', // редактирование закрытой проверки
 };
--- src/components/new/pages/inspection/pgm_base/_config_data/permissions.ts
+++ src/components/new/pages/inspection/pgm_base/_config_data/permissions.ts
@@ -3,7 +3,7 @@ const inspectPgmBasePermissions = {
   create: 'inspect.pgm_base.create',
   read: 'inspect.pgm_base.read',
   update: 'inspect.pgm_base.update',
-  delete: 'inspect.pgm_base.list',
+  delete: 'inspect.pgm_base.delete',
 
   update_closed: 'inspect.pgm_base_closed.update', // редактирование закрытой проверки
 };

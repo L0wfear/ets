b205afdcbdfa29de35b90452f475aace502cb5c6
Rafael <rafamg05@gmail.com>
2021-03-17 11:18:21 +0300

    DITETS19-3718 поправил права в реестре настройки доступов

--- src/components/new/pages/nsi/user_acces/_config-data/permissions.ts
+++ src/components/new/pages/nsi/user_acces/_config-data/permissions.ts
@@ -1,6 +1,6 @@
 export default {
   list: 'user_access_registry.list',
-  create: 'user_access_registry.create',
+  create: 'user_access_registry.update',
   read: 'user_access_registry.read',
   update: 'user_access_registry.update',
   delete: 'user_access_registry.delete',

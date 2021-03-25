45a2755c5ac51f70b7870eaa2d130419add2e2fd
mrAmiD <nadymov2910@gmail.com>
2021-03-25 16:00:31 +0300

    DITETS19-3715 Дорнивест. Новые права при работе со скан-копиями. Названия прав

--- src/components/new/ui/registry/components/data/header/buttons/component-button/button-by-type/inspect/forms/show_acts/registry/permissions.ts
+++ src/components/new/ui/registry/components/data/header/buttons/component-button/button-by-type/inspect/forms/show_acts/registry/permissions.ts
@@ -2,7 +2,7 @@ const inspectActScanPermissions = {
   list: 'inspect.act_scan.read',
   create: 'inspect.act_scan.update',
   read: 'inspect.act_scan.read',
-  update: 'inspect.act_scan.update',
+  update: 'inspect.act_scan.add',
   delete: 'inspect.act_scan.delete',
 };
 
